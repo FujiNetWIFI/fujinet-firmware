20f4b4d25335b3b4ceb00a5ffcc9f9486de3ba99
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-11-04 10:55:40 -0600

    start from mozzwald's changes. command frame trapping seems to work now, onward to status and read test.

--- atariwifi.ino
+++ atariwifi.ino
@@ -29,31 +29,48 @@
 #define PIN_LED        2
 #define PIN_CMD        12
 
+#define READ_CMD_TIMEOUT  500
+
 // Debugging bits.
 // Global debug object.
 RemoteDebug Debug;
-
-int inCommand=false;
-
-const char* ssid = "Cherryhomes";
-const char* password = "e1xb64XC46";
+const char* ssid = "";
+const char* password = "";
 
 String hostNameWifi = MDNS_HOST_NAME;
 
+bool incomingSioCmd = false;
+int cmdFrame[5]; // Holds the command frame data
+int cmdFramePos = 0; // Position in command frame array
+unsigned long cmdTimeoutInterval; // max wait time for cmd frame
+
 void sio_cmd_change()
 {
-  debugI("SIO Command frame");
-  debugI("C: 0x%02x",Serial.read());
-  debugI("C: 0x%02x",Serial.read());
-  debugI("C: 0x%02x",Serial.read());
-  debugI("C: 0x%02x",Serial.read());
-  debugI("C: 0x%02x",Serial.read());
+  int pinState = digitalRead(PIN_CMD);
+  if (pinState == 0) // low indicates incoming cmd frame
+    {
+      incomingSioCmd = true;
+      cmdTimeoutInterval = millis();
+    }
+    else if(pinState == 1) // back to high, cmd frame over
+    {
+      incomingSioCmd = false;
+      cmdFramePos = 0;
+    }
+}
+
+void sio_cmd_start()
+{
+  incomingSioCmd = true;
+  cmdTimeoutInterval = millis();
 }
 
 void setup() {
   WiFi.begin(ssid, password);
-  Serial.begin(19200,SERIAL_8N1);
+  Serial.begin(19200);
+  Serial.flush();
   Serial.swap();
+  
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -66,19 +83,62 @@ void setup() {
   Debug.setResetCmdEnabled(true); // Enable the reset command
   Debug.showProfiler(true); // Profiler (Good to measure times, to optimize codes)
   Debug.showColors(true); // Colors
-  attachInterrupt(digitalPinToInterrupt(PIN_CMD), sio_cmd_change, FALLING);
+  pinMode(PIN_CMD, INPUT);
+  attachInterrupt(digitalPinToInterrupt(PIN_CMD), sio_cmd_start, FALLING);
   debugI("setup complete.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
   Debug.handle();
-  yield();
+  if (incomingSioCmd == true)
+    {
+      if (millis() - cmdTimeoutInterval > READ_CMD_TIMEOUT)
+        {
+          debugI("SIO COMMAND timeout");
+          incomingSioCmd = false;
+          cmdFramePos = 0;
+        }
+      if (cmdFramePos > 4)
+        {
+          debugI("SIO COMMAND end");
+          incomingSioCmd = false;
+          cmdFramePos = 0;
+        }
+     if (cmdFramePos == 0 && incomingSioCmd == true)
+        debugI("SIO COMMAND start");
+
+      if (Serial.available() > 0 && incomingSioCmd == true)
+        {
+          cmdFrame[cmdFramePos] = Serial.read();
+          if (cmdFramePos == 0){
+              if((cmdFrame[cmdFramePos] > 47 && cmdFrame[cmdFramePos] < 56) // Disk drives
+                  || (cmdFrame[cmdFramePos] > 79 && cmdFrame[cmdFramePos] < 84)) // R device
+                {
+                  // Continue if valid device id
+                  debugI("C%d: 0x%X", cmdFramePos, cmdFrame[cmdFramePos]); // Print in HEX
+                  cmdFramePos++;
+                }
+              else
+                {
+                  // Invalid Device ID, ignore the rest of this command
+                  debugI("SIO COMMAND Invalid DeviceID: 0x%X", cmdFrame[cmdFramePos]);
+                  incomingSioCmd = false;
+                  cmdFramePos = 0;             
+                }
+            }
+            else
+            {
+              debugI("C%d: 0x%X", cmdFramePos, cmdFrame[cmdFramePos]); // Print in HEX
+              cmdFramePos++;
+            }
+        }
+      if (digitalRead(PIN_CMD) == HIGH && incomingSioCmd == true)
+        {
+          debugI("SIO COMMAND end (pin HIGH)");
+          incomingSioCmd = false;
+          cmdFramePos = 0;
+        }
+     }
+
+  //yield(); // Not needed until our program gets bigger. This is automatically called every loop
 }
-
-#define HOST_NAME      "remotedebug"
-#define MDNS_HOST_NAME "remotedebug.local"
-
-#define PIN_LED        2
-#define PIN_CMD        12
