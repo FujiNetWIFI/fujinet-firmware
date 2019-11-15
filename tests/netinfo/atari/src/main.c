215fba77c04add450b606e08484d9425cfce090d
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-11-14 23:18:52 -0600

    replace stdio with conio, and the program works now, sheesh.

--- tests/netinfo/atari/src/main.c
+++ tests/netinfo/atari/src/main.c
@@ -11,7 +11,7 @@
 
 #include <atari.h>
 #include <6502.h>
-#include <stdio.h>
+#include <conio.h>
 
 /**
  * The Netinfo structure to make it easy.
@@ -38,69 +38,47 @@ typedef union _netinfo
 
 NetInfo ni;
 
-
-/**
- * Function to retrieve network info from adapter.
- *
- * Params: A pointer to a NetInfo struct.
- * Returns: Status (1 on success, otherwise error.)
- *          Filled in NetInfo struct on success.
- */
-unsigned char sio_get_network_info(NetInfo* ni)
-{
-  struct regs r;
-  OS.dcb.ddevic=0x70;          // Network control device
-  OS.dcb.dunit=0;              // unit 1
-  OS.dcb.dcomnd='!';           // Get network info command
-  OS.dcb.dbuf=ni->rawData;     // Pointer to netinfo buffer
-  OS.dcb.dtimlo=0x01;          // 16 frame timeout
-  OS.dcb.dstats=0x40;          // This is a read.
-  OS.dcb.dbyt=64;              // 64 bytes of data
-  OS.dcb.daux=0;               // no aux for now.
-
-  // $E459 = SIOV
-  r.pc=0xE459;
-  _sys(&r);
-
-  return OS.dcb.dstats;
-}
-
 void print_network_info(NetInfo* ni)
 {
-  printf("SSID: %s\n",ni->ssid);
-  printf("BSSID: %02x:%02x:%02x:%02x:%02x:%02x\n", ni->bssid[5], ni->bssid[4], ni->bssid[3], ni->bssid[2], ni->bssid[1], ni->bssid[0]);
-  printf("IP: %u.%u.%u.%u\n",ni->ipAddress[3],ni->ipAddress[2],ni->ipAddress[1],ni->ipAddress[0]);
-  printf("MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", ni->macAddress[5], ni->macAddress[4], ni->macAddress[3], ni->macAddress[2], ni->macAddress[1], ni->macAddress[0]);
-  printf("RSSI: %ld",ni->rssi);
-  printf("\n\n");
+  cprintf("SSID: %s\r\n",ni->ssid);
+  cprintf("BSSID: %02x:%02x:%02x:%02x:%02x:%02x\r\n", ni->bssid[5], ni->bssid[4], ni->bssid[3], ni->bssid[2], ni->bssid[1], ni->bssid[0]);
+  cprintf("IP: %u.%u.%u.%u\r\n",ni->ipAddress[3],ni->ipAddress[2],ni->ipAddress[1],ni->ipAddress[0]);
+  cprintf("MAC: %02x:%02x:%02x:%02x:%02x:%02x\r\n", ni->macAddress[5], ni->macAddress[4], ni->macAddress[3], ni->macAddress[2], ni->macAddress[1], ni->macAddress[0]);
+  cprintf("RSSI: %ld\r\n",ni->rssi);
+  cprintf("\r\n\r\n");
 }
 
 void main(void)
 {
-  unsigned char error;
-  printf("#AtariWiFi Test Program #8\n");
-  printf("Get Network Information.\n");
-  printf("\n\n");
+  struct regs r;
+  OS.dcb.ddevic=0x70;
+  OS.dcb.dunit=1;
+  OS.dcb.dcomnd='!';
+  OS.dcb.dstats=0x40;
+  OS.dcb.dbuf=ni.rawData;
+  OS.dcb.dtimlo=0x0F;
+  OS.dcb.dunuse=0;
+  OS.dcb.dbyt=64;
+  OS.dcb.daux1=0;
+  OS.dcb.daux2=0;
+  r.pc=0xE459;
+  _sys(&r);
 
-  printf("Reading...");
+  clrscr();
+  cprintf("#AtariWiFi Test Program #8\r\n\r\n");
 
-  error=sio_get_network_info(&ni);
-
-  if (error==138)
+  if (OS.dcb.dstats==138)
     {
-      printf("Timeout!\n");
+      cprintf("Request timed out.\r\n");
     }
-  else if (error==1)
+  else if (OS.dcb.dstats==1)
     {
-      printf("Success!\n");
       print_network_info(&ni);
     }
   else
     {
-      printf("Error #%d",error);
+      cprintf("Error #%d\r\n",OS.dcb.dstats);
     }
 
-  printf("Program done. ");
-  
-  for (;;) { } // spin.
+  for (;;) {}
 }
