b2ed67ac189c17fae34873d4e176a582a0210b56
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-11-24 15:48:53 -0600

    adding in what I have.

--- tests/cio2/atari/src/cio-get.c
+++ tests/cio2/atari/src/cio-get.c
@@ -7,8 +7,14 @@
 
 extern unsigned char err;
 extern unsigned char ret;
+unsigned char c=0;
+
+const char testmsg[]="Testing Input\x9b";
 
 void _cio_get(void)
 {
+  ret=testmsg[c];
+  if (c>14)
+    c=0;
   err=1;
 }
--- tests/cio2/atari/src/cio-stubs.s
+++ tests/cio2/atari/src/cio-stubs.s
@@ -6,7 +6,7 @@
 	.import _err
 	.import __cio_open, __cio_close, __cio_get, __cio_put, __cio_status, __cio_special
 	.export _cio_open, _cio_close, _cio_get, _cio_put, _cio_status, _cio_special
-	
+
 _cio_close:
 	jsr __cio_close
 	lda _ret
