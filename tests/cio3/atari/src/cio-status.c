c4ba2b11009eba44bedb6b160f27607ca92f2454
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-11-27 00:56:54 -0600

    implement more of cio handler.

--- tests/cio3/atari/src/cio-status.c
+++ tests/cio3/atari/src/cio-status.c
@@ -4,11 +4,21 @@
 
 #include <atari.h>
 #include <6502.h>
+#include "sio.h"
 
 extern unsigned char err;
 extern unsigned char ret;
 
 void _cio_status(void)
 {
-  err=1;
+  OS.dcb.ddevic=0x70; // Network adapter
+  OS.dcb.dunit=1;
+  OS.dcb.dcomnd='s';  // tcp status
+  OS.dcb.dstats=0x40; // specify a read
+  OS.dcb.dtimlo=0x1f; // Timeout
+  OS.dcb.dbyt=4;
+  OS.dcb.dbuf=&OS.dvstat; // A packet.
+  OS.dcb.daux1=0;
+  siov();
+  err=OS.dcb.dstats;
 }
