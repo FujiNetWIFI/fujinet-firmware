9d6c66650187f1105dbd90627f704742253d2a94
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-11-27 00:56:54 -0600

    implement more of cio handler.

--- tests/cio3/atari/src/cio-close.c
+++ tests/cio3/atari/src/cio-close.c
@@ -4,11 +4,22 @@
 
 #include <atari.h>
 #include <6502.h>
+#include "sio.h"
 
 extern unsigned char err;
 extern unsigned char ret;
+extern unsigned char packet[256];
 
 void _cio_close(void)
 {
-  err=1;
+  OS.dcb.ddevic=0x70; // Network adapter
+  OS.dcb.dunit=1;
+  OS.dcb.dcomnd='d';  // disconnect
+  OS.dcb.dstats=0x80; // specify a write
+  OS.dcb.dtimlo=0x1f; // Timeout
+  OS.dcb.dbyt=1;
+  OS.dcb.dbuf=&packet; // A packet.
+  OS.dcb.daux1=0;
+  siov();
+  err=OS.dcb.dstats;
 }
