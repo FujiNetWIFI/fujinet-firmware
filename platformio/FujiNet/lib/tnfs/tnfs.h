e6cf3c7c9b23a5e61e770881167b52acb7d15b42
jeffpiep <jeff.piepmeier+github@gmail.com>
2019-11-29 18:41:05 -0500

    try tnfs as File

--- platformio/FujiNet/lib/tnfs/tnfs.h
+++ platformio/FujiNet/lib/tnfs/tnfs.h
@@ -3,8 +3,12 @@
 
 #include <Arduino.h>
 
-
 #include <WiFiUdp.h>
+#ifdef ESP_8266
+#include <FS.h>
+#elif defined(ESP_32)
+#include <SPIFFS.h>
+#endif
 
 #define TNFS_SERVER "mozzwald.com"
 #define TNFS_PORT 16384
@@ -14,61 +18,141 @@ SPIFFS File class definition
 
 */
 
+class tnfsClient : public File
 
-class tnfsClient : public Stream
+/* SPIFFS File defn
+
+class File : public Stream
 {
-private:  
+public:
+    File(FileImplPtr p = FileImplPtr(), FS *baseFS = nullptr) : _p(p), _fakeDir(nullptr), _baseFS(baseFS) { }
 
-byte tnfs_fd;
+    // Print methods:
+    size_t write(uint8_t) override;
+    size_t write(const uint8_t *buf, size_t size) override;
 
-union
+    // Stream methods:
+    int available() override;
+    int read() override;
+    int peek() override;
+    void flush() override;
+    size_t readBytes(char *buffer, size_t length) override {
+        return read((uint8_t*)buffer, length);
+    }
+    size_t read(uint8_t* buf, size_t size);
+    bool seek(uint32_t pos, SeekMode mode);
+    bool seek(uint32_t pos) {
+        return seek(pos, SeekSet);
+    }
+    size_t position() const;
+    size_t size() const;
+    void close();
+    operator bool() const;
+    const char* name() const;
+    const char* fullName() const; // Includes path
+    bool truncate(uint32_t size);
+
+    bool isFile() const;
+    bool isDirectory() const;
+
+    // Arduino "class SD" methods for compatibility
+    template<typename T> size_t write(T &src){
+      uint8_t obuf[256];
+      size_t doneLen = 0;
+      size_t sentLen;
+      int i;
+
+      while (src.available() > sizeof(obuf)){
+        src.read(obuf, sizeof(obuf));
+        sentLen = write(obuf, sizeof(obuf));
+        doneLen = doneLen + sentLen;
+        if(sentLen != sizeof(obuf)){
+          return doneLen;
+        }
+      }
+
+      size_t leftLen = src.available();
+      src.read(obuf, leftLen);
+      sentLen = write(obuf, leftLen);
+      doneLen = doneLen + sentLen;
+      return doneLen;
+    }
+    using Print::write;
+
+    void rewindDirectory();
+    File openNextFile();
+
+    String readString() override;
+
+    time_t getLastWrite();
+    void setTimeCallback(time_t (*cb)(void));
+
+protected:
+    FileImplPtr _p;
+
+    // Arduino SD class emulation
+    std::shared_ptr<Dir> _fakeDir;
+    FS                  *_baseFS;
+    time_t (*timeCallback)(void) = nullptr;
+};
+*/
+
 {
-  struct 
-  {
-    byte session_idl;
-    byte session_idh;
-    byte retryCount;
-    byte command;
-    byte data[508];
-  };
-  byte rawData[512];
-} tnfsPacket;
+private:
+  byte tnfs_fd;
 
+  union {
+    struct
+    {
+      byte session_idl;
+      byte session_idh;
+      byte retryCount;
+      byte command;
+      byte data[508];
+    };
+    byte rawData[512];
+  } tnfsPacket;
 
-/**
- * Mount the TNFS server
- */
-void tnfs_mount();
-
-/**
- * Open 'autorun.atr'
- */
-void tnfs_open();
-
-void tnfs_read();
- 
+  void tnfs_mount();
+  void tnfs_open();
+  void tnfs_read();
 
 public:
-
+ // tnfsClient(FileImplPtr p = nullptr, FS *baseFS = nullptr) : _p(nullptr), _fakeDir(nullptr), _baseFS(nullptr) { }
   void begin();
-  
-  size_t write(uint8_t);
 
-  int available();
-  int peek();
-  void flush();
+  // Print methods:
+  size_t write(uint8_t) override;
+  size_t write(const uint8_t *buf, size_t size) override;
 
-/**
- * TNFS read
- */
-int read();
-void read(byte arr[], int count);
+  // Stream methods:
+  int available() override;
+  int read() override;
+  int peek() override;
+  void flush() override;
+  size_t readBytes(char *buffer, size_t length) override
+  {
+    return read((uint8_t *)buffer, length);
+  }
+  size_t read(uint8_t *buf, size_t size);
 
-/**
- * TNFS seek
- */
-void seek(long offset);
+  //File methods:
+  bool seek(uint32_t offset, SeekMode mode)
+  {
+    seek(offset);
+    return true;
+  }
+  void seek(uint32_t offset);
+  size_t position() const;
+  size_t size() const;
+  void close();
+  operator bool() const;
+  const char *name() const;
+  const char *fullName() const; // Includes path
+  bool truncate(uint32_t size);
 
+  bool isFile() const;
+  bool isDirectory() const;
 };
 
 #endif
