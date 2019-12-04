29a1f30b2684430406ea7eeee72bfeb34fa09b8d
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-12-04 15:49:12 -0600

    move memory around a bit.

--- tests/config/atari/src/screen.h
+++ tests/config/atari/src/screen.h
@@ -11,8 +11,8 @@
 #define GetChar(x,y) video_ptr[(x)+(y)*40]
 #define GRAPHICS_0_SCREEN_SIZE (40*25)
 #define DISPLAY_LIST 0x0600
-#define DISPLAY_MEMORY 0x2400
-#define FONT_MEMORY 0x2000
+#define DISPLAY_MEMORY 0x2800
+#define FONT_MEMORY 0x2400
 
 /**
  * Print ATASCII string to display memory
