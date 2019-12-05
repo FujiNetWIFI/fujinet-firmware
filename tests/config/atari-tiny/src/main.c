7fa926a09be6e5d213979a0136fec1bdd1e2743d
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-12-04 18:07:43 -0600

    add a new autorun

---
+++ tests/config/atari-tiny/src/main.c
@@ -0,0 +1,26 @@
+#include <atari.h>
+#include <stdio.h>
+#include "sio.h"
+#include "die.h"
+
+unsigned char num_networks[4];
+
+void main(void)
+{
+  printf("Scanning Networks:\n\n");
+  OS.dcb.ddevic=0x70;
+  OS.dcb.dunit=1;
+  OS.dcb.dcomnd=0xFD; // do scan
+  OS.dcb.dstats=0x40; // Peripheral->Computer
+  OS.dcb.dbuf=num_networks;
+  OS.dcb.dtimlo=0x0F; // 15 second timeout
+  OS.dcb.dbyt=4;      // 4 byte response
+  OS.dcb.daux=0;
+  siov();
+
+  printf("DSTATS: %d\n",OS.dcb.dstats);
+  printf("Num networks: %d\n",num_networks[0]);
+
+  die();
+  
+}
