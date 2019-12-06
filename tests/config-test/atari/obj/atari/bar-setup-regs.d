2f16f891da1c0c00ad7cc5eccfb1ee94f3f9d76d
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-12-06 16:12:39 -0600

    added config test for boz.

---
+++ tests/config-test/atari/obj/atari/bar-setup-regs.d
@@ -0,0 +1,4 @@
+obj/atari/bar-setup-regs.o:	src/bar-setup-regs.s /home/thomc/Workspace/cc65/asminc/atari.inc /home/thomc/Workspace/cc65/asminc/atari_gtia.inc /home/thomc/Workspace/cc65/asminc/atari_pokey.inc /home/thomc/Workspace/cc65/asminc/atari_antic.inc
+
+src/bar-setup-regs.s /home/thomc/Workspace/cc65/asminc/atari.inc /home/thomc/Workspace/cc65/asminc/atari_gtia.inc /home/thomc/Workspace/cc65/asminc/atari_pokey.inc /home/thomc/Workspace/cc65/asminc/atari_antic.inc:
+
