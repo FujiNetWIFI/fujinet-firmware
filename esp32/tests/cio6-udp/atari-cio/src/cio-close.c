ed31d1bb3eb614dee75ad102ca57d11faf5d936e
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-12-22 12:53:08 -0600

    add start of cio test #6

---
+++ esp32/tests/cio6-udp/atari-cio/src/cio-close.c
@@ -0,0 +1,15 @@
+/**
+ * CIO Close call
+ */
+
+#include <atari.h>
+#include <6502.h>
+#include "sio.h"
+
+extern unsigned char err;
+extern unsigned char ret;
+extern unsigned char packet[256];
+
+void _cio_close(void)
+{
+}
---
+++ esp32/tests/cio6-udp/atari-cio/src/cio-open.c
@@ -0,0 +1,16 @@
+/**
+ * CIO Open call
+ */
+
+#include <atari.h>
+#include <6502.h>
+#include <string.h>
+#include "sio.h"
+
+extern unsigned char err;
+extern unsigned char ret;
+extern unsigned char packet[512];
+
+void _cio_open(void)
+{
+}
