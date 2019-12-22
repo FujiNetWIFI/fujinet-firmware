d8decf363287cbc40561b06e853ed7db434e8bd5
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-12-22 16:28:12 -0600

    god #%(#@ damn it.

--- esp32/tests/cio6-udp/atari-cio/src/main.c
+++ esp32/tests/cio6-udp/atari-cio/src/main.c
@@ -50,5 +50,5 @@ void main(void)
   OS.hatabs[i].devhdl=&devhdl; // handler table for N: device.
 
   // Manually setting memlo, is there a symbol that can get me actual top of data?
-  OS.memlo=(void *)0x23A0;
+  OS.memlo=(void *)0x23C0;
 }
