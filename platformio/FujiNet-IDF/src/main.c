ce752d3be08744aec47999254ecbfc4c947f7951
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2019-12-26 11:58:10 -0600

    adding in fujinet-idf

---
+++ platformio/FujiNet-IDF/src/main.c
@@ -0,0 +1,13 @@
+/**
+ * FujiNet IDF
+ */
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+void app_main()
+{
+    
+}
