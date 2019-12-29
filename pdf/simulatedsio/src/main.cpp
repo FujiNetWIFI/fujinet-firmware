eec9c2b35588cda766d3050bdbb73ff249dae505
jeffpiep <jeff.piepmeier@gmail.com>
2019-12-28 23:55:23 -0500

    output with fprintfs

--- pdf/simulatedsio/src/main.cpp
+++ pdf/simulatedsio/src/main.cpp
@@ -15,6 +15,12 @@ using namespace std;
 // 822 thermal: 40 column, 12 point, 6 LPI, 4 7/16" width: 320 x roll, 16 left
 // 820 dot matrix: 40 column, TBD point, LPI, 3 7/8" width:  279 x roll,
 // 1027 impact:
+int lineHeight = 12;
+int pageWidth = 672;
+int pageHeight = 792;
+int leftMargin = 18;
+int bottomMargin = 2;
+int fontSize = 12;
 
 // todo:
 // specify: page size, left/bottom margins, line spacing
@@ -27,99 +33,75 @@ using namespace std;
 int main()
 {
   ifstream INSTREAM; // input file
-  ofstream OUTSTREAM;
-  FILE *f = fopen("out2.pdf", "w"); // standard C output file
-  string page;                      // buffer for page sections
-  string line;
-  stringstream str1;              // temporary stream buffer for numeric to string conversion
+  string line;       // temporary string to build up lines
+  int offset;
   string s;                       // temporary string for line reading from cin stream
   int objLocations[NUMLINES + 5]; // reference table storage
   int xref;
 
+  FILE *f = fopen("out2.pdf", "w"); // standard C output file
   INSTREAM.open("in.txt");
-  OUTSTREAM.open("out.pdf");
 
   // header ****************************************************************************************
 
-  int objCtr = 1;
-
-  objLocations[objCtr] = fprintf(f, "%%PDF-1.4\n");
-
-  line = "1 0 obj <</Type /Catalog /Pages 2 0 R>> endobj\n";
-  fprintf(f, line.c_str());
-
+  int objCtr = 0;
+  offset = fprintf(f, "%%PDF-1.4\n");
+  // first object: catalog of pages
   objCtr++;
-  objLocations[objCtr] = objLocations[objCtr - 1] + line.length();
-  line = "2 0 obj <</Type /Pages /Kids [3 0 R] /Count 1>> endobj\n";
-  fprintf(f, line.c_str());
-
+  objLocations[objCtr] = offset;
+  offset = fprintf(f, "1 0 obj <</Type /Catalog /Pages 2 0 R>> endobj\n");
+  // second object: one page
   objCtr++;
-  objLocations[objCtr] = objLocations[objCtr - 1] + line.length();
-  line = "3 0 obj <</Type /Page /Parent 2 0 R /Resources 4 0 R /MediaBox [0 0 612 792] /Contents [ ";
-
-  int templength = line.length();
-  fprintf(f, line.c_str());
+  objLocations[objCtr] = objLocations[objCtr - 1] + offset;
+  offset = fprintf(f, "2 0 obj <</Type /Pages /Kids [3 0 R] /Count 1>> endobj\n");
+  // third object: page contents
+  objCtr++;
+  objLocations[objCtr] = objLocations[objCtr - 1] + offset;
+  offset = fprintf(f, "3 0 obj <</Type /Page /Parent 2 0 R /Resources 4 0 R /MediaBox [0 0 %d %d] /Contents [ ", pageWidth, pageHeight);
   for (int i = 0; i < NUMLINES; i++)
   {
-    templength += fprintf(f, "%d 0 R ", i + 6);
+    offset += fprintf(f, "%d 0 R ", i + 6);
   }
-  templength += fprintf(f, "]>> endobj\n");
-
+  offset += fprintf(f, "]>> endobj\n");
+  // fourth object: font catalog
   objCtr++;
-  objLocations[objCtr] = objLocations[objCtr - 1] + templength;
+  objLocations[objCtr] = objLocations[objCtr - 1] + offset;
   line = "4 0 obj <</Font <</F1 5 0 R>>>> endobj\n";
   fprintf(f, line.c_str());
-
+  // fifth object: font 1
   objCtr++;
   objLocations[objCtr] = objLocations[objCtr - 1] + line.length();
-  line = "5 0 obj <</Type /Font /Subtype /Type1 /BaseFont /Courier>> endobj\n";
-  fprintf(f, line.c_str());
+  offset = fprintf(f, "5 0 obj <</Type /Font /Subtype /Type1 /BaseFont /Courier>> endobj\n");
 
   // body ******************************************************************************************
-  templength = line.length();
+  // sixth+ objects: line contents
   for (int i = 0; i < NUMLINES; i++)
   {
-    string s;
+    string payload;
     if (!INSTREAM.eof())
-      getline(INSTREAM, s);
+      getline(INSTREAM, payload);
     objCtr++;
-    objLocations[objCtr] = objLocations[objCtr - 1] + templength;
-    //stringstream str2;
-    templength = fprintf(f, "%d 0 obj <</Length %d>> stream\n", 6 + i, 31 + s.length());
-    //str2 << (6 + i) << " 0 obj <</Length " << (31 + s.length()) << ">> stream\n";
-    templength += fprintf(f, "BT /F1 12 Tf 018 %d Td (%s)Tj ET\n", 782 - i * 12, s.c_str());
-    //str2 << "BT /F1 12 Tf 018 " << (782 - i * 12) << " Td (" << s << ")Tj ET\n";
-    //page += str2.str();
-    templength += fprintf(f, "endstream endobj\n");
+    objLocations[objCtr] = objLocations[objCtr - 1] + offset;
+    offset = fprintf(f, "%d 0 obj <</Length %d>> stream\n", 6 + i, 30 + payload.length());
+    int xcoord = pageHeight - lineHeight + bottomMargin - i * lineHeight;
+    //this string right here vvvvvv is 30 chars long plus the length of the payload
+    offset += fprintf(f, "BT /F1 %2d Tf %2d %3d Td (%s)Tj ET\n", fontSize, leftMargin, xcoord, payload.c_str());
+    offset += fprintf(f, "endstream endobj\n");
   }
-  //OUTSTREAM << page;
-  //fprintf(f, page.c_str());
 
   // xref and output *****************************************************************************
-
-  xref = objLocations[objCtr] + templength;
-  line = "xref\n";
-  line += "0 72\n";
-  //OUTSTREAM << page;
-  fprintf(f, line.c_str());
-
-  //OUTSTREAM << "0000000000 65535 f\n";
+  xref = objLocations[objCtr] + offset;
+  fprintf(f, "xref\n");
+  fprintf(f, "0 %d\n", objCtr);
   fprintf(f, "0000000000 65535 f\n");
-
   for (int i = 1; i < (NUMLINES + 5); i++)
   {
-    //OUTSTREAM << setfill('0') << setw(10) << objLocations[i] << " 00000 n\n";
     fprintf(f, "%010d 00000 n\n", objLocations[i]);
   }
-  //OUTSTREAM << "trailer <</Size 72/Root 1 0 R>>\n";
-  fprintf(f, "trailer <</Size 72/Root 1 0 R>>\n");
-  //OUTSTREAM << "startxref\n";
+  fprintf(f, "trailer <</Size %d/Root 1 0 R>>\n", objCtr);
   fprintf(f, "startxref\n");
-  //OUTSTREAM << xref << "\n"
-  //          << "%%EOF\n";
   fprintf(f, "%d\n", xref);
   fprintf(f, "%%%%EOF\n");
-  //OUTSTREAM.close();
   fclose(f);
   return 0;
 }
