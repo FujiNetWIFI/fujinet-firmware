4b77a6dd5c9ee18a3593a9059573bea1634afd38
jeffpiep <jeff.piepmeier@gmail.com>
2019-12-29 16:28:58 -0500

    move code to procedures and cleanup libraries

--- pdf/simulatedsio/src/main.cpp
+++ pdf/simulatedsio/src/main.cpp
@@ -1,27 +1,29 @@
 #include <stdio.h>
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <sstream>
-#include <string>
-using namespace std;
+#include <string.h>
 
-#define OUTSTREAM pdfout
+#include <fstream>
+//using namespace std;
+
 #define INSTREAM prtin
 #define NUMLINES 66
 
 // printers
-// standard: 80 column, 12 point, 6 LPI, letter size: 612 x 792, 18 left, 2 bottom + 780-12*linenum
+// standard: 80 column, 12 point, 6 LPI, letter size: 612 x 792, 18 left, 2 bottom
 // 822 thermal: 40 column, 12 point, 6 LPI, 4 7/16" width: 320 x roll, 16 left
 // 820 dot matrix: 40 column, TBD point, LPI, 3 7/8" width:  279 x roll,
-// 1027 impact:
+// 1027 impact: 80 columns, TBD point, LPI, letter size: 612 x 792, TBD left, TBD bottom
+#define STANDARD_80
+//#define ATARI_820
+//#define ATARI_820_2_COL
+//#define ATARI_822
+//#define ATARI_1027
 int lineHeight = 12;
 int pageWidth = 612;
 int pageHeight = 792;
 int leftMargin = 18;
 int bottomMargin = 2;
 int fontSize = 12;
-const char* fontName = "Courier";
+const char *fontName = "Courier";
 
 // todo:
 // specify: page size, left/bottom margins, line spacing
@@ -31,21 +33,16 @@ const char* fontName = "Courier";
 // replace page(string) with incremental line string and accumulate lengths for xref
 // use NUMLINES to parameterize the routine
 
-int main()
+std::ifstream INSTREAM; // input file
+int offset;
+int objLocations[NUMLINES + 5]; // reference table storage
+int xref;
+int objCtr = 0;
+
+FILE *f; // standard C output file
+
+void pdf_header()
 {
-  ifstream INSTREAM; // input file
-  string line;       // temporary string to build up lines
-  int offset;
-  string s;                       // temporary string for line reading from cin stream
-  int objLocations[NUMLINES + 5]; // reference table storage
-  int xref;
-
-  FILE *f = fopen("out2.pdf", "w"); // standard C output file
-  INSTREAM.open("in.txt");
-
-  // header ****************************************************************************************
-
-  int objCtr = 0;
   offset = fprintf(f, "%%PDF-1.4\n");
   // first object: catalog of pages
   objCtr++;
@@ -67,30 +64,16 @@ int main()
   // fourth object: font catalog
   objCtr++;
   objLocations[objCtr] = objLocations[objCtr - 1] + offset;
-  line = "4 0 obj <</Font <</F1 5 0 R>>>> endobj\n";
-  fprintf(f, line.c_str());
+  //line = ;
+  offset = fprintf(f, "4 0 obj <</Font <</F1 5 0 R>>>> endobj\n");
   // fifth object: font 1
   objCtr++;
-  objLocations[objCtr] = objLocations[objCtr - 1] + line.length();
-  offset = fprintf(f, "5 0 obj <</Type /Font /Subtype /Type1 /BaseFont /%s>> endobj\n",fontName);
+  objLocations[objCtr] = objLocations[objCtr - 1] + offset;
+  offset = fprintf(f, "5 0 obj <</Type /Font /Subtype /Type1 /BaseFont /%s>> endobj\n", fontName);
+}
 
-  // body ******************************************************************************************
-  // sixth+ objects: line contents
-  for (int i = 0; i < NUMLINES; i++)
-  {
-    string payload;
-    if (!INSTREAM.eof())
-      getline(INSTREAM, payload);
-    objCtr++;
-    objLocations[objCtr] = objLocations[objCtr - 1] + offset;
-    offset = fprintf(f, "%d 0 obj <</Length %d>> stream\n", 6 + i, 30 + payload.length());
-    int xcoord = pageHeight - lineHeight + bottomMargin - i * lineHeight;
-    //this string right here vvvvvv is 30 chars long plus the length of the payload
-    offset += fprintf(f, "BT /F1 %2d Tf %2d %3d Td (%s)Tj ET\n", fontSize, leftMargin, xcoord, payload.c_str());
-    offset += fprintf(f, "endstream endobj\n");
-  }
-
-  // xref and output *****************************************************************************
+void pdf_xref()
+{
   xref = objLocations[objCtr] + offset;
   fprintf(f, "xref\n");
   fprintf(f, "0 %d\n", objCtr);
@@ -103,6 +86,33 @@ int main()
   fprintf(f, "startxref\n");
   fprintf(f, "%d\n", xref);
   fprintf(f, "%%%%EOF\n");
+}
+
+void pdf_add_line(int lineNum, const char *L)
+{
+  objCtr++;
+  objLocations[objCtr] = objLocations[objCtr - 1] + offset;
+  offset = fprintf(f, "%d 0 obj <</Length %d>> stream\n", 6 + lineNum, 30 + strlen(L));
+  int xcoord = pageHeight - lineHeight + bottomMargin - lineNum * lineHeight;
+  //this string right here vvvvvv is 30 chars long plus the length of the payload
+  offset += fprintf(f, "BT /F1 %2d Tf %2d %3d Td (%s)Tj ET\n", fontSize, leftMargin, xcoord, L);
+  offset += fprintf(f, "endstream endobj\n");
+}
+
+int main()
+{
+  INSTREAM.open("in.txt");
+  f = fopen("out2.pdf", "w");
+  pdf_header();
+  // body ******************************************************************************************
+  for (int i = 0; i < NUMLINES; i++)
+  {
+    std::string payload;
+    if (!INSTREAM.eof())
+      getline(INSTREAM, payload);
+    pdf_add_line(i, payload.c_str());
+  }
+  pdf_xref();
   fclose(f);
   return 0;
 }
