c7a0b5cbdea0cec6dba8e5f74b23795b8719241e
jeffpiep <jeff.piepmeier@gmail.com>
2020-01-01 22:58:49 -0500

    basic http server added

--- platformio/FujiNet/src/main.cpp
+++ platformio/FujiNet/src/main.cpp
@@ -29,6 +29,52 @@ File pdff;
 sioDisk sioD[8];
 
 WiFiServer server(80);
+WiFiClient client;
+
+void httpService()
+{
+  // listen for incoming clients
+  client = server.available();
+  if (client) {
+    BUG_UART.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        BUG_UART.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          client.println("Hello World!");
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    BUG_UART.println("client disconnected");
+  }
+}
 
 void setup()
 {
@@ -46,10 +92,12 @@ void setup()
   {
     delay(10);
   }
+  BUG_UART.println(WiFi.localIP());
+  server.begin();
 
   SPIFFS.begin();
   SIO.addDevice(&sioP, 0x40); // P:
-  pdff = SPIFFS.open("pdf.out", "W");
+  pdff = SPIFFS.open("/pdf.out", "w");
   sioP.initPDF(&pdff);
 
   for (int i = 0; i < 8; i++)
@@ -75,4 +123,6 @@ void setup()
 void loop()
 {
   SIO.service();
+  httpService();
 }
+
