ab41ea3739cfeabe2b62c8b43cb24f89e0a3dfbe
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-01-04 14:33:06 -0600

    second pass.

---
+++ esp32/tests/multilator-rev2/atari/src/die.h
@@ -0,0 +1,15 @@
+/**
+ * FujiNet Configurator
+ *
+ * Function to die, wait for keypress and then do coldstart
+ */
+
+#ifndef DIE_H
+#define DIE_H
+
+/**
+ * Stop, wait for keypress, then coldstart
+ */
+void die(void);
+
+#endif /* DIE_H */
