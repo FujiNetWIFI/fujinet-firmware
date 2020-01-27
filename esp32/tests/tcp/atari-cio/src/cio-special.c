05b7a45e5c896c610348b2ab4eb05dbd9dbc28a6
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-01-26 22:46:05 -0600

    put together skeleton for tcp sio

---
+++ esp32/tests/tcp/atari-cio/src/cio-special.c
@@ -0,0 +1,16 @@
+/**
+ * CIO Special (XIO) call
+ */
+
+#include <atari.h>
+#include <6502.h>
+#include <string.h>
+#include "sio.h"
+
+extern unsigned char err;
+extern unsigned char ret;
+extern unsigned char packet[256];
+
+void _cio_special(void)
+{
+}
