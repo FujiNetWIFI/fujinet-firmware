c8629aee845585cfc50e5b42ba8cd999c5f1f2ba
jeffpiep <jeff.piepmeier+github@gmail.com>
2020-02-07 20:35:09 -0500

    tnfs_read() complete

--- platformio/FujiNet/lib/TNFSlib/tnfs_udp.h
+++ platformio/FujiNet/lib/TNFSlib/tnfs_udp.h
@@ -3,6 +3,8 @@
 
 #include <Arduino.h>
 #include <WiFiUdp.h>
+#include "debug.h"
+
 #include "tnfs.h"
 #include "tnfs_imp.h"
 
@@ -32,8 +34,8 @@ bool tnfs_close(TNFSImpl* F, byte fd, const char *mountPath);
 bool tnfs_opendir(unsigned char hostSlot);
 bool tnfs_readdir(unsigned char hostSlot);
 bool tnfs_closedir(unsigned char hostSlot);
-bool tnfs_write(TNFSImpl* F, byte fd, const uint8_t* buf, unsigned short len);
-bool tnfs_read(TNFSImpl* F, byte fd, uint8_t* buf, unsigned short len);
+size_t tnfs_write(TNFSImpl* F, byte fd, const uint8_t* buf, unsigned short len);
+size_t tnfs_read(TNFSImpl* F, byte fd, uint8_t* buf, unsigned short size);
 bool tnfs_seek(TNFSImpl* F, byte fd, long offset);
 // bool tnfs_write_blank_atr(unsigned char deviceSlot, unsigned short sectorSize, unsigned short numSectors);
 
