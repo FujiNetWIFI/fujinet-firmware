d6faee3bdf8529f5cb04d4ead2a6cfb531d49eb4
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-02-20 00:05:35 -0600

    Add devicespec_parse.

--- platformio/FujiNet/lib/sio/network.cpp
+++ platformio/FujiNet/lib/sio/network.cpp
@@ -7,7 +7,45 @@
  */
 bool sioNetwork::parse_deviceSpec()
 {
+  char* p;
+  char i=0;
+  char d=0;
 
+  strcpy(tmp,deviceSpec.rawData);
+
+  p=strtok(tmp,":"); // Get Device spec
+
+  if (p[0]!='N')
+    return false;
+  else
+    strcpy(deviceSpec.device,p);
+
+  while (p!=NULL)
+    {
+      i++;
+      p=strtok(NULL,":");
+      switch(i)
+        {
+        case 1:
+          strcpy(deviceSpec.protocol,p);
+          break;
+        case 2:
+          for (d=0;d<strlen(p);d++)
+            if (!isdigit(p[d]))
+              {
+                strcpy(deviceSpec.path,p);
+                break;
+              }
+          deviceSpec.port=atoi(p);
+          return true;
+        case 3:
+          deviceSpec.port=atoi(p);
+          return true;
+          break;
+        default:
+          return false; // Too many parameters.
+        }
+    }
 }
 
 void sioNetwork::open()
