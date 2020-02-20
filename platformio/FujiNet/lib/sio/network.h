8209c70d4dfcda570145f6b803c23daddd8d4b4d
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-02-20 00:05:35 -0600

    Add devicespec_parse.

--- platformio/FujiNet/lib/sio/network.h
+++ platformio/FujiNet/lib/sio/network.h
@@ -20,29 +20,46 @@
 #define NUM_DEVICES 8
 #define TCP_INPUT_BUFFER_SIZE 256
 #define TCP_OUTPUT_BUFFER_SIZE 256
+#define DEVICESPEC_SIZE 256
 
 
 class sioNetwork : public sioDevice
 {
+
+private:
     WiFiClient client;
     WiFiServer* server;
     HTTPClient* http_client;
+    char* tmp;
 
     unsigned char eol_mode;
     bool eol_skip_byte;
     unsigned char tcp_input_buffer[TCP_INPUT_BUFFER_SIZE];
     unsigned char tcp_input_buffer_len;
     unsigned char tcp_output_buffer[TCP_OUTPUT_BUFFER_SIZE];
-    unsigned char tcp_output_buffer_len[NUM_DEVICES];
+    unsigned char tcp_output_buffer_len;
+    union 
+    {
+        struct
+        {
+            char device[4];
+            char protocol[16];
+            char path[234];
+            unsigned short port;
+        };
+        char rawData[DEVICESPEC_SIZE];
+    } deviceSpec;
 
-    void set_eol_mode();
-    void clear_input_buffer();
-    void clear_output_buffer();
-    void fill_tcp_output_buffer(byte req_len);
+public:
+    void open();
+    void close();
+    void read();
+    void write();
+    void status();
 
-    void tcp_open();
-    void tcp_close();
-    void tcp_read();
+
+    bool parse_deviceSpec();
+
 };
 
 #endif /* NETWORK_H */
