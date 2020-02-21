67c21c4beb845752aa3fc6572784bf0cc2799ae5
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-02-20 23:24:54 -0600

    fleshing it out a bit more...

---
+++ platformio/FujiNet/lib/sio/networkDeviceSpec.h
@@ -0,0 +1,27 @@
+class networkDeviceSpec
+{
+public:
+
+    char device[4];         // Device Name/Number: N1:
+    char protocol[16];      // Protocol: TCP
+    char path[234];         // Path: FOO.COM
+    unsigned short port;    // Port: 2000
+    bool isValid;           // is devicespec valid?
+
+    /**
+     * ctor to create devicespec from string
+     */
+    networkDeviceSpec(char* s) { parse(s); }
+
+    /**
+     * Parse input string: N1:TCP:FOO.COM:2000 or N1:TCP:2000 
+     * s - Input string
+     * Returns: boolean for valid devicespec.
+     */
+    bool parse(char* s);
+
+    /**
+     * Return devicespec as string "N1:TCP:FOO.COM:2000"
+     */
+    const char* toChar();
+};
