094d585f91b9f37f36147d6b28c0450f0bd328f3
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-02-22 12:23:48 -0600

    further fleshing out.

--- platformio/FujiNet/lib/sio/networkProtocolTCP.cpp
+++ platformio/FujiNet/lib/sio/networkProtocolTCP.cpp
@@ -16,40 +16,57 @@ networkProtocolTCP::~networkProtocolTCP()
 bool networkProtocolTCP::open(networkDeviceSpec *spec)
 {
     bool ret;
-    
+
     if (spec->path[0] == 0x00)
         server = new WiFiServer(spec->port);
     else
-        client.connect(spec->path,spec->port);
-    
+        client.connect(spec->path, spec->port);
+
     if (client.connected() || (server != NULL))
         ret = true;
     else
         ret = false;
-    
+
     return ret;
 }
 
 bool networkProtocolTCP::close()
 {
-    if (server==NULL)
+    if (server == NULL)
         client.stop();
     else
         server->stop();
 
-    return true;    
+    return true;
 }
 
 bool networkProtocolTCP::read(byte *rx_buf, unsigned short len)
 {
-    return (client.readBytes(rx_buf,len)==len);
+    return (client.readBytes(rx_buf, len) == len);
 }
 
 bool networkProtocolTCP::write(byte *tx_buf, unsigned short len)
 {
-    return (client.write((char *)&tx_buf),len);
+    return (client.write((char *)&tx_buf), len);
 }
 
 bool networkProtocolTCP::status(byte *status_buf)
 {
+    memset(status_buf, 0x00, 4);
+    if (client.connected())
+    {
+        status_buf[0] = client.available() & 0xFF;
+        status_buf[1] = client.available() >> 8;
+        status_buf[3] = 1;
+    }
+    if (server != NULL)
+        status_buf[2] = server->available();
+}
+
+/**
+ * Still deciding on how to handle special...
+ */
+bool networkProtocolTCP::special(byte* sp_buf, unsigned short len)
+{
+
 }
