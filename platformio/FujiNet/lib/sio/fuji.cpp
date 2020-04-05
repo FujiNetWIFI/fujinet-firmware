01efa126cc81fa8e7c97f48f2abfbf30d1eeddef
jeffpiep <jeff.piepmeier+github@gmail.com>
2020-04-05 14:56:17 -0400

    Update fuji.cpp

--- platformio/FujiNet/lib/sio/fuji.cpp
+++ platformio/FujiNet/lib/sio/fuji.cpp
@@ -255,6 +255,7 @@ void sioFuji::sio_tnfs_read_directory_entry()
     //byte ret = tnfs_readdir(hostSlot);
     File f = dir[hostSlot].openNextFile();
     int l = 0;
+    
     if (!f)
         current_entry[0] = 0x7F; // end of dir
     else
