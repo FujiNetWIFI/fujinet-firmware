ad986b8c82c21db6e641ebcbb69bb905ebd02e0c
tschak909 <thom.cherryhomes@gmail.com>
2020-04-10 17:13:45 -0500

    udp formatting pass. implement set destination.

--- platformio/FujiNet/lib/sio/networkProtocolUDP.cpp
+++ platformio/FujiNet/lib/sio/networkProtocolUDP.cpp
@@ -11,13 +11,13 @@ networkProtocolUDP::~networkProtocolUDP()
 {
 #ifdef DEBUG
     Debug_printf("networkProtocolUDP::dtor\n");
-#endif    
+#endif
 }
 
 bool networkProtocolUDP::open(networkDeviceSpec *spec)
 {
 #ifdef DEBUG
-    Debug_printf("networkProtocolUDP::OPEN %s \n",spec->toChar());
+    Debug_printf("networkProtocolUDP::OPEN %s \n", spec->toChar());
 #endif
     return udp.begin(spec->port);
 }
@@ -31,15 +31,15 @@ bool networkProtocolUDP::close()
 bool networkProtocolUDP::read(byte *rx_buf, unsigned short len)
 {
 #ifdef DEBUG
-    Debug_printf("networkProtocolUDP::read %d bytes\n",len);
-#endif    
+    Debug_printf("networkProtocolUDP::read %d bytes\n", len);
+#endif
     return (udp.read(rx_buf, len) == len);
 }
 
 bool networkProtocolUDP::write(byte *tx_buf, unsigned short len)
 {
 #ifdef DEBUG
-    Debug_printf("networkProtocolUDP::write %d bytes\n",len);
+    Debug_printf("networkProtocolUDP::write %d bytes\n", len);
 #endif
     udp.beginPacket(dest, port);
     int l = udp.write(tx_buf, len);
@@ -56,14 +56,41 @@ bool networkProtocolUDP::status(byte *status_buf)
     Debug_printf("networkProtocolUDP::status\n");
 #endif
     unsigned short len = udp.parsePacket();
+
+    // Set destination automatically to remote address.
+    strcpy(dest, udp.remoteIP().toString().c_str());
+    port = udp.remotePort();
+
     status_buf[0] = len & 0xFF;
     status_buf[1] = len >> 8;
     status_buf[2] = status_buf[3] = 0;
     return true;
 }
 
-bool networkProtocolUDP::special(byte* sp_buf, unsigned short len, cmdFrame_t* cmdFrame)
+bool networkProtocolUDP::special_supported_80_command(unsigned char comnd)
 {
-    // come back here and fix this.
-    return false;
+    if (comnd == 'D') // Set DEST address
+        return true;
+    else
+        return false;
+}
+
+bool networkProtocolUDP::special_set_destination(byte *sp_buf, unsigned short len, unsigned short new_port)
+{
+    strncpy(dest, (const char *)sp_buf, sizeof(dest));
+    port = new_port;
+    return false; // no error.
+}
+
+bool networkProtocolUDP::special(byte *sp_buf, unsigned short len, cmdFrame_t *cmdFrame)
+{
+    bool err = false;
+
+    switch (cmdFrame->comnd)
+    {
+    case 'D':
+        err = special_set_destination(sp_buf, len, (cmdFrame->aux2 * 256 + cmdFrame->aux1));
+        break;
+    }
+    return err;
 }
