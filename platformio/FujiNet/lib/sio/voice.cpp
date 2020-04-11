e57f39fe15e4a95c1f43f8d04c13c24338e411c5
jeffpiep <jeff.piepmeier@gmail.com>
2020-04-11 13:38:45 -0400

    complete sioVoice for POC, builds,  needs testing

--- platformio/FujiNet/lib/sio/voice.cpp
+++ platformio/FujiNet/lib/sio/voice.cpp
@@ -2,6 +2,7 @@
 
 void sioVoice::sio_write()
 {
+    // act like a printer for POC
     byte n = 40;
     byte ck;
 
@@ -20,4 +21,43 @@ void sioVoice::sio_write()
     }
 }
 
-void sio_sam(){};
+// Status
+void sioVoice::sio_status()
+{
+    // act like a printer for POC
+    byte status[4];
+
+    status[0] = 0;
+    status[1] = lastAux1;
+    status[2] = 5;
+    status[3] = 0;
+
+    sio_to_computer(status, sizeof(status), false);
+}
+
+void sioVoice::sio_process()
+{
+    // act like a printer for POC
+    switch (cmdFrame.comnd)
+    {
+    case 'P': // 0x50
+    case 'W': // 0x57
+        sio_ack();
+        sio_write();
+        lastAux1 = cmdFrame.aux1;
+        break;
+    case 'S': // 0x53
+        sio_ack();
+        sio_status();
+        break;
+    default:
+        sio_nak();
+    }
+}
+
+void sio_sam()
+{
+    int n = 2;
+    char *a[5] = {"sam", "I am Sam."};
+    sam(n, a);
+};
