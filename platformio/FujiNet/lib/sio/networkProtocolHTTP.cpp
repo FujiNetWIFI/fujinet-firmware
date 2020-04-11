c94a3f64873b53f93060e4b5e9f79b71fd603b04
tschak909 <thom.cherryhomes@gmail.com>
2020-04-11 17:30:27 -0500

    HTTP protocol stub.

--- platformio/FujiNet/lib/sio/networkProtocolHTTP.cpp
+++ platformio/FujiNet/lib/sio/networkProtocolHTTP.cpp
@@ -10,3 +10,33 @@ networkProtocolHTTP::~networkProtocolHTTP()
 {
     
 }
+
+bool networkProtocolHTTP::open(networkDeviceSpec *spec)
+{
+    return false;
+}
+
+bool networkProtocolHTTP::close()
+{
+    return false;
+}
+
+bool networkProtocolHTTP::read(byte *rx_buf, unsigned short len)
+{
+    return false;
+}
+
+bool networkProtocolHTTP::write(byte *tx_buf, unsigned short len)
+{
+    return false;
+}
+
+bool networkProtocolHTTP::status(byte *status_buf)
+{
+    return false;
+}
+
+bool networkProtocolHTTP::special(byte *sp_buf, unsigned short len, cmdFrame_t *cmdFrame)
+{
+    return false;
+}
