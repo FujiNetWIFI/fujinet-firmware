572ae1041b391e18a0a0f087b7deae5091a60ad1
tschak909 <thom.cherryhomes@gmail.com>
2020-04-12 13:34:42 -0500

    more http work. add ability to get/set headers.

--- platformio/FujiNet/lib/sio/networkProtocolHTTP.cpp
+++ platformio/FujiNet/lib/sio/networkProtocolHTTP.cpp
@@ -14,6 +14,8 @@ bool networkProtocolHTTP::startConnection(byte *buf, unsigned short len)
     {
     case GET:
         resultCode = client.GET();
+        headerIndex = 0;
+        numHeaders = client.headers();
         break;
     case POST:
         resultCode = client.POST(buf, len);
@@ -60,25 +62,56 @@ bool networkProtocolHTTP::read(byte *rx_buf, unsigned short len)
             return false;
     }
 
-    c = client.getStreamPtr();
+    if (headers)
+    {
+        if (headerIndex < numHeaders)
+        {
+            strcpy((char *)rx_buf, client.header(headerIndex++).c_str());
+            return true;
+        }
+        else
+            return false;
+    }
+    else
+    {
+        c = client.getStreamPtr();
 
-    if (c == nullptr)
-        return false;
+        if (c == nullptr)
+            return false;
 
-    if (c->readBytes(rx_buf, len) != len)
-        return false;
+        if (c->readBytes(rx_buf, len) != len)
+            return false;
 
-    return true;
+        return true;
+    }
 }
 
 bool networkProtocolHTTP::write(byte *tx_buf, unsigned short len)
 {
     WiFiClient *c;
 
-    if (!requestStarted)
+    if (headers)
     {
-        if (!startConnection(tx_buf, len))
-            return false;
+        String headerKey;
+        String headerValue;
+        char tmpKey[256];
+        char tmpValue[256];
+        char *p = strtok((char *)tx_buf, ":");
+
+        strcpy(tmpKey, p);
+        p = strtok(NULL, "");
+        strcpy(tmpValue, p);
+        headerKey = String(tmpKey);
+        headerValue = String(tmpValue);
+        client.addHeader(headerKey, headerValue);
+    }
+    else
+    {
+        if (!requestStarted)
+        {
+            if (!startConnection(tx_buf, len))
+                return false;
+        }
     }
 
     return true;
@@ -89,29 +122,67 @@ bool networkProtocolHTTP::status(byte *status_buf)
     WiFiClient *c;
     int a; // available bytes
 
+    status_buf[0] = status_buf[1] = status_buf[2] = status_buf[3] = 0;
+
     if (!requestStarted)
     {
-        if (!startConnection(status_buf,4))
+        if (!startConnection(status_buf, 4))
             return false;
     }
 
-    c = client.getStreamPtr();
+    if (headers)
+    {
+        if (headerIndex < numHeaders)
+        {
+            status_buf[0] = client.header(headerIndex).length() & 0xFF;
+        }
+    }
+    else
+    {
+        c = client.getStreamPtr();
 
-    if (c == nullptr)
+        if (c == nullptr)
+            return false;
+
+        // Limit to reporting max of 65535 bytes available.
+        a = (c->available() > 65535 ? 65535 : c->available());
+
+        status_buf[0] = a & 0xFF;
+        status_buf[1] = a >> 8;
+        status_buf[2] = resultCode & 0xFF;
+        status_buf[3] = resultCode >> 8;
+
+        return true;
+    }
+}
+
+bool networkProtocolHTTP::special_supported_00_command(unsigned char comnd)
+{
+    switch (comnd)
+    {
+    case 'H': // toggle headers
+        return true;
+    default:
         return false;
+    }
 
-    // Limit to reporting max of 65535 bytes available.
-    a = (c->available() > 65535 ? 65535 : c->available());
+    return false;
+}
 
-    status_buf[0] = a & 0xFF;
-    status_buf[1] = a >> 8;
-    status_buf[2] = resultCode & 0xFF;
-    status_buf[3] = resultCode >> 8;
-
-    return true;
+void networkProtocolHTTP::special_header_toggle(unsigned char aux1)
+{
+    headers = (aux1 == 1 ? true : false);
 }
 
 bool networkProtocolHTTP::special(byte *sp_buf, unsigned short len, cmdFrame_t *cmdFrame)
 {
+    switch (cmdFrame->comnd)
+    {
+    case 'H': // toggle headers
+        special_header_toggle(cmdFrame->aux1);
+        return true;
+    default:
+        return false;
+    }
     return false;
 }
