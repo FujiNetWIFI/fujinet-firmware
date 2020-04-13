928e9ae49e54264d7f7479fd8a3077251e8428b6
jeffpiep <jeff.piepmeier+github@gmail.com>
2020-04-12 22:19:41 -0400

    remove pdf_fonts from base class

--- platformio/FujiNet/lib/printer-emulator/pdf_printer.h
+++ platformio/FujiNet/lib/printer-emulator/pdf_printer.h
@@ -7,7 +7,7 @@
 
 struct pdfFont_t
 {
-/* 
+  /* 
   7 0 obj
   << 
     /Type /Font
@@ -36,72 +36,72 @@ struct pdfFont_t
   >>
   endobj
 */
- std::string subtype;
- std::string basefont;
- float width; // uniform spacing for now, todo: proportional
- float ascent;
- float capheight;
- float descent;
- byte flags;
- float bbox[4];
- float stemv;
- float xheight;
- byte ffnum;
- std::string ffname;
+  std::string subtype;
+  std::string basefont;
+  float width; // uniform spacing for now, todo: proportional
+  float ascent;
+  float capheight;
+  float descent;
+  byte flags;
+  float bbox[4];
+  float stemv;
+  float xheight;
+  byte ffnum;
+  std::string ffname;
 };
 
-class pdfPrinter: public printer_emu
+class pdfPrinter : public printer_emu
 {
 protected:
-    // PDF THINGS
-    float pageWidth;
-    float pageHeight;
-    float leftMargin;
-    float bottomMargin;
-    float printWidth;
-    float lineHeight;
-    float charWidth;
-    int fontNumber;
-    float fontSize;
+  // PDF THINGS
+  float pageWidth;
+  float pageHeight;
+  float leftMargin;
+  float bottomMargin;
+  float printWidth;
+  float lineHeight;
+  float charWidth;
+  int fontNumber;
+  float fontSize;
 
-    float pdf_X = 0.;   // across the page - columns in pts
-    bool BOLflag = true;
-    float pdf_Y = 0.; // down the page - lines in pts
-    bool TOPflag = true;
-    bool textMode = true;
-    int pageObjects[256];
-    int pdf_pageCounter = 0.;
-    size_t objLocations[256]; // reference table storage
-    int pdf_objCtr = 0;       // count the objects
+  float pdf_X = 0.; // across the page - columns in pts
+  bool BOLflag = true;
+  float pdf_Y = 0.; // down the page - lines in pts
+  bool TOPflag = true;
+  bool textMode = true;
+  int pageObjects[256];
+  int pdf_pageCounter = 0.;
+  size_t objLocations[256]; // reference table storage
+  int pdf_objCtr = 0;       // count the objects
 
-    virtual void pdf_handle_char(byte c) = 0;
-    virtual void pdf_fonts() = 0;
-    void pdf_header();
-    void pdf_add_fonts(pdfFont_t* fonts[], int n);
-    void pdf_new_page();
-    void pdf_begin_text(float Y);
-    void pdf_new_line();
-    void pdf_end_line();
-    void pdf_end_page();
-    void pdf_xref();
+  virtual void pdf_handle_char(byte c) = 0;
 
-    size_t idx_stream_length; // file location of stream length indictor
-    size_t idx_stream_start;  // file location of start of stream
-    size_t idx_stream_stop;   // file location of end of stream
+  void pdf_header();
+  void pdf_add_fonts(pdfFont_t *fonts[], int n);
+  void pdf_new_page();
+  void pdf_begin_text(float Y);
+  void pdf_new_line();
+  void pdf_end_line();
+  void pdf_end_page();
+  void pdf_xref();
+
+  size_t idx_stream_length; // file location of stream length indictor
+  size_t idx_stream_start;  // file location of start of stream
+  size_t idx_stream_stop;   // file location of end of stream
 
 public:
-    virtual void initPrinter(File *f) = 0;
-    virtual void pageEject();
-    virtual bool process(const byte* buf, byte n);
+  virtual void initPrinter(File *f) = 0;
+  virtual void pageEject();
+  virtual bool process(const byte *buf, byte n);
 };
-
 
 class asciiPrinter : public pdfPrinter
 {
 protected:
-    virtual void pdf_fonts();
-    virtual void pdf_handle_char(byte c);
+  virtual void pdf_handle_char(byte c);
+
+  void pdf_fonts();
 
 public:
-    void initPrinter(File *f);
+  void initPrinter(File *f);
 };
