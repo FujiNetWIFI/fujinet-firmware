b3643955add7a3f2205eec4431daa8430a553b1a
mozzwald <mozzwald@mozzwald.com>
2020-04-15 16:20:08 -0500

    Add 'other' button and LED3 for next board revision

--- platformio/FujiNet/lib/hardware/keys.cpp
+++ platformio/FujiNet/lib/hardware/keys.cpp
@@ -37,3 +37,41 @@ eKeyStatus KeyManager::getBootKeyStatus()
     }
     return result;
 }
+
+eKeyStatus KeyManager::getOtherKeyStatus()
+{
+    eKeyStatus result = eKeyStatus::RELEASED;
+
+    if (digitalRead(PIN_OTHER_BUTTON) == LOW)
+    {
+        if (oButtonActive == false)
+        {
+            oButtonActive = true;
+            oButtonTimer = millis();
+        }
+        if ((millis() - oButtonTimer > LONGPRESS_TIME) && (oLongPressActive == false))
+        {
+            oLongPressActive = true;
+            // long press detected
+            result = eKeyStatus::LONG_PRESSED;
+        }
+    }
+    else
+    {
+        if (oButtonActive == true)
+        {
+            if (oLongPressActive == true)
+            {
+                oLongPressActive = false;
+                // long press released
+            }
+            else
+            {
+                // short press released
+                result = eKeyStatus::SHORT_PRESSED;
+            }
+            oButtonActive = false;
+        }
+    }
+    return result;
+}
