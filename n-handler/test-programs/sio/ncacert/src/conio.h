39e49eda801abe601be306712952634f11c4816b
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-04-18 18:57:27 -0500

    add ncacert toggle program

---
+++ n-handler/test-programs/sio/ncacert/src/conio.h
@@ -0,0 +1,13 @@
+/**
+ * conio
+ */
+
+#ifndef CONIO_H
+#define CONIO_H
+
+void print(const char* c);
+void printl(const char* c, unsigned short l);
+void printc(char* c);
+void get_line(char* buf, unsigned char len);
+
+#endif /* CONIO_H */
