02ea4d20c680847b35b3ad0ca077fb25c17054c3
Jeff P <jeff.piepmeier@gmail.com>
2020-05-03 20:48:13 -0400

    1025 international chars

--- platformio/FujiNet/lib/printer-emulator/atari_1025.h
+++ platformio/FujiNet/lib/printer-emulator/atari_1025.h
@@ -4,12 +4,12 @@
 
 #include "pdf_printer.h"
 
-// to do: double check this against 1020 and 1027 test output and manuals
-const byte intlchar[28] = {225, 249, 209, 201, 231, 244, 242, 236, 163, 239, 252, 228, 214, 250, 243, 246, 220, 226, 251, 238, 233, 232, 241, 234, 229, 224, 197, 27};
-
 class atari1025 : public pdfPrinter
 {
 protected:
+    // to do: double check this against 1020 and 1027 test output and manuals
+    const byte intlchar[27] = {225, 249, 209, 201, 231, 244, 242, 236, 163, 239, 252, 228, 214, 250, 243, 246, 220, 226, 251, 238, 233, 232, 241, 234, 229, 224, 197};
+
     bool intlFlag = false;
     bool shortFlag = false;
     bool escMode = false;
@@ -19,7 +19,7 @@ protected:
 public:
     void initPrinter(FS *filesystem);
 
-    const char * modelname() { return "Atari 1025"; };
+    const char *modelname() { return "Atari 1025"; };
 
 protected:
     /*
