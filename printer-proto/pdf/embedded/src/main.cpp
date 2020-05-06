bd1a9762dc06712ef3a9633abccca2c95c2f487a
jeffpiep <jeff.piepmeier+github@gmail.com>
2020-05-05 22:11:58 -0400

    more on font embedding

--- printer-proto/pdf/embedded/src/main.cpp
+++ printer-proto/pdf/embedded/src/main.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 
 using namespace std;
 
-ifstream f;
+#define MAXFONTS 100
 
-void findFontList()
+ifstream f;
+ofstream g;
+ofstream h;
+
+int fontNumber[MAXFONTS];
+int fontObject[MAXFONTS];
+
+int findFontList()
 {
-  bool found;
+  bool found = false;
   string line;
-  //int count = 0;
+  int count = 0;
+
   do
   {
     line.clear();
     getline(f, line);
     cout << line << ": ";
     cout << line.find("/Font") << "\n";
-    // count++;
-  } while (line.find("/Font") == string::npos); // count < 40 && 
+  } while (line.find("/Font") == string::npos); // count < 40 &&
   line.clear();
   do
   {
@@ -27,23 +35,114 @@ void findFontList()
     char buffer[10];
     getline(f, line);
     size_t offset = line.find("/F");
+    printf("%u %u\n", offset, string::npos);
     // if offset not found then done
-    size_t ll = line.copy(buffer,7,offset+2);
-    sscanf(buffer,"%d %d",&f1,&f2);
-    printf("%d %d\n",f1,f2);
-    //cout << f1 << f2 << "\n";
-  } while (true);
+    if (offset < 10)
+    {
+      size_t ll = line.copy(buffer, 7, offset + 2);
+      sscanf(buffer, "%d %d", &f1, &f2);
+      printf("%d %d\n", f1, f2);
+      fontNumber[count] = f1;
+      fontObject[count] = f2;
+      count++;
+    }
+    else
+    {
+      found = true;
+    }
+
+  } while (!found);
   // need to return the font and object numbers we found (vectors?)
+  printf("found %d fonts\n", count);
+  return count;
+}
+
+void getFont(int i)
+{
+  // continue searching in file
+  // first find object
+  bool found = false;
+  string line;
+  char objstr[10];
+
+  sprintf(objstr, "%d 0 obj", fontObject[i]);
+
+  do
+  {
+    line.clear();
+    getline(f, line);
+    //cout << line << ": ";
+    //cout << line.find(objstr) << "\n";
+    // count++;
+  } while (line.find(objstr) == string::npos); // count < 40 &&
+  cout << line;
+  cout << " object found\n";
+
+  // now extract info
+/*
+  std::string subtype;
+  std::string basefont;
+  uint16_t width[256]; // uniform spacing for now, todo: proportional
+  uint16_t numwidth;
+  float ascent;
+  float capheight;
+  float descent;
+  byte flags;
+  float bbox[4];
+  float stemv;
+  float xheight;
+  byte ffnum;
+  std::string ffname;
+*/
+
+ /*  const pdfFont_t F1 = {
+      /Type /Font
+    /Subtype /Type1
+    /FontDescriptor 8 0 R
+    /BaseFont /Atari-820-Normal
+    /FirstChar 0
+    /LastChar 255
+    /Widths 10 0 R
+    /Encoding /WinAnsiEncoding
+    /Type /FontDescriptor
+    /FontName /Atari-820-Normal
+    /Ascent 1000
+    /CapHeight 1000
+    /Descent 0
+    /Flags 33
+    /FontBBox [0 0 433 700]
+    /ItalicAngle 0
+    /StemV 87
+    /XHeight 714
+    /FontFile3 9 0 R
+  
+        "Type1",            //F1->subtype =
+        "Atari-820-Normal", //F1->basefont =
+        {500},              //  F1->width[0] =
+        1,                  // F1->numwidth
+        1000,               // F1->ascent =
+        1000,               // F1->capheight =
+        0,                  // F1->descent =
+        33,                 // F1->flags =
+        {0, 0, 433, 700},
+        87,         // F1->stemv =
+        714,        // F1->xheight =
+        3,          // F1->ffnum =
+        "/a820norm" // F1->ffname =
+    };
+    */
+   h << "const pdfFont_t F" << i << " = {" << endl;
+   
 }
 
 int main(int argc, char **argv)
 {
-  if (argc != 2)
+  if (argc != 3)
   {
-    cout << "no filename specified\n";
+    cout << "no filename or printer specified\n";
     return -1;
   }
-  cout << "processing file: " << argv[1] << "\n";
+  cout << "processing file: " << argv[1] << " for printer " << argv[2] << "\n";
 
   f.open(argv[1], ios::in | ios::binary);
   if (!f)
@@ -52,7 +151,19 @@ int main(int argc, char **argv)
     return -1;
   }
 
-  findFontList();
+  // create file names
+  char gname[20]="f_";
+  char hname[20]="h_";
+  strcpy(&gname[2],argv[2]);
+  strcpy(&hname[2],argv[2]);
+  g.open(gname,ios::out | ios::binary);
+  h.open(hname,ios::out);
+
+  int numFonts = findFontList();
+  for (int i = 0; i < numFonts; i++)
+  {
+    getFont(i);
+  }
 
   return 0;
 }
