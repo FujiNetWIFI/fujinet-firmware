fb17a9959424c76467e61f7bddcddd399d51ede2
jeffpiep <jeff.piepmeier+github@gmail.com>
2020-05-06 22:53:10 -0400

    copy font objects to file

--- printer-proto/pdf/embedded/src/main.cpp
+++ printer-proto/pdf/embedded/src/main.cpp
@@ -110,7 +110,7 @@ int getFont(int i)
   float xheight;
   byte ffnum;
   std::string ffname;
-*/
+  */
 
   /*  const pdfFont_t F1 = {
       /Type /Font
@@ -170,6 +170,137 @@ int getFont(int i)
     << "    // " << param << "\n";
 }
 
+int copyFont(int i)
+{
+  // continue searching in file
+  // first find object
+  bool found = false;
+  string line;
+  string argstr;
+  string param;
+  char objstr[10];
+  char buffer[40];
+  size_t offset;
+  size_t st;
+  size_t sp;
+  int tracking = 0;
+
+  sprintf(objstr, "%d 0 obj", fontObject[i]);
+
+  do
+  {
+    line.clear();
+    getline(f, line);
+  } while (line.find(objstr) == string::npos);
+  cout << line;
+  cout << " object found\n";
+
+  // start copy
+  g << "%d 0 obj\n";
+  getline(f, line); // <<
+  g << line << "\n";
+  getline(f, line); //  /Type /Font
+  g << line << "\n";
+  getline(f, line); // /Subtype /Type1
+  g << line << "\n";
+
+  getline(f, line); // /FontDescriptor 7 0 R
+  offset = line.find_first_of("0123456789");
+  g << line.substr(0, offset) << "%d 0 obj\n";
+
+  getline(f, line); //  /BaseFont /Atari-1025-Normal
+  g << line << "\n";
+  getline(f, line); //  /FirstChar 0
+  g << line << "\n";
+  getline(f, line); //  /LastChar 255
+  g << line << "\n";
+
+  getline(f, line); // /Widths 9 0 R
+  offset = line.find_first_of("0123456789");
+  g << line.substr(0, offset) << "%d 0 obj\n";
+
+  getline(f, line); //  /Encoding /WinAnsiEncoding
+  g << line << "\n";
+  getline(f, line); // >>
+  g << line << "\n";
+  getline(f, line); //endobj
+  g << line << "\n";
+
+  getline(f, line); // 7 0 obj
+  g << "%d 0 obj\n";
+  getline(f, line); // <<
+  g << line << "\n";
+  getline(f, line); // /Type /FontDescriptor
+  g << line << "\n";
+  getline(f, line); // /FontName /Atari-1025-Normal
+  g << line << "\n";
+  getline(f, line); // /Ascent 1000
+  g << line << "\n";
+  getline(f, line); // /CapHeight 1000
+  g << line << "\n";
+  getline(f, line); // /Descent 0
+  g << line << "\n";
+  getline(f, line); // /Flags 33
+  g << line << "\n";
+  getline(f, line); // /FontBBox [0 1 499 699]
+  g << line << "\n";
+  getline(f, line); // /ItalicAngle 0
+  g << line << "\n";
+  getline(f, line); // /StemV 87
+  g << line << "\n";
+  getline(f, line); // /XHeight 500
+  g << line << "\n";
+
+  getline(f, line);                                // /FontFile3 8 0 R
+  offset = line.find_first_of("0123456789");       // always font file 3 because of OTF
+  g << line.substr(0, offset + 2) << "%d 0 obj\n"; // always font file 3 because of OTF
+
+  getline(f, line); // >>
+  g << line << "\n";
+  getline(f, line); // endobj
+  g << line << "\n";
+
+  getline(f, line); // 8 0 obj
+  g << "%d 0 obj\n";
+  getline(f, line); // <<
+  g << line << "\n";
+  getline(f, line); // /Length1 31137
+  g << line << "\n";
+  getline(f, line); // /Subtype /Type1C
+  g << line << "\n";
+  getline(f, line); // /Filter /FlateDecode
+  g << line << "\n";
+
+  getline(f, line); // /Length 5592
+  // need to get the length number to copy the deflated fontfile
+  offset = line.find_first_of("0123456789");
+  cout << line.substr(offset) << "\n";
+  size_t len;
+  sscanf(line.substr(offset).c_str(), "%d", &len);
+  printf("%d\n",len);
+  g << line << "\n";
+
+  getline(f, line); // >>
+  g << line << "\n";
+  getline(f, line); // stream
+  g << line << "\n";
+  
+  for (int i = 0; i < len; i++)
+    g.put(f.get());
+  // copy deflated font file
+
+  getline(f, line); // endstream
+  g << line << "\n";
+  getline(f, line); // endobj
+  g << line << "\n";
+  getline(f, line); // 9 0 obj
+  g << "%d 0 obj\n";
+  getline(f, line); // [ 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 600 ]
+  g << line << "\n";
+  getline(f, line); // endobj
+  g << line << "\n";
+}
+
 int main(int argc, char **argv)
 {
   if (argc != 3)
@@ -188,16 +319,14 @@ int main(int argc, char **argv)
 
   // create file names
   char gname[20] = "f_";
-  char hname[20] = "h_";
   strcpy(&gname[2], argv[2]);
-  strcpy(&hname[2], argv[2]);
   g.open(gname, ios::out | ios::binary);
-  h.open(hname, ios::out);
 
   int numFonts = findFontList();
   for (int i = 0; i < numFonts; i++)
   {
-    getFont(i);
+    // getFont(i);
+    copyFont(i);
   }
 
   return 0;
