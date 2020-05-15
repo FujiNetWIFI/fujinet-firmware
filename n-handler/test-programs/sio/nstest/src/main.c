1b69d37d518f75470ddf8fc55b5ba5abcf55c0fa
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-05-15 16:03:29 -0500

    update test programs.

--- n-handler/test-programs/sio/nstest/src/main.c
+++ n-handler/test-programs/sio/nstest/src/main.c
@@ -1,7 +1,7 @@
 /**
  * Network Testing tools
  *
- * nclose - open a network connection
+ * nstest - open a network connection
  *
  * Author: Thomas Cherryhomes
  *  <thom.cherryhomes@gmail.com>
@@ -18,17 +18,23 @@
 #include "conio.h"
 #include "err.h"
 
-void nclose(void)
+unsigned char buf[256]="N:TCP://TMA-1:2000/\x9b";
+unsigned char d0[4], d1[4], d2[4], d3[4];
+unsigned char trip=0;
+
+extern void prcd(void);
+
+void nsopen(void)
 {
   OS.dcb.ddevic=0x71;
   OS.dcb.dunit=1;
-  OS.dcb.dcomnd='C';
-  OS.dcb.dstats=0x00;
-  OS.dcb.dbuf=NULL;
+  OS.dcb.dcomnd='O';
+  OS.dcb.dstats=0x80;
+  OS.dcb.dbuf=&buf;
   OS.dcb.dtimlo=0x0f;
-  OS.dcb.dbyt=0;
-  OS.dcb.daux1=0;
-  OS.dcb.daux2=0;
+  OS.dcb.dbyt=256;
+  OS.dcb.daux1=12;
+  OS.dcb.daux2=2;
   siov();
 
   if (OS.dcb.dstats!=1)
@@ -36,15 +42,61 @@ void nclose(void)
       err_sio();
       exit(OS.dcb.dstats);
     }
-  else
+}
+
+void nstest(void)
+{
+  if (trip==1)
     {
-      print("CLOSED\x9b");
+      OS.dcb.ddevic=0x71;
+      OS.dcb.dunit=1;
+      OS.dcb.dcomnd='S';
+      OS.dcb.dstats=0x40;
+      OS.dcb.dbuf=&OS.dvstat;
+      OS.dcb.dtimlo=0x0f;
+      OS.dcb.dbyt=4;
+      OS.dcb.daux1=0;
+      OS.dcb.daux2=0;
+      siov();
+      
+      if (OS.dcb.dstats!=1)
+	{
+	  err_sio();
+	  exit(OS.dcb.dstats);
+	}
+      else
+	{
+	  itoa(OS.dvstat[0],d0,16);
+	  itoa(OS.dvstat[1],d1,16);
+	  itoa(OS.dvstat[2],d2,16);
+	  itoa(OS.dvstat[3],d3,16);
+	  print(d0);
+	  print(" ");
+	  print(d1);
+	  print(" ");
+	  print(d2);
+	  print(" ");
+	  print(d3);
+      print("\x9b");
+	}
+      trip=0;
     }
 }
 
 void main(void)
 {
   OS.lmargn=2;
+  OS.ch=0xFF;
+  OS.vprced=prcd;
   
-  nclose();
+  nsopen();
+
+  PIA.pactl |= 1;
+  
+  while(OS.ch==0xFF)
+    {
+      nstest();
+    }
+
+  PIA.pactl |= 0;
 }
