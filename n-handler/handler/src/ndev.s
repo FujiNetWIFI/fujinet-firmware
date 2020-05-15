6ce9d5a0e4493c61266422ce38151262a7cd46a7
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-05-15 16:28:16 -0500

    housecleaning.

---
+++ n-handler/handler/src/ndev.s
@@ -0,0 +1,9 @@
+	;; N: Device Handler
+	;; Compile with ATASM
+
+	;; Author: Thomas Cherryhomes
+	;;   <thom.cherryhomes@gmail.com>
+
+	*=$2300
+
+	RTS
--- n-handler/prototype/src/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define MAX_DEVICES 4
-
-#endif /* CONFIG_H */
