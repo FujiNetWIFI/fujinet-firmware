9874c729511fd08148fb43fd860a7f2deca1f0d4
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-05-17 11:57:19 -0500

    argh

--- n-handler/handler/relocate.sh
+++ n-handler/handler/relocate.sh
@@ -3,6 +3,6 @@
 make RUNADDRESS=12288 OUTFILE=ndev0.com
 make RUNADDRESS=12304 OUTFILE=ndev1.com
 
-mv ndev0.com ndev1.com relwork-dest/
+mv ndev0.com ndev1.com relwork-dist/
 
 dir2atr -b Dos20 -S relwork.atr relwork-dist 
