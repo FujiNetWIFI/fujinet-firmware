99aa8b0e3558f7430f96043a251ab2c7314a513a
Jeff P <jeff.piepmeier@gmail.com>
2020-06-05 07:01:54 -0400

    def'd trivial pdf_clear_modes() in all pdfprinters

--- platformio/FujiNet/lib/printer-emulator/atari_822.h
+++ platformio/FujiNet/lib/printer-emulator/atari_822.h
@@ -8,7 +8,7 @@ class atari822 : public pdfPrinter
 {
 protected:
     int gfxNumber = 0;
-
+   virtual void pdf_clear_modes() override {};
     virtual void post_new_file() override;
     void pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2) override; // need a custom one to handle sideways printing
 
