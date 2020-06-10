516ac9fd0b71da889ee1c24554695df58f4a1213
Oscar Fowler <oscar@justified.com>
2020-06-09 19:09:27 -0700

    BluetoothSerial replacement

--- platformio/FujiNet/lib/hardware/bluetooth.cpp
+++ platformio/FujiNet/lib/hardware/bluetooth.cpp
@@ -1,55 +1,51 @@
+
 #include "../../include/debug.h"
-#include "bluetooth.h"
 #include "../sio/sio.h"
 #include "../sio/disk.h"
-#include "BluetoothSerial.h"
+#include "bluetooth.h"
+#include "fnBluetooth.h"
 
-BluetoothSerial SerialBT;
+fnBluetooth SerialBT;
+
+/*
+.begin
+.end
+.read
+.write
+*/
 
 void BluetoothManager::start()
 {
-#ifdef DEBUG
     Debug_println("START SIO2BT");
-#endif
     SerialBT.begin(BT_NAME);
-    mActive = true;
-    SIO.setBaudrate(mBTBaudrate);
+    _mActive = true;
+    SIO.setBaudrate(_mBTBaudrate);
 }
 
 void BluetoothManager::stop()
 {
-#ifdef DEBUG
     Debug_println("STOP SIO2BT");
-#endif
-    mActive = false;
+    _mActive = false;
     SIO.setBaudrate(eBTBaudrate::BT_STANDARD_BAUDRATE);
     SerialBT.end();
 }
 
 eBTBaudrate BluetoothManager::toggleBaudrate()
 {
-    if (mBTBaudrate == eBTBaudrate::BT_STANDARD_BAUDRATE)
+    if (_mBTBaudrate == eBTBaudrate::BT_STANDARD_BAUDRATE)
     {
-        mBTBaudrate = eBTBaudrate::BT_HISPEED_BAUDRATE;
+        _mBTBaudrate = eBTBaudrate::BT_HISPEED_BAUDRATE;
     }
     else
     {
-        mBTBaudrate = eBTBaudrate::BT_STANDARD_BAUDRATE;
+        _mBTBaudrate = eBTBaudrate::BT_STANDARD_BAUDRATE;
     }
-    SIO.setBaudrate(mBTBaudrate);
-    return mBTBaudrate;
+    SIO.setBaudrate(_mBTBaudrate);
+    return _mBTBaudrate;
 }
 
 void BluetoothManager::service()
 {
-    /*
-    if (SIO_UART.available()) {
-      SerialBT.write(SIO_UART.read());
-    }
-    if (SerialBT.available()) {
-      SIO_UART.write(SerialBT.read());
-    }
-    */
     if (fnUartSIO.available())
     {
         SerialBT.write(fnUartSIO.read());
