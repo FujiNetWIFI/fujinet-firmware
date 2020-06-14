59f7ba5d1e39f2c652e32459bb5ddb60e0f3eaff
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-06-14 11:59:07 -0500

    remove obj

--- config/obj/atari/crt0.d
+++
@@ -1,4 +0,0 @@
-obj/atari/crt0.o:	src/crt0.s c:\binn\cc65/asminc/zeropage.inc c:\binn\cc65/asminc/atari.inc c:\binn\cc65/asminc/atari_gtia.inc c:\binn\cc65/asminc/atari_pokey.inc c:\binn\cc65/asminc/atari_antic.inc
-
-src/crt0.s c:\binn\cc65/asminc/zeropage.inc c:\binn\cc65/asminc/atari.inc c:\binn\cc65/asminc/atari_gtia.inc c:\binn\cc65/asminc/atari_pokey.inc c:\binn\cc65/asminc/atari_antic.inc:
-
