719a9c7da70d00d464c5d3458b129a9f753a8e18
Oscar Fowler <oscar@justified.com>
2020-06-23 07:04:15 -0700

    Changes to versioning mechanism

--- platformio/FujiNet_idf/include/version.h
+++ platformio/FujiNet_idf/include/version.h
@@ -1,10 +1,16 @@
+/*
+ This file is automatically modified by the build_version.py script
+ to update the BUILD and BUILD_DATE values based on the
+ current Git commit ID and current UTC date/time.
 
-#ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "10"
-#endif
-#ifndef VERSION
-  #define VERSION "v0.1.10 - 2020-06-22 17:33:22.129086"
-#endif
-#ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.10"
-#endif
+ FN_VERSION_MAJOR and FN_VERSION_MINOR should be manually updated
+ as needed.
+*/
+
+#define FN_VERSION_MAJOR 0
+#define FN_VERSION_MINOR 1
+
+#define FN_VERSION_BUILD "27d05712"
+#define FN_VERSION_DATE "2020-06-23 14:03:13"
+
+#define FN_VERSION_FULL "0.1.27d05712"
