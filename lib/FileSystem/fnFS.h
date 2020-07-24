9c0d96385487c74e7dfdb4da051e312ee46f6b17
Oscar Fowler <oscar@justified.com>
2020-07-23 22:34:41 -0700

    fnFsSD now caches directory reads for sorting

--- lib/FileSystem/fnFS.h
+++ lib/FileSystem/fnFS.h
@@ -68,6 +68,7 @@ public:
 
     virtual bool rename(const char* pathFrom, const char* pathTo) = 0;
 
+    // By default, a directory should be sorted and special/hidden items should be filtered out
     virtual bool dir_open(const char *path) = 0;
     virtual fsdir_entry_t *dir_read() = 0;
     virtual void dir_close() = 0;
