9a17b99a897d77b8318e6867c8838069e05ae59f
Oscar Fowler <oscar@justified.com>
2020-08-13 19:47:52 -0700

    Case-insensitive pattern matches on SDFAT

--- lib/utils/utils.cpp
+++ lib/utils/utils.cpp
@@ -259,8 +259,7 @@ std::string util_ellipsize(std::string longString, int maxLength)
     return leftPart + "..." + rightPart;
 }
 
-// Function that matches input str with
-// given wildcard pattern
+// Function that matches input string against given wildcard pattern
 bool util_wildcard_match(const char *str, const char *pattern)
 {
     if(str == nullptr || pattern == nullptr)
@@ -269,19 +268,17 @@ bool util_wildcard_match(const char *str, const char *pattern)
     int m = strlen(pattern);
     int n = strlen(str);
 
-    // empty pattern can only match with
-    // empty string
+    // Empty pattern can only match with empty string
     if (m == 0)
         return (n == 0);
 
-    // lookup table for storing results of
-    // subproblems
+    // Lookup table for storing results of subproblems
     bool lookup[n + 1][m + 1];
 
-    // initailze lookup table to false
+    // Initailze lookup table to false
     memset(lookup, false, sizeof(lookup));
 
-    // empty pattern can match with empty string
+    // Empty pattern can match with empty string
     lookup[0][0] = true;
 
     // Only '*' can match with empty string
@@ -289,29 +286,28 @@ bool util_wildcard_match(const char *str, const char *pattern)
         if (pattern[j - 1] == '*')
             lookup[0][j] = lookup[0][j - 1];
 
-    // fill the table in bottom-up fashion
+    // Fill the table in bottom-up fashion
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
         {
-            // Two cases if we see a '*'
-            // a) We ignore ‘*’ character and move
-            //    to next  character in the pattern,
-            //     i.e., ‘*’ indicates an empty sequence.
-            // b) '*' character matches with ith
-            //     character in input
+            // Two cases if we see a '*':
+            // a) We ignore '*' character and move to next character in the pattern,
+            //     i.e., '*' indicates an empty sequence.
+            // b) '*' character matches with i-th character in input
             if (pattern[j - 1] == '*')
-                lookup[i][j] = lookup[i][j - 1] ||
-                               lookup[i - 1][j];
-
-            // Current characters are considered as
-            // matching in two cases
-            // (a) current character of pattern is '?'
-            // (b) characters actually match
+            {
+                lookup[i][j] = lookup[i][j - 1] || lookup[i - 1][j];
+            }
+            // Current characters are considered as matching in two cases:
+            // (a) Current character of pattern is '?'
+            // (b) Characters actually match (case-insensitive)
             else if (pattern[j - 1] == '?' ||
-                     str[i - 1] == pattern[j - 1])
+                     str[i - 1] == pattern[j - 1] ||
+                     tolower(str[i - 1]) == tolower(pattern[j - 1]))
+            {
                 lookup[i][j] = lookup[i - 1][j - 1];
-
+            }
             // If characters don't match
             else
                 lookup[i][j] = false;
