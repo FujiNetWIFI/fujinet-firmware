3d0d039bcdc92cc26f9a3382819a63be77b82464
Oscar Fowler <oscar@justified.com>
2020-08-24 20:30:19 -0700

    Corrected some status values when encountering errors in ATX files

--- lib/sio/diskTypeAtx.h
+++ lib/sio/diskTypeAtx.h
@@ -9,10 +9,15 @@
     ATX file format data from:
     http://a8preservation.com/#/guides/atx
 */
+// Sector data exists but is incomplete
 #define ATX_SECTOR_STATUS_FDC_LOSTDATA_ERROR 0x04
+// Sector data exists but is incorrect
 #define ATX_SECTOR_STATUS_FDC_CRC_ERROR 0x08
+// No sector data available
 #define ATX_SECTOR_STATUS_MISSING_DATA 0x10
+// Sector data exists but is marked as deleted
 #define ATX_SECTOR_STATUS_DELETED 0x20
+// Sector has extended information chunk
 #define ATX_SECTOR_STATUS_EXTENDED 0x40
 
 #define ATX_WEAKOFFSET_NONE 0xFFFF
