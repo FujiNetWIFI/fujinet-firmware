6b3ad9e429d553c230df357a6ed7b0f0dd05146f
Jeff P <jeff.piepmeier@gmail.com>
2020-08-28 15:53:47 -0400

    hook in to printer list,fix class in epson_tps.cpp

--- lib/printer-emulator/epson_tps.cpp
+++ lib/printer-emulator/epson_tps.cpp
@@ -2,25 +2,28 @@
 #include "../utils/utils.h"
 #include "../../include/debug.h"
 
-void epson80::post_new_file()
+void epsonTPS::post_new_file()
 {
-    translate850 = true;
-    _eol = ASCII_CR;
+    // translate850 = true;
+    // _eol = ASCII_CR;
 
-    shortname = "epson";
+    // shortname = "epson";
 
-    pageWidth = 612.0;
-    pageHeight = 792.0;
-    // leftMargin = 18.0;
-    // bottomMargin = 0;
-    // printWidth = 576.0; // 8 inches
-    // lineHeight = 12.0;
-    // charWidth = 7.2;
-    // fontNumber = 1;
-    // fontSize = 12;
-    at_reset(); // moved all those parameters so could be excuted with ESC-@ command
+    // pageWidth = 612.0;
+    // pageHeight = 792.0;
+    // // leftMargin = 18.0;
+    // // bottomMargin = 0;
+    // // printWidth = 576.0; // 8 inches
+    // // lineHeight = 12.0;
+    // // charWidth = 7.2;
+    // // fontNumber = 1;
+    // // fontSize = 12;
+    // at_reset(); // moved all those parameters so could be excuted with ESC-@ command
+
+    // pdf_header();
+    // escMode = false;
+
+    epson80::post_new_file();
     pdf_dY = lineHeight; // go up one line for The Print Shop
 
-    pdf_header();
-    escMode = false;
 }
--- lib/printer-emulator/epson_tps.h
+++ lib/printer-emulator/epson_tps.h
@@ -1,11 +1,13 @@
 #ifndef EPSON_TPS_H
 #define EPSON_TPS_H
 
-#include "pdf_printer.h"
 #include "epson_80.h"
 
 class epsonTPS : public epson80
+
 {
+protected:
+    virtual void pdf_clear_modes() override{};
     virtual void post_new_file() override;
 
 public:
