773071388ab672c6715639f7ce25f4f99a6fe690
Jeff P <jeff.piepmeier@gmail.com>
2020-09-04 13:40:24 -0400

    copy Sdrive tape.c/h into cassette.cpp/h

--- lib/sio/cassette.h
+++ lib/sio/cassette.h
@@ -5,15 +5,14 @@
 #include "sio.h"
 //#include "../tcpip/fnUDP.h"
 
-#define LEDC_TIMER_RESOLUTION LEDC_TIMER_1_BIT
-
 #define CASSETTE_BAUD 600
+#define BLOCK_LEN 128
 
 class sioCassette : public sioDevice
 {
-private:
+protected:
     FileSystem *_FS = nullptr;
-    FILE * _file = nullptr;
+    FILE *_file = nullptr;
 
     void sio_status() override; // $53, 'S', Status
     void sio_process(uint32_t commanddata, uint8_t checksum) override;
@@ -22,9 +21,29 @@ public:
     bool cassetteActive = false; // If we are in cassette mode or not
 
     void open_cassette_file(FileSystem *filesystem); // open a file
-    void sio_enable_cassette();  // setup cassette
-    void sio_disable_cassette(); // stop cassette
-    void sio_handle_cassette();  // Handle incoming & outgoing data for cassette
+    void sio_enable_cassette();                      // setup cassette
+    void sio_disable_cassette();                     // stop cassette
+    void sio_handle_cassette();                      // Handle incoming & outgoing data for cassette
+
+private:
+    struct tape_FUJI_hdr
+    {
+        char chunk_type[4];
+        unsigned short chunk_length;
+        unsigned short irg_length;
+        char data[];
+    };
+
+    struct t_flags
+    {
+        unsigned char run : 1;
+        unsigned char FUJI : 1;
+        unsigned char turbo : 1;
+    } tape_flags;
+
+    unsigned int send_tape_block(unsigned int offset);
+    void check_for_FUJI_file();
+    unsigned int send_FUJI_tape_block(unsigned int offset);
 };
 
 #endif
