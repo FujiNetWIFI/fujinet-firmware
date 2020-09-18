a9fbf5ddf5961cc23e44fe5f81451a492dc9138d
tschak909 <thom.cherryhomes@gmail.com>
2020-09-18 13:41:11 -0500

    Implement parameters for voice.

--- lib/sio/voice.h
+++ lib/sio/voice.h
@@ -1,10 +1,9 @@
 #ifndef VOICE_H
 #define VOICE_H
 
+#include <string>
 #include "sio.h"
 #include "samlib.h"
-
-
 
 class sioVoice : public sioDevice
 {
@@ -12,14 +11,23 @@ protected:
     // act like a printer for POC
     uint8_t lastAux1 = 0;
 
-    uint8_t buffer[41];
+    uint8_t sioBuffer[41];
+    uint8_t samBuffer[41];
     void sio_write();
 
     void sio_process(uint32_t commanddata, uint8_t checksum) override;
     virtual void sio_status() override;
 
 private:
+    bool sing = false;
+    std::string pitch;
+    std::string mouth;
+    bool phonetic = false;
+    std::string speed;
+    std::string throat;
+
     void sio_sam();
+    void sio_sam_parameters();
 
 public:
 };
