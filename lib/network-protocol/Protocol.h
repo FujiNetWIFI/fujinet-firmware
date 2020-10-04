0c6f01e89eb900800744cf4766b7e69d63348f25
tschak909 <thom.cherryhomes@gmail.com>
2020-10-04 12:59:19 -0500

    NetworkProtocol now instantiable for translation testing.

--- lib/network-protocol/Protocol.h
+++ lib/network-protocol/Protocol.h
@@ -129,14 +129,14 @@ public:
      * @param cmd The Command (0x00-0xFF) for which DSTATS is requested.
      * @return a 0x00 = No payload, 0x40 = Payload to Atari, 0x80 = Payload to FujiNet, 0xFF = Command not supported.
      */
-    virtual uint8_t special_inquiry(uint8_t cmd) = 0;
+    virtual uint8_t special_inquiry(uint8_t cmd) { return 0xFF; };
 
     /**
      * @brief execute a command that returns no payload
      * @param cmdFrame a pointer to the passed in command frame for aux1/aux2/etc
      * @return error flag. TRUE on error, FALSE on success.
      */
-    virtual bool special_00(cmdFrame_t *cmdFrame) = 0;
+    virtual bool special_00(cmdFrame_t *cmdFrame) { return false; };
 
     /**
      * @brief execute a command that returns a payload to the atari.
@@ -144,14 +144,14 @@ public:
      * @param len Length of data to request from protocol. Should not be larger than buffer.
      * @return error flag. TRUE on error, FALSE on success.
      */
-    virtual bool special_40(uint8_t *sp_buf, unsigned short len, cmdFrame_t *cmdFrame) = 0;
+    virtual bool special_40(uint8_t *sp_buf, unsigned short len, cmdFrame_t *cmdFrame) { return false; };
 
     /**
      * @brief execute a command that sends a payload to fujinet (most common, XIO)
      * @param sp_buf, a pointer to the special buffer, usually a EOL terminated devicespec.
      * @param len length of the special buffer, typically SPECIAL_BUFFER_SIZE
      */
-    virtual bool special_80(uint8_t *sp_buf, unsigned short len, cmdFrame_t *cmdFrame) = 0;
+    virtual bool special_80(uint8_t *sp_buf, unsigned short len, cmdFrame_t *cmdFrame) { return false; };
 
 private:
     /**
