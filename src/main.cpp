029360d3ae46b93e5eeb707b5d081b1a8488c9f4
tschak909 <thom.cherryhomes@gmail.com>
2020-11-07 00:26:53 -0600

    turn off sniffer by default.

--- src/main.cpp
+++ src/main.cpp
@@ -105,7 +105,7 @@ void main_setup()
 
     SIO.addDevice(ptr, SIO_DEVICEID_PRINTER + fnPrinters.get_port(0)); // P:
 
-    sioR = new sioModem(ptrfs, true);
+    sioR = new sioModem(ptrfs, false); // turned off by default.
     
     SIO.addDevice(sioR, SIO_DEVICEID_RS232); // R:
 
