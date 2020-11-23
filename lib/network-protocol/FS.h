771a2dad182913f45fb266c47c6fadea8752f39a
tschak909 <thom.cherryhomes@gmail.com>
2020-11-22 20:19:17 -0600

    working on FS API

--- lib/network-protocol/FS.h
+++ lib/network-protocol/FS.h
@@ -10,12 +10,40 @@
 class NetworkProtocolFS : public NetworkProtocol
 {
     /**
+     * @brief Open a URL
+     * @param url pointer to EdUrlParser pointing to file to open.
+     * @param cmdFrame pointer to command frame for aux1/aux2/etc values.
+     * @return error flag TRUE on error, FALSE on success.
+     */
+    virtual bool open(EdUrlParser *url, cmdFrame_t *cmdFrame);
+
+    /**
+     * @brief Close the open URL
+     * @return error flag TRUE on error, FALSE on success.
+     */
+    virtual bool close();
+
+    /**
+     * @brief Read len bytes from the open URL.
+     * @param len Length in bytes.
+     * @return error flag TRUE on error, FALSE on success
+     */
+    virtual bool read(unsigned short len);
+
+    /**
+     * @brief Write len bytes to the open URL.
+     * @param len Length in bytes.
+     * @return error flag TRUE on error, FALSE on success
+     */
+    virtual bool write(unsigned short len);
+
+    /**
      * @brief Delete a file specified by URL.
      * @param url pointer to EdUrlParser pointing to file to delete
      * @param cmdFrame pointer to command frame for aux1/aux2/etc values
      * @return error flag TRUE on error, FALSE on success
      */
-    virtual bool del(EdUrlParser *url, cmdFrame_t *cmdFrame) = 0;
+    virtual bool del(EdUrlParser *url, cmdFrame_t *cmdFrame);
 
     /**
      * @brief Rename a file specified by URL.
@@ -23,7 +51,7 @@ class NetworkProtocolFS : public NetworkProtocol
      * @param cmdFrame pointer to command frame for aux1/aux2/etc values
      * @return error flag TRUE on error, FALSE on success
      */
-    virtual bool rename(EdUrlParser *url, cmdFrame_t *cmdFrame) = 0;
+    virtual bool rename(EdUrlParser *url, cmdFrame_t *cmdFrame);
 
     /**
      * @brief Make a directory at specified URL.
@@ -31,7 +59,7 @@ class NetworkProtocolFS : public NetworkProtocol
      * @param cmdFrame pointer to command frame for aux1/aux2/etc values.
      * @return error flag. TRUE on error, FALSE on success.
      */
-    virtual bool mkdir(EdUrlParser *url, cmdFrame_t *cmdFrame) = 0;
+    virtual bool mkdir(EdUrlParser *url, cmdFrame_t *cmdFrame);
 
     /**
      * @brief Remove directory at specified URL.
@@ -39,7 +67,7 @@ class NetworkProtocolFS : public NetworkProtocol
      * @param cmdFrame pointer to command frame for aux1/aux2/etc values.
      * @return error flag. TRUE on error, FALSE on success.
      */
-    virtual bool rmdir(EdUrlParser *url, cmdFrame_t *cmdFrame) = 0;
+    virtual bool rmdir(EdUrlParser *url, cmdFrame_t *cmdFrame);
     
 };
 
