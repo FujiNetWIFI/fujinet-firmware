7fafefcc4250e96571da9df3a5d933973e03ab43
tschak909 <thom.cherryhomes@gmail.com>
2020-11-24 17:50:24 -0600

    udp open, start read.

--- lib/network-protocol/UDP.cpp
+++ lib/network-protocol/UDP.cpp
@@ -23,7 +23,7 @@ bool NetworkProtocolUDP::open(EdUrlParser *urlParser, cmdFrame_t *cmdFrame)
     // Set destination to hostname, if set.
     if (!urlParser->hostName.empty())
     {
-        Debug_printf("Setting destination hostname to: %s\n",urlParser->hostName.c_str());
+        Debug_printf("Setting destination hostname to: %s\n", urlParser->hostName.c_str());
         dest = urlParser->hostName;
     }
 
@@ -35,11 +35,11 @@ bool NetworkProtocolUDP::open(EdUrlParser *urlParser, cmdFrame_t *cmdFrame)
     }
 
     // Attempt to bind port.
-    Debug_printf("Binding port %u\n",atoi(urlParser->port.c_str()));
+    Debug_printf("Binding port %u\n", atoi(urlParser->port.c_str()));
     if (udp.begin(atoi(urlParser->port.c_str())) == false)
     {
         errno_to_error();
-        return true;        
+        return true;
     }
 
     // call base class
@@ -57,4 +57,51 @@ bool NetworkProtocolUDP::close()
     udp.stop();
 
     return false; // all good.
+}
+
+bool NetworkProtocolUDP::read(unsigned short len)
+{
+    unsigned short actual_len = 0;
+    uint8_t *newData = (uint8_t *)malloc(len);
+    string newString;
+
+    Debug_printf("NetworkProtocolTCP::read(%u)\n", len);
+
+    if (newData == nullptr)
+    {
+        Debug_printf("Could not allocate %u bytes! Aborting!\n");
+        return true; // error.
+    }
+
+    if (receiveBuffer->length() == 0)
+    {
+        if (udp.available()==0)
+        {
+            errno_to_error();
+            return true;
+        }
+
+        // Do the socket read.
+        // bail if the connection is reset.
+        if (errno == ECONNRESET)
+        {
+            error = NETWORK_ERROR_CONNECTION_RESET;
+            return true;
+        }
+        else if (actual_len != len) // Read was short and timed out.
+        {
+            Debug_printf("Short receive. We got %u bytes, returning %u bytes and ERROR\n", actual_len, len);
+            error = NETWORK_ERROR_SOCKET_TIMEOUT;
+            return true;
+        }
+
+        // Add new data to buffer.
+        newString = string((char *)newData, len);
+        *receiveBuffer += newString;
+
+        free(newData);
+    }
+    // Return success
+    error = 1;
+    return NetworkProtocol::read(len);
 }
