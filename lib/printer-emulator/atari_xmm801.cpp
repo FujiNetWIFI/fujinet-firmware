c5a57bb8bd54fd12ee71deab2b4220fb7bf27164
Jeff P <jeff.piepmeier@gmail.com>
2020-12-01 23:23:57 -0500

    merge 825 & epson cmds per XMM801 manual

--- lib/printer-emulator/atari_xmm801.cpp
+++ lib/printer-emulator/atari_xmm801.cpp
@@ -49,55 +49,121 @@ void xmm801::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
         // state machine actions
         switch (epson_cmd.cmd)
         {
-        case '#': // accept 8th bit "as is"
-            esc_not_implemented();
-            break;
-        case '-': // underline
-            if (epson_cmd.ctr > 0)
+        case 1: // XMM
+        case 2: // XMM
+        case 3: // XMM
+        case 4: // XMM
+        case 5: // XMM
+        case 6: // XMM
+            // forward dot spaces
+            // need either 1-dot glyph or need to do a spacing adjustment like backspace
+
+            if (epson_font_mask & fnt_proportional)
             {
-                if (epson_cmd.N1 != 0)
-                    set_mode(fnt_underline); // underline mode ON
-                else
-                    clear_mode(fnt_underline); // underline mode OFF
-                reset_cmd();
+                fprintf(_file, " )%d(", (int)(280 - epson_cmd.cmd * 40));
+                pdf_X += 0.48 * (double)epson_cmd.cmd;
             }
-            break;
-        case '0': // 1/8 inch spacing 9 pts
-            lineHeight = 72 / 8;
+            else if (epson_font_mask & fnt_compressed)
+            {
+                fprintf(_file, " )%d(", (int)(360 - epson_cmd.cmd * 40)); // need correct value for 16.7 CPI
+                pdf_X += 0.48 * (double)epson_cmd.cmd;
+            }
+            else
+            {
+                fprintf(_file, " )%d(", (int)(600 - epson_cmd.cmd * 60)); // need correct value for 10 CPI
+                pdf_X += 0.72 * (double)epson_cmd.cmd;
+            }
+
             reset_cmd();
             break;
-        case '1': // 7/72" spacing
+        case 10:                  // XMM                 // full reverse line feed
+            pdf_dY += lineHeight; // set pdf_dY and rise to N1/216.
+            pdf_set_rise();
+            reset_cmd();
+            break;
+        case 14: // XMM // Double width printing. Stays ON until turned OFF
+            set_mode(fnt_expanded);
+            reset_cmd();
+            break;
+        case 15: // XMM // double width OFF
+            clear_mode(fnt_expanded);
+            reset_cmd();
+            break;
+        // case 17: // Proportional character set ON
+        //     set_mode(fnt_proportional);
+        //     clear_mode(fnt_compressed);
+        //     reset_cmd();
+        //     break;
+        case 19: // XMM // Select 10 CPI
+            clear_mode(fnt_proportional | fnt_compressed | fnt_elite);
+            reset_cmd();
+            break;
+        case 20: // XMM // Turns on compressed mode.
+            set_mode(fnt_compressed);
+            clear_mode(fnt_proportional);
+            reset_cmd();
+            break;
+        case 23: // XMM // international ON
+        case 24: // XMM // international OFF
+            esc_not_implemented();
+            reset_cmd();
+            break;
+        case 25: // XMM // underline on
+            set_mode(fnt_underline);
+            reset_cmd();
+            break;
+        case 26: // XMM // underline off
+            clear_mode(fnt_underline);
+            reset_cmd();
+            break;
+        case 28:                       /// XMM                     // half forward line feed
+            pdf_dY -= lineHeight / 2.; // set pdf_dY and rise to N1/216.
+            pdf_set_rise();
+            reset_cmd();
+            break;
+        case 30:                       // XMM              // half reverse line feed
+            pdf_dY += lineHeight / 2.; // set pdf_dY and rise to N1/216.
+            pdf_set_rise();
+            reset_cmd();
+            break;
+
+        case '0': // XMM - turn off paper-out sensor
+        case '/': // XMM - turn on paper-out sensor
+            esc_not_implemented();
+            reset_cmd();
+            break;
+        case '1': // XMM // 7/72" spacing
             lineHeight = 7;
             reset_cmd();
             break;
-        case '2': //Returns line spacing to default of 1/6
+        case '6': // XMM //Returns line spacing to default of 1/6
             lineHeight = 72 / 6;
             reset_cmd();
             break;
-        case '3': // Sets line spacing to N/216". Stays on until changed
+        case '3': // XMM // Sets line spacing to N/216". Stays on until changed
             if (epson_cmd.ctr > 0)
             {
                 lineHeight = 72.0 * (double)epson_cmd.N1 / 216.0;
                 reset_cmd();
             }
             break;
-        case '4': // Italic character set ON
-            set_mode(fnt_italic);
-            reset_cmd();
-            break;
-        case '5': // Italic character set OFF
-            clear_mode(fnt_italic);
-            reset_cmd();
-            break;
-        case '8':
-        case '9':
-        case '<':
-        case '=':
-        case '>':
+        // case '4': // Italic character set ON
+        //     set_mode(fnt_italic);
+        //     reset_cmd();
+        //     break;
+        // case '5': // Italic character set OFF
+        //     clear_mode(fnt_italic);
+        //     reset_cmd();
+        //     break;
+        // case '8':
+        // case '9':
+        case '<': // XMM
+                  // case '=':
+                  // case '>':
             esc_not_implemented();
             reset_cmd();
             break;
-        case '@': // Resets all special modes to power up state including Top Of Form
+        case '@': // XMM // Resets all special modes to power up state including Top Of Form
                   // need to reset font to normal
                   // not sure what to do about TOF?
 #ifdef DEBUG
@@ -107,14 +173,20 @@ void xmm801::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
             epson_set_font(epson_font_lookup(0), 7.2);
             reset_cmd();
             break;
-        case 'A': //Sets spacing of LF (line feed) to N/72
+        case 'A': // XMM //Sets spacing of LF (line feed) to N/72
             if (epson_cmd.ctr > 0)
             {
                 lineHeight = epson_cmd.N1;
                 reset_cmd();
             }
             break;
-        case 'C':
+        case 'B': // XMM - vertical tab stops
+            // can be up to 5, terminated in 0x00
+            // this will cause error
+            esc_not_implemented();
+            reset_cmd();
+            break;
+        case 'C': // XMM
             // Sets form length (FL) to N lines. Default is 66
             // Format: <ESC>"C" N, 1 < = N < = 127.
             // Resets top of form.
@@ -127,34 +199,34 @@ void xmm801::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
             else if (epson_cmd.ctr == 2)
                 reset_cmd();
             break;
-        case 'D':
-            // Reset current tabs and sets up to 28 HT (horiz tabs) ................. 9-4
-            // TABs may range up to maximum width for character and
-            // printer size. E.G. Maximum TAB for normal characters on
-            // MX-80 is 80.
-            // Format: <ESC>"D" NI N2 N3 ... NN 0.
-            // Terminate TAB sequence with zero or 128.
-            esc_not_implemented();
-            if (((c & 0x7F) == 0) || (epson_cmd.ctr > 28))
-                reset_cmd();
-            break;
-        case 'E': // Turns on emphasized mode. Can't mix with superscript, subscript, or compressed modes
+        // case 'D':
+        // Reset current tabs and sets up to 28 HT (horiz tabs) ................. 9-4
+        // TABs may range up to maximum width for character and
+        // printer size. E.G. Maximum TAB for normal characters on
+        // MX-80 is 80.
+        // Format: <ESC>"D" NI N2 N3 ... NN 0.
+        // Terminate TAB sequence with zero or 128.
+        // esc_not_implemented();
+        // if (((c & 0x7F) == 0) || (epson_cmd.ctr > 28))
+        //     reset_cmd();
+        // break;
+        case 'E': // XMM // Turns on emphasized mode. Can't mix with superscript, subscript, or compressed modes
             set_mode(fnt_emphasized);
             reset_cmd();
             break;
-        case 'F': // Turns off emphasized mode
+        case 'F': // XMM // Turns off emphasized mode
             clear_mode(fnt_emphasized);
             reset_cmd();
             break;
-        case 'G': // Turns on double strike mode.
+        case 'G': // XMM // Turns on double strike mode.
             set_mode(fnt_doublestrike);
             reset_cmd();
             break;
-        case 'H': // Turns off double strike mode, superscript, and subscript modes
+        case 'H': // XMM // Turns off double strike mode, superscript, and subscript modes
             clear_mode(fnt_doublestrike | fnt_superscript | fnt_subscript);
             reset_cmd();
             break;
-        case 'J': // Sets line spacing to N/216" for one line only and
+        case 'J': // XMM // Sets line spacing to N/216" for one line only and
                   // when received causes contents of buffer to print
                   // IMMEDIATE LINE FEED OF SIZE N/216
             if (epson_cmd.ctr > 0)
@@ -164,20 +236,21 @@ void xmm801::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
                 reset_cmd();
             }
             break;
-        case 'j': // FX-80 immediate reverse line feed just like 'J'
-                  // when received causes contents of buffer to print
-                  // IMMEDIATE REVERSE LINE FEED OF SIZE N/216
-            if (epson_cmd.ctr > 0)
-            {
-                pdf_dY += 72. * (double)epson_cmd.N1 / 216.; // set pdf_dY and rise to N1/216.
-                pdf_set_rise();
-                reset_cmd();
-            }
-            break;
-        case 'K': // Sets dot graphics mode to 480 dots per 8" line
-        case 'L': // Sets dot graphics mode to 960 dots per 8" line
-        case 'Y': // on FX-80 this is double speed but with gotcha
-        case 'Z': // quadruple density mode with gotcha
+            //        case 'j': // FX-80 immediate reverse line feed just like 'J'
+            // when received causes contents of buffer to print
+            // IMMEDIATE REVERSE LINE FEED OF SIZE N/216
+            // if (epson_cmd.ctr > 0)
+            // {
+            //     pdf_dY += 72. * (double)epson_cmd.N1 / 216.; // set pdf_dY and rise to N1/216.
+            //     pdf_set_rise();
+            //     reset_cmd();
+            // }
+            // break;
+        case 'K': // XMM // Sets dot graphics mode to 480 dots per 8" line
+        case 'V': // XMM // high density graphics
+                  // case 'L': // Sets dot graphics mode to 960 dots per 8" line
+                  // case 'Y': // on FX-80 this is double speed but with gotcha
+                  // case 'Z': // quadruple density mode with gotcha
             /* 
                Format: <ESC>"K" Nl N2, N1 and N2 determine line length.
                Line length = N1 +. 256*N2.
@@ -203,16 +276,19 @@ void xmm801::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
                 case 'K':
                     charWidth = 1.2;
                     break;
-                case 'L': // Sets dot graphics mode to 960 dots per 8" line
-                case 'Y': // on FX-80 this is double speed but with gotcha
+                // case 'L': // Sets dot graphics mode to 960 dots per 8" line
+                case 'V': // XMM
+                // case 'Y': // on FX-80 this is double speed but with gotcha
                     charWidth = 0.6;
                     break;
-                case 'Z': // on FX-80 this is double speed but with gotcha
-                    charWidth = 0.3;
-                    break;
+                //case 'Z': // on FX-80 this is double speed but with gotcha
+                //    charWidth = 0.3;
+                //    break;
+                default:
+                    charWidth = 1.2;
                 }
-                fprintf(_file, ")]TJ /F33 9 Tf 100 Tz [("); // set font to GFX mode
-                fontUsed[32] = true;
+                fprintf(_file, ")]TJ /F%d 9 Tf 100 Tz [(", NUMFONTS); // set font to GFX mode
+                fontUsed[NUMFONTS - 1] = true;
             }
 
             if (epson_cmd.ctr > 2)
@@ -222,13 +298,14 @@ void xmm801::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
                 {
                 case 'K':
                     break;
-                case 'L': // Sets dot graphics mode to 960 dots per 8" line
-                case 'Y': // on FX-80 this is double speed but with gotcha
+                //case 'L': // Sets dot graphics mode to 960 dots per 8" line
+                //case 'Y': // on FX-80 this is double speed but with gotcha
+                case 'V': // XMM
                     fprintf(_file, ")66.5(");
                     break;
-                case 'Z': // on FX-80 this is double speed but with gotcha
-                    fprintf(_file, ")99.75(");
-                    break;
+                //case 'Z': // on FX-80 this is double speed but with gotcha
+                //    fprintf(_file, ")99.75(");
+                //    break;
                 }
                 //fprintf(_file, "]TJ [(");
                 if (epson_cmd.ctr == (epson_cmd.N + 2))
@@ -243,17 +320,25 @@ void xmm801::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
                 }
             }
             break;
-        case 'N': // Sets skip over perforation to N lines
+        case 'M': // XMM // elite on
+            set_mode(fnt_elite);
+            reset_cmd();
+            break;
+        case 'N': // XMM // Sets skip over perforation to N lines
             esc_not_implemented();
             if (epson_cmd.ctr > 0)
             {
                 reset_cmd();
             }
             break;
-        case 'O': // Resets skip over perforation to 0 lines
-            esc_not_implemented();
+        // case 'O': // Resets skip over perforation to 0 lines
+        //     esc_not_implemented();
+        //     break;
+        case 'P': // XMM // elite off
+            clear_mode(fnt_elite);
+            reset_cmd();
             break;
-        case 'Q': // Sets column width to N
+        case 'Q': //XMM // Sets column width to N
             // 1 < = N < = maximum number of characters/line.
             // width on MX80, right margin on FX80
             if (epson_cmd.ctr > 0)
@@ -263,23 +348,24 @@ void xmm801::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
                 reset_cmd();
             }
             break;
-        case 'S': // Sets superscript/subscript modes
+        case 'R': // XMM - reverse of epson
+                  // case 'S': // Sets superscript/subscript modes
             // N=0 = > superscript, N>0 = > subscript.
             if (epson_cmd.ctr > 0)
             {
                 if (epson_cmd.N1 != 0)
-                    set_mode(fnt_subscript);
+                    set_mode(fnt_superscript); // XMM
                 else
-                    set_mode(fnt_superscript);
+                    set_mode(fnt_subscript); // XMM
                 reset_cmd();
             }
             break;
-        case 'T': // Resets superscript, subscript, and unidrectional printing
+        case 'T': // XMM // Resets superscript, subscript, and unidrectional printing
             // does not turn off double strike from script modes
             clear_mode(fnt_superscript | fnt_subscript);
             reset_cmd();
             break;
-        case 'U': // Unidirectional printing. Prints each line from left to right
+        case 'U': // XMM // Unidirectional printing. Prints each line from left to right
                   // N=0 = > OFF,N>0 = > ON.
             esc_not_implemented();
             if (epson_cmd.ctr > 0)
@@ -287,7 +373,7 @@ void xmm801::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
                 reset_cmd();
             }
             break;
-        case 'W': // Double width printing. Stays ON until turned OFF
+        case 'W': // XMM // Double width printing. Stays ON until turned OFF
             // N=0 = > OFF, N=1 = > ON.
             // Has precedence over Shift Out (SO = CHR$(14))
             // Clears SO/14 mode
@@ -304,63 +390,14 @@ void xmm801::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
                 reset_cmd();
             }
             break;
-        case 'l': // left margin on FX-80
-                  // would need to restart the text object like on the Atari 1025 emulator
-                  // or keep track of a pdf_dX variable to make adjustments in relative
-                  // X-coordinates. For example:
-                  // 0 -18 Td
-                  // then adjust left margin
-                  // pdf_dX (new left margin) -18 Td
-                  // 0 -18 Td (just another line)
-                  // then adjust left margin back to 0
-                  // -pdf_dX -18 Td (have to shift it back to left)
-                  // 0 -18 Td (just another line)
+        case 'p': // XMM - proportional
+            // N=0 = > OFF,N>0 = > ON.
             esc_not_implemented();
             if (epson_cmd.ctr > 0)
             {
-                // range check N1
-                // printWidth = epson_cmd.N1 * charWidth;
                 reset_cmd();
             }
             break;
-
-            //        case 0x4c: // 'L'
-            /* code */
-            // for long and short lines, i think we end line, ET, then set the leftMargin and pageWdith and begin text
-            // challenge is to not skip a line if we're at the beginning of a line
-            // could also add a state variable so we don't unnecessarily change the line width
-            /* if (shortFlag)
-            {
-                if (!BOLflag)
-                    pdf_end_line();   // close out string array
-                fprintf(_file,"ET\n"); // close out text object
-                // set new margins
-                leftMargin = 18.0;  // (8.5-8.0)/2*72
-                printWidth = 576.0; // 8 inches
-                pdf_begin_text(pdf_Y);
-                // start text string array at beginning of line
-                fprintf(_file,"[(");
-                BOLflag = false;
-                shortFlag = false;
-            } */
-            //            break;
-            //        case 0x53: // 'S'
-            // for long and short lines, i think we end line, ET, then set the leftMargin and pageWdith and begin text
-            /* if (!shortFlag)
-            {
-                if (!BOLflag)
-                    pdf_end_line();   // close out string array
-                fprintf(_file,"ET\n"); // close out text object
-                // set new margins
-                leftMargin = 75.6;  // (8.5-6.4)/2.0*72.0;
-                printWidth = 460.8; //6.4*72.0; // 6.4 inches
-                pdf_begin_text(pdf_Y);
-                // start text string array at beginning of line
-                fprintf(_file,"[(");
-                BOLflag = false;
-                shortFlag = true;
-            } */
-            //            break;
         default:
             reset_cmd();
             break;
