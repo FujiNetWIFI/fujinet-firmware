4d9c9589b6e5d519b378e867d601337a00ee0d8a
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-12-20 21:37:16 -0600

    HTTP Open.

---
+++ lib/network-protocol/HTTP.cpp
@@ -0,0 +1,203 @@
+/**
+ * HTTP implementation
+ */
+
+#include "HTTP.h"
+#include "status_error_codes.h"
+
+NetworkProtocolHTTP::NetworkProtocolHTTP(string *rx_buf, string *tx_buf, string *sp_buf)
+    : NetworkProtocolFS(rx_buf, tx_buf, sp_buf)
+{
+    rename_implemented = true;
+    delete_implemented = true;
+    mkdir_implemented = true;
+    rmdir_implemented = true;
+}
+
+NetworkProtocolHTTP::~NetworkProtocolHTTP()
+{
+}
+
+bool NetworkProtocolHTTP::open_file_handle()
+{
+    Debug_printf("NetworkProtocolHTTP::open_file_handle()\n");
+
+    error = NETWORK_ERROR_SUCCESS;
+
+    switch (aux1_open)
+    {
+    case 4:
+        openMode = GET;
+        break;
+    case 8:
+        openMode = PUT;
+        break;
+    case 12:
+        openMode = POST;
+        break;
+    default:
+        error = NETWORK_ERROR_NOT_IMPLEMENTED;
+        return true;
+    }
+
+    return false;
+}
+
+bool NetworkProtocolHTTP::open_dir_handle()
+{
+    Debug_printf("NetworkProtocolHTTP::open_dir_handle()\n");
+    return false;
+}
+
+bool NetworkProtocolHTTP::mount(EdUrlParser *url)
+{
+    const char *content_length_header[] = {"Content-Length"};
+
+    Debug_printf("NetworkProtocolHTTP::mount(%s)\n", url->toString().c_str());
+
+    // fix scheme because esp-idf hates uppercase for some #()$@ reason.
+    if (url->scheme == "HTTP")
+        url->scheme = "http";
+    else if (url->scheme == "HTTPS")
+        url->scheme = "https";
+
+    // First try to do a HEAD request, to get some information about the URL
+    client = new fnHttpClient();
+    client->begin(url->toString());
+    client->collect_headers(content_length_header, 1);
+    resultCode = client->HEAD();
+    fserror_to_error();
+    fileSize = atoi(client->get_header(0).c_str());
+    Debug_printf("File size: %u\n",fileSize);
+    client->close();
+    delete client;
+
+    if (resultCode > 399)
+        return true; // error
+
+    client = new fnHttpClient();
+
+    return !client->begin(url->toString());
+}
+
+bool NetworkProtocolHTTP::umount()
+{
+    Debug_printf("NetworkProtocolHTTP::umount()\n");
+
+    if (client == nullptr)
+        return false;
+
+    delete client;
+
+    return false;
+}
+
+void NetworkProtocolHTTP::fserror_to_error()
+{
+    switch (resultCode)
+    {
+    case 200:
+    case 201:
+    case 202:
+    case 203:
+    case 204:
+    case 205:
+    case 206:
+    case 207:
+    case 208:
+    case 226:
+        error = NETWORK_ERROR_SUCCESS;
+        break;
+    case 401: // Unauthorized
+    case 402:
+    case 403: // Forbidden
+    case 407:
+        error = NETWORK_ERROR_INVALID_USERNAME_OR_PASSWORD;
+        break;
+    case 404:
+    case 410:
+        error = NETWORK_ERROR_FILE_NOT_FOUND;
+        break;
+    case 405:
+        error = NETWORK_ERROR_NOT_IMPLEMENTED;
+        break;
+    case 408:
+        error = NETWORK_ERROR_GENERAL_TIMEOUT;
+        break;
+    case 423:
+    case 451:
+        error = NETWORK_ERROR_ACCESS_DENIED;
+        break;
+    case 400: // Bad request
+    case 406: // not acceptible
+    case 409:
+    case 411:
+    case 412:
+    case 413:
+    case 414:
+    case 415:
+    case 416:
+    case 417:
+    case 418:
+    case 421:
+    case 422:
+    case 424:
+    case 425:
+    case 426:
+    case 428:
+    case 429:
+    case 431:
+    case 500:
+    case 501:
+    case 502:
+    case 503:
+    case 504:
+    case 505:
+    case 506:
+    case 507:
+    case 508:
+    case 510:
+    case 511:
+    default:
+        error = NETWORK_ERROR_GENERAL;
+        break;
+    }
+}
+
+bool NetworkProtocolHTTP::read_file_handle(uint8_t *buf, unsigned short len)
+{
+    Debug_printf("NetworkProtocolHTTP::read_file_handle(%p,%u)\n", buf, len);
+    return false;
+}
+
+bool NetworkProtocolHTTP::read_dir_entry(char *buf, unsigned short len)
+{
+    Debug_printf("NetworkProtocolHTTP::read_dir_entry(%p,%u)\n", buf, len);
+    return false;
+}
+
+bool NetworkProtocolHTTP::close_file_handle()
+{
+    Debug_printf("NetworkProtocolHTTP::close_file_Handle()\n");
+    if (client != nullptr)
+        client->close();
+    return false;
+}
+
+bool NetworkProtocolHTTP::close_dir_handle()
+{
+    Debug_printf("NetworkProtocolHTTP::close_dir_handle()\n");
+    return false;
+}
+
+bool NetworkProtocolHTTP::write_file_handle(uint8_t *buf, unsigned short len)
+{
+    Debug_printf("NetworkProtocolHTTP::write_file_handle(%p,%u)\n", buf, len);
+    return false;
+}
+
+bool NetworkProtocolHTTP::stat(string path)
+{
+    Debug_printf("NetworkProtocolHTTP::stat(%s)\n", path.c_str());
+    return false;
+}
