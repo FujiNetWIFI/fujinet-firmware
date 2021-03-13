f286bda38dc6944c157ce4ccbd79f1783efeab23
Jeff P <jeff.piepmeier@gmail.com>
2021-03-13 18:43:29 -0500

    converting device_db to FN framework

--- lib/iec/device_db.h
+++ lib/iec/device_db.h
@@ -1,51 +1,43 @@
 #ifndef DEVICE_DB_H
 #define DEVICE_DB_H
 
-#include "global_defines.h"
-#include <ArduinoJson.h>
-
-#if defined(USE_SPIFFS)
-#include <SPIFFS.h>
-#elif defined(USE_LITTLEFS)
-#if defined(ESP8266)
-#include <LittleFS.h>
-#elif defined(ESP32)
-#include <LITTLEFS.h>
-#endif
-#endif
+//#include "global_defines.h"
+//#include <ArduinoJson.h>
+#include <string>
+#include "../FileSystem/fnFS.h"
 
 #define RECORD_SIZE 256
 
 class DeviceDB
 {
 public:
-    DeviceDB(FS *fileSystem);
+    DeviceDB(FileSystem *fs);
     ~DeviceDB();
 
-    bool init(String database);
+    bool init(std::string database);
     bool check();
     bool save();
 
-    String database;
+    std::string database;
 
-    byte device();
-    void device(byte device);
-    byte drive();
-    void drive(byte drive);
-    byte partition();
-    void partition(byte partition);
-    String url();
-    void url(String url);
-    String path();
-    void path(String path);
-    String image();
-    void image(String image);
+    uint8_t device();
+    void device(uint8_t device);
+    uint8_t drive();
+    void drive(uint8_t drive);
+    uint8_t partition();
+    void partition(uint8_t partition);
+    std::string url();
+    void url(std::string url);
+    std::string path();
+    void path(std::string path);
+    std::string image();
+    void image(std::string image);
 
-    bool select(byte device);
+    bool select(uint8_t device);
 
 private:
     bool m_dirty;
-    FS *m_fileSystem;
+    FileSystem *m_fileSystem;
     StaticJsonDocument<256> m_device;
 };
 
