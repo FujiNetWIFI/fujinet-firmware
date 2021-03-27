3c86902534a7d116d266c9c71233c5909ff1a2ca
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-03-27 15:15:08 -0500

    start SSH implementation.

--- lib/sio/network.cpp
+++ lib/sio/network.cpp
@@ -15,6 +15,7 @@
 #include "../network-protocol/TNFS.h"
 #include "../network-protocol/FTP.h"
 #include "../network-protocol/HTTP.h"
+#include "../network-protocol/SSH.h"
 
 using namespace std;
 
@@ -783,6 +784,10 @@ bool sioNetwork::instantiate_protocol()
     else if (urlParser->scheme == "HTTP" || urlParser->scheme == "HTTPS")
     {
         protocol = new NetworkProtocolHTTP(receiveBuffer, transmitBuffer, specialBuffer);        
+    }
+    else if (urlParser->scheme == "SSH")
+    {
+        protocol = new NetworkProtocolSSH(receiveBuffer, transmitBuffer, specialBuffer);
     }
     else
     {
