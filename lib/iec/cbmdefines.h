9c13c188da8925af404b7f94dec5485bf2f10d3e
James Johnston <jjohnston@techknowpro.com>
2021-04-02 11:24:10 -0500

    Added attribution and license header Minor tweaks

--- lib/iec/cbmdefines.h
+++ lib/iec/cbmdefines.h
@@ -1,3 +1,23 @@
+// Meatloaf - A Commodore 1541 disk drive emulator
+// https://github.com/idolpx/meatloaf
+// Copyright(C) 2020 James Johnston
+//
+// This file is part of Meatloaf but adapted for use in the FujiNet project
+// https://github.com/FujiNetWIFI/fujinet-platformio
+// 
+// Meatloaf is free software : you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Meatloaf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Meatloaf. If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef CBMDEFINES_H
 #define CBMDEFINES_H
 
@@ -20,15 +40,10 @@ namespace CBM {
 // Device OPEN channels.
 // Special channels.
 enum IECChannels {
-	READPRG_CHANNEL = 0,
-	WRITEPRG_CHANNEL = 1,
+	READ_CHANNEL = 0,
+	WRITE_CHANNEL = 1,
 	CMD_CHANNEL = 15
 };
-/*
-#define READPRG_CHANNEL 0
-#define WRITEPRG_CHANNEL 1
-#define CMD_CHANNEL 15
-*/
 
 // Back arrow character code.
 #define CBM_BACK_ARROW 0x5f
--- lib/iec/device_db.h
+++ lib/iec/device_db.h
@@ -1,3 +1,23 @@
+// Meatloaf - A Commodore 1541 disk drive emulator
+// https://github.com/idolpx/meatloaf
+// Copyright(C) 2020 James Johnston
+//
+// This file is part of Meatloaf but adapted for use in the FujiNet project
+// https://github.com/FujiNetWIFI/fujinet-platformio
+// 
+// Meatloaf is free software : you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Meatloaf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Meatloaf. If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef DEVICE_DB_H
 #define DEVICE_DB_H
 
