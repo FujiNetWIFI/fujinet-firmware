5a4df24e052825ecb92d065e1e14a28c10e8e3a6
Bill Kendrick <nbs@sonic.net>
2021-07-17 03:29:32 -0700

    New APETIME calls for dealing with timezones
    Adding a new SETTZ call for APETIME (0x99), which allows you
    to send a string from the Atari to FujiNet, specifying the
    timezone you wish to receive times for -- rather than the
    timezone set on the FujiNet overall (e.g., via web interface).
    
    The standard GETTIME call (0x93) is unaffected.  A new call,
    GETTZTIME (0x9A) fetches the time in the timezone specified
    when SETTZ was called.
    
    Under the hood, the new GETTZTIME simply sets a "TZ" environment
    variable with the string received in the SETTZ call, and invokes
    "tzset()".  Then it gets the time, as usual (via "localtime()").
    Finally, it sets "TZ" back to what it was, and calls "tzset()"
    again.  (Therefore, it should not affect the Fujinet's overall
    timezone setting.  Nor, obviously, does it mess with the
    timezone config settings.)
    
    Motivation - The APOD image viewer benefits from knowing the
    date _in the Eastern timezone_, since new APOD images are posted
    daily at 00:00 (12:00am) Eastern.
    
    See https://github.com/FujiNetWIFI/fujinet-platformio/issues/449

--- lib/sio/apetime.h
+++ lib/sio/apetime.h
@@ -6,7 +6,8 @@
 class sioApeTime : public sioDevice
 {
 private:
-    void _sio_get_time();
+    void _sio_get_time(bool use_timezone);
+    void _sio_set_tz();
 
 public:
     void sio_process(uint32_t commanddata, uint8_t checksum) override;
