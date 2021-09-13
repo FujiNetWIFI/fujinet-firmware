ec51be476c378ebd7487601cc0bef76f682a5a26
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-09-13 17:58:51 -0500

    Start on libadamnet.

---
+++ lib/adamnet/adamnet.h
@@ -0,0 +1,13 @@
+#ifndef ADAMNET_H
+#define ADAMNET_H
+
+/**
+ * AdamNet Routines
+ */
+
+#include <forward_list>
+#include "fnSystem.h"
+
+uint8_t adamnet_checksum(uint8_t *buf, unsigned short len);
+
+#endif /* ADAMNET_H */
