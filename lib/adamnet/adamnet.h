e8e2c608ab5c8eb539e500250adfa9e78b23ff96
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-09-13 19:52:26 -0500

    Add documentation for adamnet_checksum()

--- lib/adamnet/adamnet.h
+++ lib/adamnet/adamnet.h
@@ -8,6 +8,15 @@
 #include <forward_list>
 #include "fnSystem.h"
 
+/**
+ * Calculate checksum for AdamNet packets
+ * uses a simple 8-bit XOR of each successive byte.
+ * @brief buf pointer to buffer
+ * @brief len length of buffer
+ * @return checksum value (0x00 - 0xFF)
+ */
 uint8_t adamnet_checksum(uint8_t *buf, unsigned short len);
+
+
 
 #endif /* ADAMNET_H */
