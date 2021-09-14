cdaae911f15baf676cb904c2ab24bab6d83076a3
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-09-13 19:41:08 -0500

    assign uart numbers 3 and 4 for the unusual adamnet mode.

--- lib/hardware/fnUART.cpp
+++ lib/hardware/fnUART.cpp
@@ -7,8 +7,10 @@
 #include "../../include/pinmap.h"
 #include "fnUART.h"
 
-#define UART_DEBUG UART_NUM_0
-#define UART_SIO   UART_NUM_2
+#define UART_DEBUG      UART_NUM_0
+#define UART_ADAMNET_TX UART_NUM_1
+#define UART_ADAMNET_RX UART_NUM_2
+#define UART_SIO        UART_NUM_2
 
 // Number of RTOS ticks to wait for data in TX buffer to complete sending
 #define MAX_FLUSH_WAIT_TICKS 200
@@ -18,6 +20,8 @@
 
 UARTManager fnUartDebug(UART_DEBUG);
 UARTManager fnUartSIO(UART_SIO);
+UARTManager fnUartAdamNetTX(UART_ADAMNET_TX);
+UARTManager fnUartAdamNetRX(UART_ADAMNET_RX);
 
 // Constructor
 UARTManager::UARTManager(uart_port_t uart_num) : _uart_num(uart_num), _uart_q(NULL) {}
@@ -75,11 +79,23 @@ void UARTManager::begin(int baud)
     {
         rx = PIN_UART2_RX;
         tx = PIN_UART2_TX;
+    } 
+    else if (_uart_num == 3) // AdamNet RX
+    {
+        rx = PIN_ADAMNET_RX;
+        tx = PIN_TX_DUMMY;
+    }
+    else if (_uart_num == 4) // AdamNet TX (inverted polarity)
+    {
+        rx = PIN_RX_DUMMY;
+        tx = PIN_ADAMNET_TX;
     } else {
         return;
     }
 
     uart_set_pin(_uart_num, tx, rx, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+    if (_uart_num == 4)
+        uart_set_line_inverse(_uart_num,UART_SIGNAL_TXD_INV);
 
     // Arduino default buffer size is 256
     int uart_buffer_size = 256;
