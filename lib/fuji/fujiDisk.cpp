1f0c98eed1f5cf5efd5c8e959d048758218f802d
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-10-13 18:54:09 -0500

    fujiDisk now uses media type defines.

--- lib/fuji/fujiDisk.cpp
+++ lib/fuji/fujiDisk.cpp
@@ -8,17 +8,16 @@ void fujiDisk::reset()
     filename[0] = '\0';
     fileh = nullptr;
     access_mode = DISK_ACCESS_MODE_READ;
-    disk_type = DISKTYPE_UNKNOWN;
+    disk_type = MEDIA_TYPE_UNKNOWN;
     host = nullptr;
 }
 
 void fujiDisk::reset(const char *fname, uint8_t hostslot, uint8_t mode)
 {
     fileh = nullptr;
-    disk_type = DISKTYPE_UNKNOWN;
+    disk_type = MEDIA_TYPE_UNKNOWN;
     host = nullptr;
 
     host_slot = hostslot;
     access_mode = mode;
-//    strlcpy(filename, fname, sizeof(filename));
 }
