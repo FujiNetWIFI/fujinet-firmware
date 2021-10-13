fc253824dee960fa5e99a7d95222e9c47343e9c1
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-10-13 18:52:50 -0500

    stub adamnet network device.

--- lib/device/adamnet/network.cpp
+++ lib/device/adamnet/network.cpp
@@ -1,3 +1,25 @@
 #ifdef BUILD_ADAM
 
+#include "adamnet/network.h"
+
+adamNetwork::adamNetwork()
+{
+
+}
+
+adamNetwork::~adamNetwork()
+{
+
+}
+
+void adamNetwork::adamnet_status()
+{
+
+}
+
+void adamNetwork::adamnet_process(uint8_t b)
+{
+
+}
+
 #endif /* BUILD_ADAM */
