fd3eb83e1257df7ed721a4641c2033f68e3b2697
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-10-14 16:41:16 -0500

    add missing adamPrinter methods.

--- lib/device/adamnet/printer.cpp
+++ lib/device/adamnet/printer.cpp
@@ -5,17 +5,9 @@
 
 #include "file_printer.h"
 #include "html_printer.h"
-#include "atari_820.h"
-#include "atari_822.h"
-#include "atari_825.h"
 #include "svg_plotter.h"
-#include "atari_1025.h"
-#include "atari_1027.h"
-#include "atari_1029.h"
 #include "epson_80.h"
 #include "epson_tps.h"
-#include "atari_xmm801.h"
-#include "atari_xdm121.h"
 #include "okimate_10.h"
 #include "png_printer.h"
 
@@ -46,12 +38,54 @@ adamPrinter::printer_type adamPrinter::match_modelname(std::string model_name)
 
 void adamPrinter::adamnet_process(uint8_t b)
 {
-    
 }
 
 void adamPrinter::shutdown()
 {
+}
 
+void adamPrinter::set_printer_type(printer_type printer_type)
+{
+    // Destroy any current printer emu object
+    delete _pptr;
+
+    _ptype = printer_type;
+    switch (printer_type)
+    {
+    case PRINTER_FILE_RAW:
+        _pptr = new filePrinter(RAW);
+        break;
+    case PRINTER_FILE_TRIM:
+        _pptr = new filePrinter;
+        break;
+    case PRINTER_FILE_ASCII:
+        _pptr = new filePrinter(ASCII);
+        break;
+    case PRINTER_EPSON:
+        _pptr = new epson80;
+        break;
+    case PRINTER_EPSON_PRINTSHOP:
+        _pptr = new epsonTPS;
+        break;
+    case PRINTER_OKIMATE10:
+        _pptr = new okimate10;
+        break;
+    case PRINTER_PNG:
+        _pptr = new pngPrinter;
+        break;
+    case PRINTER_HTML:
+        _pptr = new htmlPrinter;
+        break;
+    case PRINTER_HTML_ATASCII:
+        _pptr = new htmlPrinter(HTML_ATASCII);
+        break;
+    default:
+        _pptr = new filePrinter;
+        _ptype = PRINTER_FILE_TRIM;
+        break;
+    }
+
+    _pptr->initPrinter(_storage);
 }
 
 #endif /* BUILD_ADAM */
