dc9e2a68b96469431d8f9238b4dceb645f919496
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-10-15 23:19:02 -0500

    adamnet extend block data type

--- lib/media/adam/mediaType.h
+++ lib/media/adam/mediaType.h
@@ -62,7 +62,7 @@ public:
     virtual bool format(uint16_t *respopnsesize);
 
     // Returns TRUE if an error condition occurred
-    virtual bool read(uint16_t blockNum, uint16_t *readcount) = 0;
+    virtual bool read(uint32_t blockNum, uint16_t *readcount) = 0;
     // Returns TRUE if an error condition occurred
     virtual bool write(uint16_t blockNum, bool verify);
     
