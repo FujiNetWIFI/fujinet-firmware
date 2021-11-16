2003a8056f661e233f5e01b60a4a8730c0e26f2a
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-11-15 18:14:08 -0600

    add adamnet scaffold to printer.

--- lib/device/adamnet/printer.h
+++ lib/device/adamnet/printer.h
@@ -13,7 +13,13 @@ protected:
     // SIO THINGS
     uint8_t _buffer[40];
     void sio_write(uint8_t aux1, uint8_t aux2);
-    void adamnet_control_status() override;
+    
+    virtual void adamnet_control_status();
+    virtual void adamnet_control_clr();
+    virtual void adamnet_control_send();
+    virtual void adamnet_control_receive();
+    virtual void adamnet_control_ready();
+
     void adamnet_process(uint8_t b) override;
     void shutdown() override;
 
