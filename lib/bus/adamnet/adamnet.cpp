0dc755b53ddcba0515d425181649e2a2064e2ca7
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-11-23 19:52:38 -0600

    remove debug msg.

--- lib/bus/adamnet/adamnet.cpp
+++ lib/bus/adamnet/adamnet.cpp
@@ -13,7 +13,6 @@ uint8_t adamnet_checksum(uint8_t *buf, unsigned short len)
 {
     uint8_t checksum = 0x00;
 
-    Debug_printf("ck len: %u\n",len);
     for (unsigned short i = 0; i < len; i++)
         checksum ^= buf[i];
 
