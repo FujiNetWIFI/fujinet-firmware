9e1f15d5b3f3207ea89dc7b508863c00b9dd4331
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2021-11-30 14:26:06 -0600

    add reset hook to disk

--- lib/device/adamnet/disk.h
+++ lib/device/adamnet/disk.h
@@ -10,7 +10,7 @@ private:
     MediaType *_media = nullptr;
     TaskHandle_t diskTask;
 
-    unsigned long blockNum=0;
+    unsigned long blockNum=INVALID_SECTOR_VALUE;
 
     void adamnet_control_status() override;
     void adamnet_control_ack();
@@ -34,6 +34,7 @@ public:
     mediatype_t mount(FILE *f, const char *filename, uint32_t disksize, mediatype_t disk_type = MEDIATYPE_UNKNOWN);
     void unmount();
     bool write_blank(FILE *f, uint32_t numBlocks);
+    virtual void reset();
 
     mediatype_t mediatype() { return _media == nullptr ? MEDIATYPE_UNKNOWN : _media->_mediatype; };
 
