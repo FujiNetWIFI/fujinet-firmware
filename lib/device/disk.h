8da96abdfe5e1bfb8be0e8789b0d0738bb7a88e7
idolpx <jaime@idolpx.com>
2022-02-02 13:08:53 -0600

    Renamed MEDIA_TYPE_UNKNOWN to MEDIATYPE_UNKNOWN

--- lib/device/disk.h
+++ lib/device/disk.h
@@ -3,29 +3,21 @@
 
 #ifdef BUILD_ATARI
 # include "sio/disk.h"
-# define MEDIA_TYPE mediatype_t
-# define MEDIA_TYPE_UNKNOWN MEDIATYPE_UNKNOWN
 # define DEVICE_TYPE sioDisk
 #endif
 
 #ifdef BUILD_CBM
 # include "iec/disk.h"
-# define MEDIA_TYPE mediatype_t
-# define MEDIA_TYPE_UNKNOWN MEDIATYPE_UNKNOWN
 # define DEVICE_TYPE iecDisk
 #endif
 
 #ifdef BUILD_ADAM
 # include "adamnet/disk.h"
-# define MEDIA_TYPE mediatype_t
-# define MEDIA_TYPE_UNKNOWN MEDIATYPE_UNKNOWN
 # define DEVICE_TYPE adamDisk
 #endif
 
 #ifdef NEW_TARGET
 # include "new/disk.h"
-# define MEDIA_TYPE mediatype_t
-# define MEDIA_TYPE_UNKNOWN MEDIATYPE_UNKNOWN
 # define DEVICE_TYPE adamDisk
 #endif
 
