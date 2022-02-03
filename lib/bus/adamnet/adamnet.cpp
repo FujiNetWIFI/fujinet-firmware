5a2437013799cf18a9a8c5dcfb5efd40a7f51a36
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2022-02-02 21:56:13 -0600

    change reset task delay.

--- lib/bus/adamnet/adamnet.cpp
+++ lib/bus/adamnet/adamnet.cpp
@@ -53,7 +53,7 @@ static void adamnet_reset_intr_task(void *arg)
             reset_debounced = false;
             ;
         }
-        vTaskDelay(1000);
+        vTaskDelay(10);
     }
 }
 
@@ -161,6 +161,10 @@ void adamNetDevice::adamnet_response_ack()
         AdamNet.wait_for_idle();
         adamnet_send(0x90 | _devnum);
     }
+    else
+    {
+        Debug_printf("NM_ACK too long: %u μs\n",t);
+    }
 }
 
 void adamNetDevice::adamnet_response_nack()
@@ -234,6 +238,7 @@ void adamNetBus::_adamnet_process_cmd()
     uint8_t b;
 
     b = fnUartSIO.read();
+    start_time = esp_timer_get_time();
 
     uint8_t d = b & 0x0F;
 
@@ -245,7 +250,6 @@ void adamNetBus::_adamnet_process_cmd()
     {
         // turn on AdamNet Indicator LED
         fnLedManager.set(eLed::LED_BUS, true);
-        start_time = esp_timer_get_time();
         _daisyChain[d]->adamnet_process(b);
         // turn off AdamNet Indicator LED
         fnLedManager.set(eLed::LED_BUS, false);
