3631375c7c536b60208d0a228f1c73d87d20caaa
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2022-02-07 15:06:26 -0600

    cleanup

--- lib/device/adamnet/disk.h
+++ lib/device/adamnet/disk.h
@@ -24,7 +24,7 @@ private:
     void adamnet_control_send();
     void adamnet_control_send_block_num();
     void adamnet_control_send_block_data();
-    virtual void adamnet_response_status();
+    virtual void adamnet_response_status() override;
     void adamnet_response_send();
 
     void adamnet_process(uint8_t b) override;
@@ -34,7 +34,7 @@ public:
     mediatype_t mount(FILE *f, const char *filename, uint32_t disksize, mediatype_t disk_type = MEDIATYPE_UNKNOWN);
     void unmount();
     bool write_blank(FILE *f, uint32_t numBlocks);
-    virtual void reset();
+    virtual void reset() override;
 
     mediatype_t mediatype() { return _media == nullptr ? MEDIATYPE_UNKNOWN : _media->_mediatype; };
 
