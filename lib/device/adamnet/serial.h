e5bc219a7eb1b6514041851eca1779aca2dfc638
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2022-02-17 08:40:50 -0600

    i can code, honest.

--- lib/device/adamnet/serial.h
+++ lib/device/adamnet/serial.h
@@ -2,6 +2,8 @@
 #define ADAM_SERIAL_H
 
 #include <cstdint>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 #include "bus.h"
 
@@ -40,6 +42,30 @@ class adamSerial : public virtualDevice
     virtual void adamnet_control_ready();
     
     void adamnet_control_send();
+
+    /**
+     * Queue Handle
+     */
+    xQueueHandle serial_out_queue;
+
+private:
+
+    /**
+     * Task handle for TX task
+     */
+    TaskHandle_t thSerial;
+
+    /**
+     * Send Structure
+     */
+    typedef struct _sendData
+    {
+        uint8_t len;
+        uint8_t data[16];
+    } SendData;
+
+    SendData next;
+
 };
 
 #endif /* ADAM_SERIAL_H */
