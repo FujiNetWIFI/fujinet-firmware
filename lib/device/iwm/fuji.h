8ffedd8bffe2607ead0078e76034a6788c9e3b12
Jeff P <jeff.piepmeier@gmail.com>
2022-02-22 16:37:27 -0500

    have to stop for now

--- lib/device/iwm/fuji.h
+++ lib/device/iwm/fuji.h
@@ -64,6 +64,13 @@ private:
     //uint8_t response[1024]; // use packet_buffer instead
     //uint16_t response_len;
 
+    // Response to SIO_FUJICMD_GET_SCAN_RESULT
+    struct
+    {
+        char ssid[MAX_SSID_LEN];
+        uint8_t rssi;
+    } detail;
+
     iwmBus *_iwm_bus;
 
     fujiHost _fnHosts[MAX_HOSTS];
@@ -80,42 +87,49 @@ private:
 
     appkey _current_appkey;
 
-    uint8_t app_key_buffer[64];
-    size_t app_key_len;
+    uint8_t ctrl_stat_buffer[512]; // what is proper length
+    size_t ctrl_stat_len;
+
+    char dirpath[256];
 
 protected:
-    void iwm_dummy_command();               // control 0xAA
-    void iwm_reset_fujinet();               // control 0xFF
-    void iwm_net_get_ssid();                // status 0xFE
-    void iwm_net_scan_networks();           // status 0xFD
-    void iwm_net_scan_result();             // status 0xFC
-    void iwm_net_set_ssid();                // control 0xFB
-    void iwm_net_get_wifi_status();         // status 0xFA
-    void iwm_mount_host();                  // 0xF9
-    void iwm_disk_image_mount();            // 0xF8
-    /*
-    void adamnet_open_directory(uint16_t s);         // 0xF7
-    void adamnet_read_directory_entry();   // 0xF6
-    void adamnet_close_directory();        // 0xF5
-    void adamnet_read_host_slots();        // 0xF4
-    void adamnet_write_host_slots();       // 0xF3
-    void adamnet_read_device_slots();      // 0xF2
-    void adamnet_write_device_slots();     // 0xF1
-   */
-    void iwm_disk_image_umount();      // 0xE9
-  /*
-    void adamnet_get_adapter_config();     // 0xE8
-    void adamnet_new_disk();               // 0xE7
-    void adamnet_unmount_host();           // 0xE6
-    void adamnet_get_directory_position(); // 0xE5
-    void adamnet_set_directory_position(); // 0xE4
+    void iwm_dummy_command();                    // control 0xAA
+    void iwm_ctrl_reset_fujinet();               // control 0xFF
+    void iwm_stat_net_get_ssid();                // status 0xFE
+    void iwm_stat_net_scan_networks();           // status 0xFD
+    void iwm_ctrl_net_scan_result();             // control 0xFC
+    void iwm_stat_net_scan_result();             // status 0xFC
+    void iwm_ctrl_net_set_ssid();                // control 0xFB
+    void iwm_stat_net_get_wifi_status();         // status 0xFA
+    void iwm_ctrl_mount_host();                  // 0xF9
+    void iwm_ctrl_disk_image_mount();            // 0xF8
+    void iwm_ctrl_open_directory();              // 0xF7
+    void iwm_ctrl_read_directory_entry();        // 0xF6
+    void iwm_stat_read_directory_entry();        // 0xF6
+
+    void iwm_ctrl_close_directory();        // 0xF5
+    void iwm_stat_read_host_slots();        // 0xF4
+    void iwm_ctrl_write_host_slots();       // 0xF3
+    void iwm_stat_read_device_slots();      // 0xF2
+    void iwm_ctrl_read_device_slots();     // 0xF1
+    void iwm_ctrl_disk_image_umount();      // 0xE9
+    void iwm_stat_get_adapter_config();     // 0xE8
+    void iwm_ctrl_new_disk();               // 0xE7
+  /*  void adamnet_unmount_host();           // 0xE6
+ */
+   void iwm_stat_get_directory_position(); // 0xE5
+   void iwm_ctrl_set_directory_position(); // 0xE4
+/*
     void adamnet_set_hadamnet_index();         // 0xE3
-    void adamnet_set_device_filename(uint16_t s);    // 0xE2
-    void adamnet_set_host_prefix();        // 0xE1
-    void adamnet_get_host_prefix();        // 0xE0
+ */
+   void iwm_ctrl_set_device_filename();    // 0xE2
+
+    void iwm_ctrl_set_host_prefix();        // 0xE1
+    void iwm_stat_get_host_prefix();        // 0xE0
+/*
     void adamnet_set_adamnet_external_clock(); // 0xDF
     */
-    void iwm_write_app_key();          // 0xDE
+    void iwm_ctrl_write_app_key();          // 0xDE
     void iwm_ctrl_read_app_key();           // 0xDD - control
     void iwm_stat_read_app_key();             // 0xDD - status
     /*
@@ -125,9 +139,9 @@ protected:
     void iwm_ctrl_get_device_filename();    // 0xDA
     void iwm_stat_get_device_filename();    // 0xDA
     
-    void iwm_set_boot_config();            // 0xD9
-    void iwm_copy_file();                  // 0xD8
-    void iwm_set_boot_mode();              // 0xD6
+    void iwm_ctrl_set_boot_config();            // 0xD9
+    void iwm_ctrl_copy_file();                  // 0xD8
+    void iwm_ctrl_set_boot_mode();              // 0xD6
 
     void shutdown() override;
     void process(cmdPacket_t cmd) override;
