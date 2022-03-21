36f1ea33497084224183fb460bcb80372e5e6d20
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2022-03-20 22:21:51 -0500

    increase task priority of main task to 20.

--- src/main.cpp
+++ src/main.cpp
@@ -232,7 +232,7 @@ extern "C"
 // Create a new high-priority task to handle the main loop
 // This is assigned to CPU1; the WiFi task ends up on CPU0
 #define MAIN_STACKSIZE 32768
-#define MAIN_PRIORITY 10
+#define MAIN_PRIORITY 20
 #define MAIN_CPUAFFINITY 1
         xTaskCreatePinnedToCore(fn_service_loop, "fnLoop",
                                 MAIN_STACKSIZE, nullptr, MAIN_PRIORITY, nullptr, MAIN_CPUAFFINITY);
