71842c7913a9ab677d0b3199ea466424982f7327
Thomas <thom.cherryhomes@gmail.com>
2022-04-04 20:44:31 -0500

    bump ver.

--- include/version.h
+++ include/version.h
@@ -10,10 +10,10 @@
 #define FN_VERSION_MAJOR 0
 #define FN_VERSION_MINOR 5
 
-#define FN_VERSION_BUILD "31fdd4a2"
+#define FN_VERSION_BUILD "ece872fe"
 
-#define FN_VERSION_DATE "2022-04-04 23:22:22"
+#define FN_VERSION_DATE "2022-04-05 01:36:00"
 
-#define FN_VERSION_FULL "0.5.31fdd4a2"
+#define FN_VERSION_FULL "0.5.ece872fe"
 
 
