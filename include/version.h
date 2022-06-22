f8a6e37344c79481e620a928256df8fc29aeeb94
Jeff P <jeff.piepmeier@gmail.com>
2022-06-21 23:19:15 -0400

    segregate atari from apple FN1.0 usage

--- include/version.h
+++ include/version.h
@@ -10,11 +10,11 @@
 #define FN_VERSION_MAJOR 0
 #define FN_VERSION_MINOR 5
 
-#define FN_VERSION_BUILD "8d1c2cac"
+#define FN_VERSION_BUILD "b27a46c5"
 
-#define FN_VERSION_DATE "2022-06-19 01:26:11"
+#define FN_VERSION_DATE "2022-06-22 03:15:06"
 
-#define FN_VERSION_FULL "0.5.8d1c2cac"
+#define FN_VERSION_FULL "0.5.b27a46c5"
 
 
 
