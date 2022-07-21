b582111276414cbcaa66c1f31055c8f26daa11f4
idolpx <jaime@idolpx.com>
2022-07-21 13:28:37 -0400

    Split up pinmap defines

---
+++ include/pinmap/esp32s3.h
@@ -0,0 +1,48 @@
+/* FujiNet Hardware Pin Mapping */
+#ifdef PINMAP_ESP32S3
+
+// ***** NOTE: UNTESTED ***** //
+/* SD Card */
+#define PIN_CARD_DETECT 12 // fnSystem.h
+#define PIN_CARD_DETECT_FIX 15 // fnSystem.h
+#define PIN_SD_HOST_MISO 19
+#define PIN_SD_HOST_MOSI 23
+#define PIN_SD_HOST_SCK  18
+#define PIN_SD_HOST_CS   GPIO_NUM_5
+
+/* UART */
+#define PIN_UART0_RX 3 // fnUART.cpp
+#define PIN_UART0_TX 1
+#define PIN_UART1_RX 9
+#define PIN_UART1_TX 10
+#define PIN_UART2_RX 33
+#define PIN_UART2_TX 21
+
+/* Buttons */
+#define PIN_BUTTON_A 0 // keys.cpp
+#define PIN_BUTTON_B 34
+#define PIN_BUTTON_C 14
+
+/* LEDs */
+#define PIN_LED_WIFI 2 // led.cpp
+#define PIN_LED_BUS 4
+// pins 12-15 are used to interface with the JTAG debugger
+// so leave them alone if we're using JTAG
+#if !defined(JTAG)
+#define PIN_LED_BT 13
+#else
+#define PIN_LED_BT 4
+#endif
+
+/* Atari SIO Pins */
+#define PIN_INT 26 // sio.h
+#define PIN_PROC 22
+#define PIN_CKO 32
+#define PIN_CKI 27
+#define PIN_MTR 36
+#define PIN_CMD 39
+
+/* Audio Output */
+#define PIN_DAC1 25 // samlib.h
+
+#endif /* PINMAP_ESP32S3 */
