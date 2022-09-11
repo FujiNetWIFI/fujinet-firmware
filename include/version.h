f3afea36165c86de5e4c8d3b6bb4f9c9e0212d4b
Jeff P <jeff.piepmeier@gmail.com>
2022-09-10 20:20:15 -0400

    apple2: use malloc for DMA safe memory

--- include/version.h
+++ include/version.h
@@ -10,11 +10,11 @@
 #define FN_VERSION_MAJOR 0
 #define FN_VERSION_MINOR 5
 
-#define FN_VERSION_BUILD "4d9b6297"
+#define FN_VERSION_BUILD "3e0fb9fc"
 
-#define FN_VERSION_DATE "2022-09-10 23:06:56"
+#define FN_VERSION_DATE "2022-09-11 00:14:57"
 
-#define FN_VERSION_FULL "0.5.4d9b6297"
+#define FN_VERSION_FULL "0.5.3e0fb9fc"
 
 
 
