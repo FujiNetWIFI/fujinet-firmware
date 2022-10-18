43f4c64d7b4a65ebafee025ce80f5909ef203742
Jan Krupa <apc.atari@gmail.com>
2022-10-18 20:40:04 +0200

    adjust empty ack

--- lib/bus/sio/sio.cpp
+++ lib/bus/sio/sio.cpp
@@ -253,11 +253,6 @@ void systemBus::_sio_process_cmd()
                 }
             }
         }
-        if (!_command_processed)
-        {
-            // Notify NetSIO hub we are not interested to handle the command
-            sio_empty_ack();
-        }
     } // valid checksum
     else
     {
@@ -269,6 +264,12 @@ void systemBus::_sio_process_cmd()
             _command_frame_counter = 0;
             toggleBaudrate();
         }
+    }
+
+    if (!_command_processed)
+    {
+        // Notify NetSIO hub that we are not interested to handle this command
+        sio_empty_ack();
     }
     fnLedManager.set(eLed::LED_BUS, false);
 }
