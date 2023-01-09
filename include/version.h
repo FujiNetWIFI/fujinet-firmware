6e15df9449479f513389341936a0f27a62a4e574
Thomas <thom.cherryhomes@gmail.com>
2023-01-08 22:08:23 -0600

    limit cpm throughput to 512 bytes for now.

--- include/version.h
+++ include/version.h
@@ -10,8 +10,8 @@
 #define FN_VERSION_MAJOR 0
 #define FN_VERSION_MINOR 5
 
-#define FN_VERSION_BUILD "d2ce660e"
+#define FN_VERSION_BUILD "0f9c1f0b"
 
-#define FN_VERSION_DATE "2023-01-08 21:41:31"
+#define FN_VERSION_DATE "2023-01-09 02:08:36"
 
-#define FN_VERSION_FULL "0.5.d2ce660e"
+#define FN_VERSION_FULL "0.5.0f9c1f0b"
