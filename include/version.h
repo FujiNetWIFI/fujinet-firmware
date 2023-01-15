f8ff9bdec94a7ac1583137b984a36aca47cfba21
Jeff P <jeff.piepmeier@gmail.com>
2023-01-15 15:30:48 -0500

    add .woz mediatype

--- include/version.h
+++ include/version.h
@@ -10,8 +10,8 @@
 #define FN_VERSION_MAJOR 0
 #define FN_VERSION_MINOR 5
 
-#define FN_VERSION_BUILD "ed9aa924"
+#define FN_VERSION_BUILD "535df3ee"
 
-#define FN_VERSION_DATE "2023-01-13 21:47:55"
+#define FN_VERSION_DATE "2023-01-15 20:30:18"
 
-#define FN_VERSION_FULL "0.5.ed9aa924"
+#define FN_VERSION_FULL "0.5.535df3ee"
