2da2175261e00ecbb8f1b55c06680653c61e2026
Jeff P <jeff.piepmeier@gmail.com>
2023-01-15 18:39:18 -0500

    disk 2 class, low level, service

--- lib/device/iwm/fuji.cpp
+++ lib/device/iwm/fuji.cpp
@@ -209,8 +209,17 @@ void iwmFuji::iwm_ctrl_disk_image_mount() // SP CTRL command
     // We need the file size for loading XEX files and for CASSETTE, so get that too
     disk.disk_size = host.file_size(disk.fileh);
 
-    // And now mount it
+    // special handling for Disk ][ .woz images
+    mediatype_t mt = MediaType::discover_mediatype(disk.filename);
+    if (mt == mediatype_t::MEDIATYPE_PO)
+    { // And now mount it
     disk.disk_type = disk.disk_dev.mount(disk.fileh, disk.filename, disk.disk_size);
+}
+    else if (mt == mediatype_t::MEDIATYPE_WOZ)
+    {
+      _fnDisk2s[deviceSlot % 2].init();
+      _fnDisk2s[deviceSlot % 2].mount(disk.fileh); // modulo to ensure device 0 or 1
+    }
 }
 
 
