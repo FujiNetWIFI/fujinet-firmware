3aed5906390f0a36fe127a16d7cee6f62db2a3d7
Thomas <thom.cherryhomes@gmail.com>
2023-02-10 13:41:31 -0600

    [cx16] dummy disk device.

---
+++ lib/device/cx16_i2c/disk.h
@@ -0,0 +1,31 @@
+#ifndef DISK_H
+#define DISK_H
+
+#include <fujiHost.h>
+#include "bus.h"
+#include "media.h"
+
+class sioDisk : public virtualDevice
+{
+private:
+    MediaType *_disk = nullptr;
+
+    void sio_read();
+    void sio_write(bool verify);
+    void sio_format();
+    void status() override;
+    void process(uint32_t commanddata, uint8_t checksum) override;
+
+public:
+    sioDisk();
+    fujiHost *host;
+    mediatype_t mount(FILE *f, const char *filename, uint32_t disksize, mediatype_t disk_type = MEDIATYPE_UNKNOWN);
+    void unmount();
+    bool write_blank(FILE *f, uint16_t sectorSize, uint16_t numSectors);
+
+    mediatype_t disktype() { return _disk == nullptr ? MEDIATYPE_UNKNOWN : _disk->_mediatype; };
+
+    ~sioDisk();
+};
+
+#endif
