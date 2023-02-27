b444203238140eb56c9b2fc9190d683b6711192c
Thomas <thom.cherryhomes@gmail.com>
2023-02-26 18:16:39 -0600

    [iec] implement wait.

--- lib/bus/iec/protocol/iecProtocolBase.cpp
+++ lib/bus/iec/protocol/iecProtocolBase.cpp
@@ -63,6 +63,45 @@ int16_t IecProtocolBase::timeoutWait(uint8_t pin, bool target_status, size_t wai
 
 bool IecProtocolBase::wait(size_t wait, uint64_t start = 0)
 {
+    uint64_t current, elapsed;
+    elapsed = 0;
+    wait--; // Shave 1us for overhead
+
+    if ( start == 0 )
+    {
+        esp_timer_init();
+        start = current = esp_timer_get_time();
+    }
+    else
+    {
+        current = esp_timer_get_time();
+    }
+
+    // Sample ATN and set flag to indicate SELECT or DATA mode
+    bool atn_status = IEC.status ( PIN_IEC_ATN );
+    if ( atn_status == PULLED)
+        IEC.flags |= ATN_PULLED;
+
+    // pull ( PIN_IEC_SRQ );
+    while ( elapsed < wait )
+    {
+        current = esp_timer_get_time();
+        elapsed = current - start;
+
+        bool atn_check = IEC.status ( PIN_IEC_ATN );
+        if ( atn_check == PULLED)
+            IEC.flags |= ATN_PULLED;
+
+        if ( atn_check != atn_status )
+        {
+            // release ( PIN_IEC_SRQ );
+            // Debug_printv("wait[%d] elapsed[%d]", wait, elapsed);
+            return false;
+        }
+    }
+
+    // release ( PIN_IEC_SRQ );
+    return true;
 }
 
 #endif /* BUILD_IEC */
