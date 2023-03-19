9d8fed0400f5f7f1067f353b0c63ae9d4d733595
Thomas <thom.cherryhomes@gmail.com>
2023-03-18 20:57:21 -0500

    [network-protocol][fs] prevent short wrap.

--- lib/network-protocol/FS.cpp
+++ lib/network-protocol/FS.cpp
@@ -254,7 +254,7 @@ bool NetworkProtocolFS::status_file(NetworkStatus *status)
     if (aux1_open == 8)
         status->rxBytesWaiting = 0;
     else
-        status->rxBytesWaiting = fileSize;
+        status->rxBytesWaiting = fileSize > 65534 ? 65534 : fileSize;
     status->connected = fileSize > 0 ? 1 : 0;
     status->error = fileSize > 0 ? error : NETWORK_ERROR_END_OF_FILE;
 
