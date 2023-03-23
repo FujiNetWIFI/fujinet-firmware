a30306f0c0eb7d10a890312506ef319dc627ffb2
Thomas <thom.cherryhomes@gmail.com>
2023-03-23 13:25:23 -0500

    [iec][network] start over.

--- lib/device/iec/network.h
+++ lib/device/iec/network.h
@@ -16,111 +16,81 @@
 #include "string_utils.h"
 
 /**
- * # of devices to expose via IEC
- */
-#define NUM_DEVICES 2
-
-/**
  * The size of rx and tx buffers
  */
 #define INPUT_BUFFER_SIZE 65535
 #define OUTPUT_BUFFER_SIZE 65535
 #define SPECIAL_BUFFER_SIZE 256
 
+/**
+ * The number of IEC secondary addresses (16)
+ */
+#define NUM_CHANNELS 16
+
+using namespace std;
+
 class iecNetwork : public virtualDevice
-{
-public:
-    /**
-     * Command frame for protocol adapters
-     */
-    cmdFrame_t cmdFrame;
+{    
+    public:
 
     /**
-     * CTOR
+     * @brief CTOR
      */
     iecNetwork();
 
     /**
-     * DTOR
+     * @brief the Receive buffers, for each channel
+     */
+    string *receiveBuffer[NUM_CHANNELS];
+    
+    /**
+     * @brief the Transmit buffers, one for each channel.
+     */
+    string *transmitBuffer[NUM_CHANNELS];
+
+    /**
+     * @brief the Special buffers, one for each channel.
+     */
+    string *specialBuffer[NUM_CHANNELS];
+
+    /**
+     * @brief the protocol instance for given channel
+    */
+    NetworkProtocol *protocol[NUM_CHANNELS];
+
+    /**
+     * @brief DTOR
      */
     virtual ~iecNetwork();
 
-    // Status
-    void status()
-    {
-        // TODO IMPLEMENT
-    }
-
-protected:
-    device_state_t process(IECData *commanddata) override;
-    void shutdown() override;
-
-private:
     /**
-     * JSON Object
+     * @brief Process command fanned out from bus
+     * @param _commanddata the passed in commanddata
+     * @return new device state
      */
-    FNJSON *json[16];
+    device_state_t process(IECData *_commanddata);
+
+    private:
 
     /**
-     * The Receive buffers for this N: device
+     * @brief the active URL for each channel
      */
-    std::string *receiveBuffer[16] = {nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr};
+    string deviceSpec[NUM_CHANNELS];
 
     /**
-     * The transmit buffers for this N: device
+     * @brief the URL parser for each channel
      */
-    std::string *transmitBuffer[16] = {nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr};
+    EdUrlParser *urlParser[NUM_CHANNELS];
 
     /**
-     * The special buffers for this N: device
+     * @brief the prefix for each channel
      */
-    std::string *specialBuffer[16] = {nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr};
+    string prefix[NUM_CHANNELS];
 
     /**
-     * The EdUrlParser object used to hold/process a URL
+     * @brief the active Channel mode for each channel
      */
-    EdUrlParser *urlParser[16] = {nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr};
-
-    /**
-     * Instance of currently open network protocol
-     */
-    NetworkProtocol *protocol[16] = {nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr};
-
-    /**
-     * Network Status object
-     */
-    union _status
-    {
-        struct _statusbits
-        {
-            bool client_data_available : 1;
-            bool client_connected : 1;
-            bool client_error : 1;
-            bool server_connection_available : 1;
-            bool server_error : 1;
-        } bits;
-        unsigned char byte;
-    } statusByte;
-
-    /**
-     * Error number, if status.bits.client_error is set.
-     */
-    uint8_t err;
-
-    /**
-     * @brief the Device spec currently open (N:TCP://192.168.1.1:1234/)
-     */
-    string deviceSpec[16];
-
-    /**
-     * The channel mode for a given IEC subdevice. By default, it is PROTOCOL, which passes
-     * read/write/status commands to the protocol. Otherwise, it's a special mode, e.g. to pass to
-     * the JSON or XML parsers.
-     *
-     * @enum PROTOCOL Send to protocol
-     * @enum JSON Send to JSON parser.
-     */
-    enum _channel_mode
+        enum _channel_mode
     {
         PROTOCOL,
         JSON
@@ -144,155 +114,90 @@ private:
         };
 
     /**
-     * @brief the current translation mode for given channel.
+     * @brief the active translation mode for each channel 
      */
-    uint8_t translationMode[16] = 
-    {
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    };
+    uint8_t translationMode[NUM_CHANNELS];
 
     /**
-     * The login to use for a protocol action
+     * @brief the login (username) for each channel
      */
-    std::string login;
+    string login[NUM_CHANNELS];
 
     /**
-     * The password to use for a protocol action
+     * @brief the password for each channel
      */
-    std::string password;
+    string password[NUM_CHANNELS];
 
     /**
-     * @brief The currently set path prefix.
+     * @brief the JSON object for each channel
      */
-    std::string prefix[16];
+    FNJSON *json[NUM_CHANNELS];
 
     /**
-     * @brief # of bytes remaining in JSON stream
-     */
-    uint16_t json_bytes_remaining[16] = 
-        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-
-    /**
-     * @brief respond to OPEN command ($F0)
-     */
-    void iec_open();
-
-    /**
-     * @brief response to CLOSE command ($E0)
-     */
-    void iec_close();
-
-    /**
-     * @brief response to DATA command on LOAD channel ($60)
-     */
-    void iec_reopen_load();
-
-    /**
-     * @brief response to DATA command on SAVE channel ($60)
-     */
-    void iec_reopen_save();
-
-    /**
-     * @brief response to DATA command on any other channel ($60)
-     */
-    void iec_reopen_channel();
-
-    /**
-     * @brief Computer->FujiNet write
-     */
-    void iec_write();
-
-    /**
-     * @brief FujiNet->Com puter read
-     */
-    void iec_read();
-
-    /**
-     * @brief return data waiting.
-     */
-    void data_waiting();
-
-    /**
-     * @brief handle 
-    */
-
-    /**
-     * @brief set network prefix on desired device.
+     * @brief Set desired prefix for channel
      */
     void set_prefix();
 
     /**
-     * @brief Parse JSON in currently open channel
+     * @brief called to open a connection to a protocol
      */
-    void parse_json();
+    void iec_open();
 
     /**
-     * @brief set desired translation mode
+     * @brief called to close a connection.
      */
-    void set_translation();
+    void iec_close();
+    
+    /**
+     * @brief called when a TALK, then REOPEN happens on channel 0
+     */
+    void iec_reopen_load();
 
     /**
-     * @brief Set channel mode
+     * @brief called when TALK, then REOPEN happens on channel 1
      */
-    void set_channel_mode();
+    void iec_reopen_save();
 
     /**
-     * @brief Set Login/password
+     * @brief called when LISTEN happens on command channel (15).
      */
-    void set_login();
+    void iec_listen_command();
 
     /**
-     * @brief perform idempotent filesystem op via protocol
-     * @param _comnd the command to send to protocol
+     * @brief called when TALK happens on command channel (15).
      */
-    void fsop(unsigned char _comnd);
+    void iec_talk_command();
 
     /**
-     * @brief perform JSON Query 
+     * @brief called to process command either at open or listen
      */
-    void set_json_query();
+    void iec_command();
 
     /**
-     * @brief Deal with commands sent to command channel
+     * @brief If response queue is empty, Return 1 if ANY receive buffer has data in it, else 0
      */
-    void process_command();
+    void iec_talk_command_buffer_status();
 
     /**
-     * @brief Called to process a special command
-     */
-    void process_command_special();
-
-    /**
-     * @brief Call protocol to process special command
-     */
-    void process_command_special_protocol();
-
-    /**
-     * @brief Deal with URLs passed to load channel
+     * @brief process command for channel 0 (load)
      */
     void process_load();
 
     /**
-     * @brief Deal with URLs passed to save channel
+     * @brief process command for channel 1 (save)
      */
     void process_save();
 
     /**
-     * @brief Deal with URLs passed to data channels (3-14)
+     * @brief process command channel
+     */
+    void process_command();
+
+    /**
+     * @brief process every other channel (2-14)
      */
     void process_channel();
 
-    /**
-     * @brief read from desired channel
-     * @param l Number of bytes to read
-     */
-    void read_channel(uint16_t l);
-
-    /**
-     * @brief read channel, protocol mode JSON
-     * @param l Number of bytes to read
-     */
-    void read_channel_json(uint16_t l);
 };
 
 #endif /* NETWORK_H */
