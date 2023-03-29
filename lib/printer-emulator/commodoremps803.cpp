10005f0465bd50bc64bd635216628e3c6e18498a
Jeff P <jeff.piepmeier@gmail.com>
2023-03-29 06:59:07 -0400

    business mode enabled

--- lib/printer-emulator/commodoremps803.cpp
+++ lib/printer-emulator/commodoremps803.cpp
@@ -27,6 +27,66 @@ void commodoremps803::pdf_handle_char(uint8_t c, uint8_t aux1, uint8_t aux2)
     //      bitmap graphics mode
     // there's also case for repeated bit image printing so need ESC-mode type processing
 
+/**
+ * TABLE 1 Page 26 of Users Manual
+Special Control Character Summary
+CHRSCode
+CHRSI14)
+CHRSM5)
+CHR$|8)
+CHRSCI8
+CHRSM46)
+CHRS(13)
+CHRS(IO)
+CHRS<17)
+Printer function
+Enhance ON
+Enhance OF F
+Bit Image Printing
+Reverse ON
+Reverse OFF
+Carriage Return
+Line Feed
+Print in
+Business Mode
+Print in
+Graphic Mode
+Quote
+Tab Setting
+the Print Head
+Repeat Graphic
+Selected
+Specify Dot Address
+CHRSIH5)
+CHRS(34>
+CHRS[16); "nHnL"
+CHR$(26J; CHRS|btt image data)
+CHRS(27);CHRS(16);CHRS(nH)CHRS{nL)
+'
+i
+*/
+
+    if (aux1 == 0)
+    {
+        if (fontNumber == 3 || fontNumber == 4)
+        {
+            fontNumber -= 2;
+            fprintf(_file, ")]TJ\n/F%1d 12 Tf [(", fontNumber);
+            // charWidth = 14.4; //72.0 / 5.0;
+            fontUsed[fontNumber - 1] = true;
+        }
+    }
+    else if (aux1 == 7)
+    {
+        if (fontNumber == 1 || fontNumber == 2)
+        {
+            fontNumber += 2;
+            fprintf(_file, ")]TJ\n/F%1d 12 Tf [(", fontNumber);
+            // charWidth = 14.4; //72.0 / 5.0;
+            fontUsed[fontNumber - 1] = true;
+        }
+    }
+    
     // maybe printable character
     if (c > 31 && c < 127)
     {
