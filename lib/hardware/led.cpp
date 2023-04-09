8c6e5109a97c3f1e9410050701350eae3675effd
mozzwald <mozzwald@gmail.com>
2023-04-08 20:05:41 -0500

    ledstrip: use ledstrip if detected at startup

--- lib/hardware/led.cpp
+++ lib/hardware/led.cpp
@@ -3,9 +3,7 @@
 
 #include "fnSystem.h"
 
-#ifdef LED_STRIP
 #include "led_strip.h"
-#endif
 
 #define BLINKING_TIME 100 // 200 ms
 
@@ -50,83 +48,91 @@ void LedManager::setup()
 
 void LedManager::set(eLed led, bool on)
 {
-#ifdef LED_STRIP
-    switch (led)
+    if(fnSystem.check_ledstrip())
     {
-    case eLed::LED_BUS:
-        fnLedStrip.set(stripLed::LED_STRIP_BUS, on);
-        break;
-    case eLed::LED_BT:
-        fnLedStrip.set(stripLed::LED_STRIP_BT, on);
-        break;
-    case eLed::LED_WIFI:
-        fnLedStrip.set(stripLed::LED_STRIP_WIFI, on);
-        break;
-    default:
-        break;
+        switch (led)
+        {
+        case eLed::LED_BUS:
+            fnLedStrip.set(stripLed::LED_STRIP_BUS, on);
+            break;
+        case eLed::LED_BT:
+            fnLedStrip.set(stripLed::LED_STRIP_BT, on);
+            break;
+        case eLed::LED_WIFI:
+            fnLedStrip.set(stripLed::LED_STRIP_WIFI, on);
+            break;
+        default:
+            break;
+        }
     }
-#else
-    mLedState[led] = on;
-#ifdef PINMAP_A2_REV0
-    // FujiApple Rev 0 BUS LED has reverse logic
-    if (led == LED_BUS)
-        fnSystem.digital_write(mLedPin[led], (on ? DIGI_HIGH : DIGI_LOW));
     else
-        fnSystem.digital_write(mLedPin[led], (on ? DIGI_LOW : DIGI_HIGH));
+    {
+        mLedState[led] = on;
+#ifdef PINMAP_A2_REV0
+        // FujiApple Rev 0 BUS LED has reverse logic
+        if (led == LED_BUS)
+            fnSystem.digital_write(mLedPin[led], (on ? DIGI_HIGH : DIGI_LOW));
+        else
+            fnSystem.digital_write(mLedPin[led], (on ? DIGI_LOW : DIGI_HIGH));
 #else
-    fnSystem.digital_write(mLedPin[led], (on ? DIGI_LOW : DIGI_HIGH));
+        fnSystem.digital_write(mLedPin[led], (on ? DIGI_LOW : DIGI_HIGH));
 #endif
-#endif // LED_STRIP
+    }
 }
 
 void LedManager::toggle(eLed led)
 {
-#ifdef LED_STRIP
-    switch (led)
+    if(fnSystem.check_ledstrip())
     {
-    case eLed::LED_BUS:
-        fnLedStrip.toggle(stripLed::LED_STRIP_BUS);
-        break;
-    case eLed::LED_BT:
-        fnLedStrip.toggle(stripLed::LED_STRIP_BT);
-        break;
-    case eLed::LED_WIFI:
-        fnLedStrip.toggle(stripLed::LED_STRIP_WIFI);
-        break;
-    default:
-        break;
+        switch (led)
+        {
+        case eLed::LED_BUS:
+            fnLedStrip.toggle(stripLed::LED_STRIP_BUS);
+            break;
+        case eLed::LED_BT:
+            fnLedStrip.toggle(stripLed::LED_STRIP_BT);
+            break;
+        case eLed::LED_WIFI:
+            fnLedStrip.toggle(stripLed::LED_STRIP_WIFI);
+            break;
+        default:
+            break;
+        }
     }
-#else
-    set(led, !mLedState[led]);
-#endif // LED_STRIP
+    else
+    {
+        set(led, !mLedState[led]);
+    }
 }
 
 void LedManager::blink(eLed led, int count)
 {
-#ifdef LED_STRIP
-    switch (led)
+    if(fnSystem.check_ledstrip())
     {
-    case eLed::LED_BUS:
-        fnLedStrip.blink(stripLed::LED_STRIP_BUS, count);
-        break;
-    case eLed::LED_BT:
-        fnLedStrip.blink(stripLed::LED_STRIP_BT, count);
-        break;
-    case eLed::LED_WIFI:
-        fnLedStrip.blink(stripLed::LED_STRIP_WIFI, count);
-        break;
-    default:
-        break;
+        switch (led)
+        {
+        case eLed::LED_BUS:
+            fnLedStrip.blink(stripLed::LED_STRIP_BUS, count);
+            break;
+        case eLed::LED_BT:
+            fnLedStrip.blink(stripLed::LED_STRIP_BT, count);
+            break;
+        case eLed::LED_WIFI:
+            fnLedStrip.blink(stripLed::LED_STRIP_WIFI, count);
+            break;
+        default:
+            break;
+        }
     }
-
-#else
-    for(int i = 0; i < count; i++)
+    else
     {
-        toggle(led);
-        fnSystem.delay(BLINKING_TIME);
-        toggle(led);
-        if(i < count - 1)
+        for(int i = 0; i < count; i++)
+        {
+            toggle(led);
             fnSystem.delay(BLINKING_TIME);
+            toggle(led);
+            if(i < count - 1)
+                fnSystem.delay(BLINKING_TIME);
+        }
     }
-#endif // LED_STRIP
 }
