f7f879857d89c477792cd21f4e68a7f10a8e7989
idolpx <jaime@idolpx.com>
2023-05-11 04:06:38 -0400

    [iec][disk] change this back

--- lib/device/iec/disk.cpp
+++ lib/device/iec/disk.cpp
@@ -161,7 +161,7 @@ void iecDisk::process_command()
     {
         iec_talk_command();
     }
-    else if (commanddata->primary == IEC_LISTEN)
+    else if (commanddata->primary == IEC_UNLISTEN)
     {
         iec_command();
     }
@@ -1020,7 +1020,7 @@ bool iecDisk::sendFile()
             }
 #endif
             // Send Byte
-            avail = istream->available();
+            avail = istream->available() + 1;
             if ( !avail || !success_rx )
             {
                 //Debug_printv("b[%02X] EOI", b);
@@ -1038,7 +1038,6 @@ bool iecDisk::sendFile()
             }
             b = nb; // byte = next byte
             i++;
-			avail++;
 
 #ifdef DATA_STREAM
             // Show ASCII Data
