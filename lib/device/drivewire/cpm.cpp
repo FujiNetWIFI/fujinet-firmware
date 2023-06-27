cb4b05a109ab51cf7a106362b046fa26cff4aa09
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2023-06-27 18:06:38 -0500

    [drivewire] and with this, drivewire scaffold builds.

--- lib/device/drivewire/cpm.cpp
+++ lib/device/drivewire/cpm.cpp
@@ -1,8 +1,8 @@
-#ifdef BUILD_ATARI
+#ifdef BUILD_COCO
 
 #define CCP_INTERNAL
 
-#include "siocpm.h"
+#include "cpm.h"
 
 #include "fnSystem.h"
 #include "fnUART.h"
@@ -23,71 +23,64 @@
 # include "../runcpm/ccp.h" // ccp.h - Defines a simple internal CCP
 #endif
 
-
-void sioCPM::sio_status()
+void drivewireCPM::drivewire_handle_cpm()
 {
-    // Nothing to do here
-    return;
+//     _puts(CCPHEAD);
+//     _PatchCPM();
+//     Status = 0;
+// #ifdef CCP_INTERNAL
+//     _ccp();
+// #else
+//     if (!_sys_exists((uint8 *)CCPname))
+//     {
+//         _puts("Unable to load CP/M CCP.\r\nCPU halted.\r\n");
+//         break;
+//     }
+//     _RamLoad((uint8 *)CCPname, CCPaddr);    // Loads the CCP binary file into memory
+//     Z80reset();                             // Resets the Z80 CPU
+//     SET_LOW_REGISTER(BC, _RamRead(0x0004)); // Sets C to the current drive/user
+//     PC = CCPaddr;                           // Sets CP/M application jump point
+//     Z80run();                               // Starts simulation
+// #endif
+//     if (Status == 1) // This is set by a call to BIOS 0 - ends CP/M
+//     {
+//         cpmActive = false;
+//         free(RAM);
+//     }
 }
 
-void sioCPM::sio_handle_cpm()
+void drivewireCPM::init_cpm(int baud)
 {
-    _puts(CCPHEAD);
-    _PatchCPM();
-    Status = 0;
-#ifdef CCP_INTERNAL
-    _ccp();
-#else
-    if (!_sys_exists((uint8 *)CCPname))
-    {
-        _puts("Unable to load CP/M CCP.\r\nCPU halted.\r\n");
-        break;
-    }
-    _RamLoad((uint8 *)CCPname, CCPaddr);    // Loads the CCP binary file into memory
-    Z80reset();                             // Resets the Z80 CPU
-    SET_LOW_REGISTER(BC, _RamRead(0x0004)); // Sets C to the current drive/user
-    PC = CCPaddr;                           // Sets CP/M application jump point
-    Z80run();                               // Starts simulation
-#endif
-    if (Status == 1) // This is set by a call to BIOS 0 - ends CP/M
-    {
-        cpmActive = false;
-        free(RAM);
-    }
+    // fnUartSIO.set_baudrate(baud);
+    // Status = Debug = 0;
+    // Break = Step = -1;
+    // RAM = (uint8_t *)malloc(MEMSIZE);
+    // memset(RAM, 0, MEMSIZE);
+    // memset(filename, 0, sizeof(filename));
+    // memset(newname, 0, sizeof(newname));
+    // memset(fcbname, 0, sizeof(fcbname));
+    // memset(pattern, 0, sizeof(pattern));
 }
 
-void sioCPM::init_cpm(int baud)
+void drivewireCPM::drivewire_process(uint32_t commanddata, uint8_t checksum)
 {
-    fnUartSIO.set_baudrate(baud);
-    Status = Debug = 0;
-    Break = Step = -1;
-    RAM = (uint8_t *)malloc(MEMSIZE);
-    memset(RAM, 0, MEMSIZE);
-    memset(filename, 0, sizeof(filename));
-    memset(newname, 0, sizeof(newname));
-    memset(fcbname, 0, sizeof(fcbname));
-    memset(pattern, 0, sizeof(pattern));
+    // cmdFrame.commanddata = commanddata;
+    // cmdFrame.checksum = checksum;
+
+    // switch (cmdFrame.comnd)
+    // {
+    // case 'G':
+    //     sio_ack();
+    //     fnSystem.delay(10);
+    //     sio_complete();
+    //     fnSystem.delay(5000);
+    //     init_cpm(9600);
+    //     cpmActive = true;
+    //     break;
+    // default:
+    //     sio_nak();
+    //     break;
+    // }
 }
 
-void sioCPM::sio_process(uint32_t commanddata, uint8_t checksum)
-{
-    cmdFrame.commanddata = commanddata;
-    cmdFrame.checksum = checksum;
-
-    switch (cmdFrame.comnd)
-    {
-    case 'G':
-        sio_ack();
-        fnSystem.delay(10);
-        sio_complete();
-        fnSystem.delay(5000);
-        init_cpm(9600);
-        cpmActive = true;
-        break;
-    default:
-        sio_nak();
-        break;
-    }
-}
-
-#endif /* BUILD_ATARI */
+#endif /* BUILD_COCO */
