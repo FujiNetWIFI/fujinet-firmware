f308742b062cafc7f143740de93e5a11415c6598
mozzwald <mozzwald@gmail.com>
2023-07-08 09:54:33 -0500

    pack firmware: cleanup & reorder steps

--- build_firmwarezip.py
+++ build_firmwarezip.py
@@ -11,29 +11,7 @@ from datetime import datetime
 from zipfile import ZipFile
 
 def makezip(source, target, env):
-    # Create the 'firmware' output dir if it doesn't exist
-    if not os.path.exists('firmware'):
-        os.makedirs('firmware')
-
-    # Clean the firmware output dir
-    folder = 'firmware'
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-
-    # Get the build_board variable
-    config = configparser.ConfigParser()
-    config.read('platformio.ini')
-    environment = "env:"+config['fujinet']['build_board'].split()[0]
-    print(f"Creating firmware zip for FujiNet ESP32 Board: {config[environment]['board']}")
-
-    # Make sure all the files are built
+    # Make sure all the files are built and ready to zip
     zipit = True
     if not os.path.exists(env.subst("$BUILD_DIR/bootloader.bin")):
         print("BOOTLOADER not available to pack in firmware zip")
@@ -48,11 +26,14 @@ def makezip(source, target, env):
         print("SPIFFS not available to pack in firmware zip, run \"Build Filesystem Image\" first")
         zipit = False
 
-    if zipit == False:
-        print("Skipping making firmware ZIP due to error")
-
     if zipit == True:
-        # {'FN_VERSION_MAJOR': '0', 'FN_VERSION_MINOR': '5', 'FN_VERSION_BUILD': '63d992c8', 'FN_VERSION_DATE': '2023-05-07 08:00:00', 'FN_VERSION_FULL': 'v1.0'}
+        # Get the build_board variable
+        config = configparser.ConfigParser()
+        config.read('platformio.ini')
+        environment = "env:"+config['fujinet']['build_board'].split()[0]
+        print(f"Creating firmware zip for FujiNet ESP32 Board: {config[environment]['board']}")
+
+        # Get version information
         with open("include/version.h", "r") as file:
             version_content = file.read()
         defines = re.findall(r'#define\s+(\w+)\s+"?([^"\n]+)"?\n', version_content)
@@ -63,10 +44,11 @@ def makezip(source, target, env):
             value = define[1]
             version[name] = value
 
-        # get commit msg. needs to be cleaned for JSON before using!
+        # Get and clean the current commit message
         try:
             version_desc = subprocess.check_output(["git", "log", "-1", "--pretty=%B"], universal_newlines=True).strip().replace('"', '')
         except subprocess.CalledProcessError as e:
+            # Revert to full version if no commit msg or error
             version_desc = version['FN_VERSION_FULL']
 
         try:
@@ -78,86 +60,109 @@ def makezip(source, target, env):
         version['FN_VERSION_BUILD'] = version_build
         version['BUILD_DATE'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+        # Create the 'firmware' output dir if it doesn't exist
+        firmdir = 'firmware'
+        if not os.path.exists(firmdir):
+            os.makedirs(firmdir)
+
+        # Clean the firmware output dir
+        releasefile = firmdir+"/release.json"
+        firmwarezip = firmdir+"/fujinet-"+config['fujinet']['build_platform'].split("_")[1]+"-"+version['FN_VERSION_FULL']+".zip"
+        try:
+            if os.path.isfile(releasefile):
+                os.unlink(releasefile)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (releasefile, e))
+        try:
+            if os.path.isfile(firmwarezip):
+                os.unlink(firmwarezip)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (firmwarezip, e))
+
+        # Create release JSON
         json_contents = """{
-            "version": "%s",
-            "version_date": "%s",
-            "build_date": "%s",
-            "description": "%s",
-            "git_commit": "%s",
-            "files":
-            [
-            """ % (version['FN_VERSION_FULL'], version['FN_VERSION_DATE'], version['BUILD_DATE'], version['FN_VERSION_DESC'], version['FN_VERSION_BUILD'])
+    "version": "%s",
+    "version_date": "%s",
+    "build_date": "%s",
+    "description": "%s",
+    "git_commit": "%s",
+    "files":
+    [
+""" % (version['FN_VERSION_FULL'], version['FN_VERSION_DATE'], version['BUILD_DATE'], version['FN_VERSION_DESC'], version['FN_VERSION_BUILD'])
 
         if config[environment]['board'] == "fujinet-v1":
-            json_contents += """		{
-                            "filename": "bootloader.bin",
-                            "offset": "0x1000"
-                        },
-                        {
-                            "filename": "partitions.bin",
-                            "offset": "0x8000"
-                        },
-                        {
-                            "filename": "firmware.bin",
-                            "offset": "0x10000"
-                        },
-                        {
-                            "filename": "spiffs.bin",
-                            "offset": "0x910000"
-                        }
-                    ]
-                }"""
+            json_contents += """        {
+            "filename": "bootloader.bin",
+            "offset": "0x1000"
+        },
+        {
+            "filename": "partitions.bin",
+            "offset": "0x8000"
+        },
+        {
+            "filename": "firmware.bin",
+            "offset": "0x10000"
+        },
+        {
+            "filename": "spiffs.bin",
+            "offset": "0x910000"
+        }
+    ]
+}
+"""
         elif config[environment]['board'] == "fujinet-v1-8mb":
-            json_contents += """		{
-                            "filename": "bootloader.bin",
-                            "offset": "0x1000"
-                        },
-                        {
-                            "filename": "partitions.bin",
-                            "offset": "0x8000"
-                        },
-                        {
-                            "filename": "firmware.bin",
-                            "offset": "0x10000"
-                        },
-                        {
-                            "filename": "spiffs.bin",
-                            "offset": "0x600000"
-                        }
-                    ]
-                }"""
+            json_contents += """        {
+            "filename": "bootloader.bin",
+            "offset": "0x1000"
+        },
+        {
+            "filename": "partitions.bin",
+            "offset": "0x8000"
+        },
+        {
+            "filename": "firmware.bin",
+            "offset": "0x10000"
+        },
+        {
+            "filename": "spiffs.bin",
+            "offset": "0x600000"
+        }
+    ]
+}
+"""
         elif config[environment]['board'] == "fujinet-v1-4mb":
-            json_contents += """		{
-                    "filename": "bootloader.bin",
-                    "offset": "0x1000"
-                },
-                {
-                    "filename": "partitions.bin",
-                    "offset": "0x8000"
-                },
-                {
-                    "filename": "firmware.bin",
-                    "offset": "0x10000"
-                },
-                {
-                    "filename": "spiffs.bin",
-                    "offset": "0x250000"
-                }
-            ]
-        }"""
+            json_contents += """        {
+            "filename": "bootloader.bin",
+            "offset": "0x1000"
+        },
+        {
+            "filename": "partitions.bin",
+            "offset": "0x8000"
+        },
+        {
+            "filename": "firmware.bin",
+            "offset": "0x10000"
+        },
+        {
+            "filename": "spiffs.bin",
+            "offset": "0x250000"
+        }
+    ]
+}
+"""
 
         # Save Release JSON
-        # print(json_contents)
         with open('firmware/release.json', 'w') as f:
             f.write(json_contents)
 
         # Create the ZIP File
-        firmwarezip = "firmware/fujinet-"+config['fujinet']['build_platform'].split("_")[1]+"-"+version['FN_VERSION_FULL']+".zip"
         with ZipFile(firmwarezip, 'w') as zip_object:
             zip_object.write(env.subst("$BUILD_DIR/bootloader.bin"), "bootloader.bin")
             zip_object.write(env.subst("$BUILD_DIR/partitions.bin"), "partitions.bin")
             zip_object.write(env.subst("$BUILD_DIR/firmware.bin"), "firmware.bin")
             zip_object.write(env.subst("$BUILD_DIR/spiffs.bin"), "spiffs.bin")
             zip_object.write("firmware/release.json", "release.json")
+    else:
+        print("Skipping making firmware ZIP due to error")
 
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", makezip)
