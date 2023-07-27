cce1e7f66e0de258f3ae2ddfd5d8d09851b08211
jagmod <scott@jagmod.com>
2023-07-27 18:10:54 -0500

    cppcheck findings: passing std::string by const ref whenever possible

--- lib/ftp/fnFTP.h
+++ lib/ftp/fnFTP.h
@@ -42,7 +42,7 @@ public:
      * @param port port to login (default 21)
      * @return TRUE on error, FALSE on success
      */
-    bool login(string _username, string _password, string _hostname, unsigned short _port = 21);
+    bool login(const string &_username, const string &_password, const string &_hostname, unsigned short _port = 21);
 
     /**
      * Log out of FTP server, closes control connection.
