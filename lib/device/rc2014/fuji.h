7f3846999b8b993d59fdd8db0622580ea450cd4f
Justin Skists <justin.skists@juzza.co.uk>
2023-08-15 10:16:29 +0100

    RC2014: fuji: align SSID size with other platforms
    The RC2014 does not match the other platforms with the size of the SSID array. Fix this.

--- lib/device/rc2014/fuji.h
+++ lib/device/rc2014/fuji.h
@@ -24,7 +24,7 @@
 
 typedef struct
 {
-    char ssid[32];
+    char ssid[MAX_SSID_LEN+1];
     char hostname[64];
     unsigned char localIP[4];
     unsigned char gateway[4];
@@ -33,7 +33,7 @@ typedef struct
     unsigned char macAddress[6];
     unsigned char bssid[6];
     char fn_version[15];
-} AdapterConfig;
+} __attribute__((packed)) AdapterConfig;
 
 enum appkey_mode : uint8_t
 {
