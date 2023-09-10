9d6a9dbe56d3898499960d783855c720c8c1ec83
Jeff Piepmeier <jeff.piepmeier@gmail.com>
2023-09-09 20:00:43 -0400

    link at 2mbps

--- pico/mac/commands.c
+++ pico/mac/commands.c
@@ -36,7 +36,7 @@ void pio_latch(PIO pio, uint sm, uint offset, uint in_pin, uint out_pin);
 void pio_mux(PIO pio, uint sm, uint offset, uint in_pin, uint mux_pin);
 
 #define UART_ID uart1
-#define BAUD_RATE 1000000 //230400 //115200
+#define BAUD_RATE 2000000 //230400 //115200
 #define DATA_BITS 8
 #define STOP_BITS 1
 #define PARITY UART_PARITY_NONE
