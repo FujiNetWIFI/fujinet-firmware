3f489704567292228c9754b37d5aff50b8dee0a9
Thomas <thom.cherryhomes@gmail.com>
2023-10-05 21:13:05 -0500

    Add 'E' error command

--- lib/device/adamnet/network.h
+++ lib/device/adamnet/network.h
@@ -53,6 +53,11 @@ public:
     bool interruptProceed = false;
 
     /**
+     * @brief called to return the extended error number from a protocol adapter
+     */
+    virtual void get_error();
+
+    /**
      * Called for ADAM Command 'O' to open a connection to a network protocol, allocate all buffers,
      * and start the receive PROCEED interrupt.
      */
