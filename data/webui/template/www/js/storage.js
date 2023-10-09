45a0199131dd18c6b2fbcab4f61d945a510b7ef7
mozzwald <mozzwald@gmail.com>
2023-10-09 14:19:59 -0500

    atari: cpm: webui: implement disable cpm option

--- data/webui/template/www/js/storage.js
+++ data/webui/template/www/js/storage.js
@@ -14,7 +14,7 @@ var Piechart = function(options)
     this.canvas = options.canvas;
     this.ctx = this.canvas.getContext("2d");
     this.colors = options.colors;
- 
+
     this.draw = function(){
         var total_value = 0;
         var color_index = 0;
@@ -22,12 +22,12 @@ var Piechart = function(options)
             var val = this.options.data[categ];
             total_value += val;
         }
- 
+
         var start_angle = 0;
         for (categ in this.options.data){
             val = this.options.data[categ];
             var slice_angle = 2 * Math.PI * val / total_value;
- 
+
             drawPieSlice(
                 this.ctx,
                 this.canvas.width/2,
@@ -37,11 +37,11 @@ var Piechart = function(options)
                 start_angle+slice_angle,
                 this.colors[color_index%this.colors.length]
             );
- 
+
             start_angle += slice_angle;
             color_index++;
         }
- 
+
     }
 }
 
