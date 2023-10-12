be9c9f28389cf541473299fa295320f4c44eb672
Thomas <thom.cherryhomes@gmail.com>
2023-10-12 12:52:38 -0500

    [adam] raise mainloop priority.

--- src/main.cpp
+++ src/main.cpp
@@ -351,7 +351,11 @@ extern "C"
 // Create a new high-priority task to handle the main loop
 // This is assigned to CPU1; the WiFi task ends up on CPU0
 #define MAIN_STACKSIZE 32768
+#ifdef BUILD_ADAM
+#define MAIN_PRIORITY 30
+#else
 #define MAIN_PRIORITY 17
+#endif
 #define MAIN_CPUAFFINITY 1
 
         xTaskCreatePinnedToCore(fn_service_loop, "fnLoop",
