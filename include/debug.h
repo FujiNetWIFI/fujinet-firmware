c811c51d6ecdda4a2429ed8a4b9764d6a0ca5216
Jan Krupa <apc.atari@gmail.com>
2023-11-09 17:59:42 +0100

    [PC] merge lib/utils debug.h pinmap.h

--- include/debug.h
+++ include/debug.h
@@ -6,7 +6,10 @@
 #include "ansi_codes.h"
 
 // __PLATFORMIO_BUILD_DEBUG__ is set when build_type is set to debug in platformio.ini
-#if defined(__PLATFORMIO_BUILD_DEBUG__) || defined(DBUG2)
+// __PC_BUILD_DEBUG__ is set when build_type is set Debug in in fujinet_pc.cmake
+// DBUG2 is set to enable monitor messages for a release build
+//       in platformio.ini for ESP build, in fujinet_pc.cmake for PC build
+#if defined(__PLATFORMIO_BUILD_DEBUG__) || defined(__PC_BUILD_DEBUG__) || defined(DBUG2)
 #define DEBUG
 #endif
 
@@ -14,21 +17,34 @@
 #undef DEBUG
 #endif
 
-#include "../lib/hardware/fnUART.h"
 
 
 /*
   Debugging Macros
 */
 #ifdef DEBUG
+#ifdef ESP_PLATFORM
     // Use FujiNet debug serial output
+    #include "../lib/hardware/fnUART.h"
+
     #define Debug_print(...) fnUartDebug.print( __VA_ARGS__ )
     #define Debug_printf(...) fnUartDebug.printf( __VA_ARGS__ )
     #define Debug_println(...) fnUartDebug.println( __VA_ARGS__ )
     #define Debug_printv(format, ...) {fnUartDebug.printf( ANSI_YELLOW "[%s:%u] %s(): " ANSI_GREEN_BOLD format ANSI_RESET "\r\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);}
 
     #define HEAP_CHECK(x) Debug_printf("HEAP CHECK %s " x "\r\n", heap_caps_check_integrity_all(true) ? "PASSED":"FAILED")
+#else
+    // Use util_debug_printf() helper function
+    #include <utils.h>
+
+    #define Debug_print(...) util_debug_printf(nullptr, __VA_ARGS__)
+    #define Debug_printf(...) util_debug_printf(__VA_ARGS__)
+    #define Debug_println(...) util_debug_printf("%s\n", __VA_ARGS__)
+    #define Debug_printv(format, ...) {util_debug_printf( ANSI_YELLOW "[%s:%u] %s(): " ANSI_GREEN_BOLD format ANSI_RESET "\r\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);}
+
+    #define HEAP_CHECK(x) Debug_printf("HEAP CHECK %s " x "\r\n", heap_caps_check_integrity_all(true) ? "PASSED":"FAILED")
 #endif
+#endif // DEBUG
 
 #ifndef DEBUG
     #define Debug_print(...)
@@ -37,6 +53,6 @@
     #define Debug_printv(format, ...)
 
     #define HEAP_CHECK(x)
-#endif
+#endif // !DEBUG
 
 #endif // _DEBUG_H_
