256af9738b6cd67234eafb3ca42434ea916a7131
Thomas <thom.cherryhomes@gmail.com>
2023-12-08 19:37:26 -0600

    [coco] implement DSK media type.

--- lib/device/drivewire/disk.cpp
+++ lib/device/drivewire/disk.cpp
@@ -28,8 +28,37 @@ void drivewireDisk::drivewire_process(uint32_t commanddata, uint8_t checksum)
 }
 
 mediatype_t drivewireDisk::mount(FILE *f, const char *filename, uint32_t disksize, mediatype_t disk_type)
-{   
-    return MEDIATYPE_UNKNOWN;
+{
+    mediatype_t mt = MEDIATYPE_UNKNOWN;
+
+    Debug_printf("disk MOUNT %s\n", filename);
+
+    // Destroy any existing MediaType
+    if (_media != nullptr)
+    {
+        delete _media;
+        _media = nullptr;
+    }
+
+    // Determine MediaType based on filename extension
+    if (disk_type == MEDIATYPE_UNKNOWN && filename != nullptr)
+        disk_type = MediaType::discover_mediatype(filename);
+
+    switch (disk_type)
+    {
+    case MEDIATYPE_DSK:
+        _media = new MediaTypeDSK();
+        _media->_media_host = host;
+        strcpy(_media->_disk_filename,filename);
+        mt = _media->mount(f, disksize);
+        device_active = true;
+        break;
+    default:
+        device_active = false;
+        break;
+    }
+
+    return mt; 
 }
     
 void drivewireDisk::unmount()
--- lib/device/drivewire/disk.h
+++ lib/device/drivewire/disk.h
@@ -8,7 +8,7 @@
 class drivewireDisk : public virtualDevice
 {
 private:
-    MediaType *_disk = nullptr;
+    MediaType *_media = nullptr;
 
     void drivewire_process(uint32_t commanddata, uint8_t checksum);
 
@@ -18,7 +18,7 @@ public:
 
     fujiHost *host = nullptr;
 
-    mediatype_t disktype() { return _disk == nullptr ? MEDIATYPE_UNKNOWN : _disk->_mediatype; };
+    mediatype_t disktype() { return _media == nullptr ? MEDIATYPE_UNKNOWN : _media->_mediatype; };
     mediatype_t mount(FILE *f, const char *filename, uint32_t disksize, mediatype_t disk_type = MEDIATYPE_UNKNOWN);
     void unmount();
 };
