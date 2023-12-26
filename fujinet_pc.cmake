9e4541a25a6c3f0cb163a2bb89abbb2c4e66cc5b
Jan Krupa <apc.atari@gmail.com>
2023-12-26 18:22:07 +0100

    [PC] merge TCP protocol adapter

--- fujinet_pc.cmake
+++ fujinet_pc.cmake
@@ -151,7 +151,7 @@ set(SOURCES
     lib/network-protocol/Protocol.h lib/network-protocol/Protocol.cpp
     lib/network-protocol/ProtocolParser.h lib/network-protocol/ProtocolParser.cpp
     lib/network-protocol/Test.h lib/network-protocol/Test.cpp
-#     lib/network-protocol/TCP.h lib/network-protocol/TCP.cpp
+    lib/network-protocol/TCP.h lib/network-protocol/TCP.cpp
 #     lib/network-protocol/UDP.h lib/network-protocol/UDP.cpp
 #     lib/network-protocol/Telnet.h lib/network-protocol/Telnet.cpp
 #     lib/network-protocol/FS.h lib/network-protocol/FS.cpp
