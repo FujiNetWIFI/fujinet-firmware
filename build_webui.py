2c053cd205d5ba3091a442aad053a66a9953ac83
Jan Krupa <apc.atari@gmail.com>
2024-01-08 00:07:49 +0100

    [PC] merge build_webui.py

--- build_webui.py
+++ build_webui.py
@@ -4,49 +4,72 @@ import os, glob, re, shutil, configparser
 from jinja2 import Environment, FileSystemLoader
 from yaml import load, Loader
 
-def prep_dst(fname, build_platform, prefix):
+def prep_dst(fname, build_platform, prefix, data_dir=None):
     rel_path = fname.replace(prefix, '')
-    destination = os.path.join('data', build_platform, rel_path)
+    if data_dir is None:
+        destination = os.path.join('data', build_platform, rel_path) # e.g. data/BUILD_APPLE/...
+    else:
+        destination = os.path.join(data_dir, rel_path) # e.g. /fujinet-pc/build/windows-x64/APPLE/data/...
     dest_dir = os.path.dirname(destination)
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
     return destination
 
 
-def process_template(fname, build_platform, template_env, config, prefix):
+def process_template(fname, build_platform, template_env, config, prefix, build_data_dir=None):
     print(f"processing template file {fname}")
     # jinja2 insists on '/' as the path separator even on windows. see https://github.com/pallets/jinja/issues/767
     template = template_env.get_template(fname.replace(prefix, '').replace('\\', '/'))
     r = template.render(config)
-    destination = prep_dst(fname, build_platform, prefix).replace('.tmpl.', '.')
+    destination = prep_dst(fname, build_platform, prefix, build_data_dir).replace('.tmpl.', '.')
     with open(destination, 'w') as f:
         f.write(r)
 
-def copy_file(fname, build_platform, prefix):
-    destination = prep_dst(fname, build_platform, prefix)
+def copy_file(fname, build_platform, prefix, build_data_dir=None):
+    destination = prep_dst(fname, build_platform, prefix, build_data_dir)
     shutil.copy(fname, destination)
 
-Import("env")
+# collect environment variables
+build_board = os.environ.get("FUJINET_BUILD_BOARD")
+build_platform = os.environ.get("FUJINET_BUILD_PLATFORM")
+build_data_dir = os.environ.get("BUILD_DATA_DIR")
 
-# print(env.Dump())
+try:
+    Import("env")
+    # print(env.Dump())
+except NameError:
+    print("Running build_webui.py outside the PlatformIO environment.")
+    env = None
 
-target = env["PIOENV"]
-# PROGRAM_ARGS is a list of args provided by pio with "-a" switch
-if 'dev' in env["PROGRAM_ARGS"]:
-    target = "dev"
+if env is not None:
+    if build_board is None:
+        build_board = env["PIOENV"]
+        # PROGRAM_ARGS is a list of args provided by pio with "-a" switch
+    if 'dev' in env["PROGRAM_ARGS"]:
+        build_board = "dev"
+
+    if build_platform is None:
+        pio_config = configparser.ConfigParser()
+        pio_config.read('platformio.ini')
+        build_platform = pio_config['fujinet']['build_platform']
+
+if build_data_dir is None:
+    build_data_dir = f"data/{build_platform}"
+
+print(f"Building webUI into {build_data_dir}")
+print(f"  build_platform: {build_platform}")
+print(f"  build_board: {build_board}")
 
 template_env = Environment(loader=FileSystemLoader("data/webui/template"))
-config = load(open(os.path.join('data', 'webui', 'config', f'{target}.yaml')), Loader=Loader)
-
-pio_config = configparser.ConfigParser()
-pio_config.read('platformio.ini')
-build_platform = pio_config['fujinet']['build_platform']
-print(f"Building webUI into data/{build_platform}")
+config = load(open(os.path.join('data', 'webui', 'config', f'{build_board}.yaml')), Loader=Loader)
 
 if not build_platform.startswith('BUILD_'):
     raise Exception(f"build_platform does not match BUILD_*, aborting")
 
-data_build_platform_path = os.path.join("data", build_platform)
+if build_data_dir is None:
+    data_build_platform_path = os.path.join("data", build_platform)
+else:
+    data_build_platform_path = build_data_dir
 if (os.path.isdir(data_build_platform_path)):
     shutil.rmtree(data_build_platform_path)
 
@@ -54,7 +77,7 @@ if (os.path.isdir(data_build_platform_path)):
 common_prefix = os.path.join('data', 'webui', 'common', '')
 for filename in glob.iglob(f'{common_prefix}**', recursive=True):
     if os.path.isfile(filename):
-        copy_file(filename, build_platform, common_prefix)
+        copy_file(filename, build_platform, common_prefix, build_data_dir)
 
 # copy template files, rendering if name matches *.tmpl.*
 template_matcher = re.compile(r'^.*\.tmpl\.[a-zA-Z0-9_]+$')
@@ -62,9 +85,9 @@ webui_template_prefix = os.path.join('data', 'webui', 'template', '')
 for filename in glob.iglob(f'{webui_template_prefix}**', recursive=True):
     if os.path.isfile(filename):
         if (template_matcher.search(filename)):
-            process_template(filename, build_platform, template_env, config, webui_template_prefix)
+            process_template(filename, build_platform, template_env, config, webui_template_prefix, build_data_dir)
         else:
-            copy_file(filename, build_platform, webui_template_prefix)
+            copy_file(filename, build_platform, webui_template_prefix, build_data_dir)
 
 # copy additional files from appropriate BUILD_* data dir, which is stored in the ini file under fujinet.build_platform
 # if there are file clashes, these will override the above, so it allows for device specific overrides
@@ -72,4 +95,4 @@ for filename in glob.iglob(f'{webui_template_prefix}**', recursive=True):
 dev_specific_prefix = os.path.join('data', 'webui', 'device_specific', build_platform, '')
 for filename in glob.iglob(f"{dev_specific_prefix}**", recursive=True):
     if os.path.isfile(filename) and filename != '.keep':
-        copy_file(filename, build_platform, dev_specific_prefix)
+        copy_file(filename, build_platform, dev_specific_prefix, build_data_dir)
