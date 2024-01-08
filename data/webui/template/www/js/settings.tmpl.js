c21931d5f79eac8aff77b1c17731af821872b671
Jan Krupa <apc.atari@gmail.com>
2024-01-08 17:21:23 +0100

    [PC] merge webui/template files

--- data/webui/template/www/js/settings.tmpl.js
+++ data/webui/template/www/js/settings.tmpl.js
@@ -59,6 +59,41 @@ function setPrinterPort(printer_port) {
 	}
 }
 
+{% if components.serial_port %}
+function setSerialCommand(serial_command) {
+	// Serial Command is triple/quadruplevalue (hide 0 for webui).
+	const cmd1E = document.getElementById("command-on-dsr");
+	const cmd2E = document.getElementById("command-on-cts");
+	const cmd3E = document.getElementById("command-on-ri");
+	if (cmd1E == null || cmd2E == null || cmd3E == null) return;
+
+	if (serial_command == "1") {
+		cmd1E.checked = true;
+	} else if (serial_command == "2") {
+		cmd2E.checked = true;
+	} else if (serial_command == "3") {
+		cmd3E.checked = true;
+	} else {
+		console.log("ERROR: unknown serial_command value:", serial_command);
+	}
+}
+
+function setSerialProceed(serial_proceed) {
+	// Serial Proceed is double/triple value (hide 0 from webui).
+	const prc1E = document.getElementById("proceed-on-dtr");
+	const prc2E = document.getElementById("proceed-on-rts");
+	if (prc1E == null || prc2E == null ) return;
+
+	if (serial_proceed == "1") {
+		prc1E.checked = true;
+	} else if (serial_proceed == "2") {
+		prc2E.checked = true;
+	} else {
+		console.log("ERROR: unknown serial_proceed value:", serial_proceed);
+	}
+}
+{% endif %}
+
 {% if components.printer_settings %}
 setInputValue(current_printer_enabled == 1, "printer-virt-yes", "printer-virt-no");
 setPrinterPort(current_printerport);
@@ -76,7 +111,9 @@ selectListValue("select_hsioindex", current_hsioindex);
 
 {% if components.program_recorder %}
 setInputValue(current_cassette_enabled == 1, "pr-virt-yes", "pr-virt-no");
+{% if not tweaks.fujinet_pc %}
 setInputValue(current_play_record == "0 PLAY", "pr-mode-play", "pr-mode-rec");
+{% endif %}
 setInputValue(current_pulldown == "0 B Button Press", "pr-act-b", "pr-act-pull");
 {% endif %}
 
@@ -97,4 +134,13 @@ setInputValue(current_apetime == 1, "tz-apetime-yes", "tz-apetime-no");
 
 {% if components.cpm_settings %}
 setInputValue(current_cpm_enabled == 1, "cpm-virt-yes", "cpm-virt-no");
+{% endif %}
+
+{% if components.serial_port %}
+setSerialCommand(current_serial_command);
+setSerialProceed(current_serial_proceed);
+{% endif %}
+
+{% if components.emulator_settings %}
+setInputValue(current_netsio_enabled == 1, "netsio-yes", "netsio-no");
 {% endif %}
