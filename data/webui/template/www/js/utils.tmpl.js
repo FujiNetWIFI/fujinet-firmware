db8580d5d855eeee255e06dbb02fa582c0d0214f
Jan Krupa <apc.atari@gmail.com>
2024-01-08 17:21:23 +0100

    [PC] merge webui/template files

--- data/webui/template/www/js/utils.tmpl.js
+++ data/webui/template/www/js/utils.tmpl.js
@@ -21,9 +21,33 @@ function writeUptimeString(secs, id) {
 		result += (mins % 60) + ' minutes, ';
 	if (secs % 60)
 		result += (secs % 60) + ' seconds';
+	else if (!secs)
+		result = '0 seconds';
 
 	const e = document.getElementById(id);
 	if (e !== null) {
 		e.innerText = result;
 	}
 }
+
+{% if tweaks.fujinet_pc %}
+function restartButton() {
+	var btn = document.getElementById("restartButton");
+	if (btn.value == "Confirm")
+		window.location.assign("/restart");
+	if (btn.value == "Restart...")
+		btn.value = "Confirm";
+		setTimeout(function(){
+			btn.value = "Restart...";
+		}, 2000);
+}
+
+function swapButton() {
+	window.location.assign("/swap?redirect=1");
+}
+
+function mountAllButton() {
+	window.location.assign("/mount?mountall=1&redirect=1");
+}
+{% endif %}
+
