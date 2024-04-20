e0a89fe17b04469d3effd402c2e90c73bc8a7548
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-04-20 18:17:54 -0500

    [all][fuji] temporarily remove url decode from psk.

--- lib/device/rs232/fuji.cpp
+++ lib/device/rs232/fuji.cpp
@@ -191,8 +191,8 @@ void rs232Fuji::rs232_net_set_ssid()
         bool save = cmdFrame.aux1 != 0;
 
         // URL Decode SSID/PASSWORD to handle special chars
-        mstr::urlDecode(cfg.ssid, sizeof(cfg.ssid));
-        mstr::urlDecode(cfg.password, sizeof(cfg.password));
+        //mstr::urlDecode(cfg.ssid, sizeof(cfg.ssid));
+        //mstr::urlDecode(cfg.password, sizeof(cfg.password));
 
         Debug_printf("Connecting to net: %s password: %s\n", cfg.ssid, cfg.password);
 
