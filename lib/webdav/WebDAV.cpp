1c10132243c7a75a2942bd81499d873845c2b445
Jan Krupa <apc.atari@gmail.com>
2024-05-18 11:39:43 +0200

    [WebDAV] ignore element namespace

--- lib/webdav/WebDAV.cpp
+++ lib/webdav/WebDAV.cpp
@@ -6,27 +6,34 @@
 
 #include <cstring>
 
+#include "../../include/debug.h"
+
+// check if element el is matching pattern *:pat (i.e. ends with ":"+pat)
+#define IS_ANYNS_ELEMENT(pat, el, el_len) (el_len >= sizeof(pat) && strcmp(el+el_len-sizeof(pat), ":" pat) == 0)
 
 void WebDAV::Start(const XML_Char *el, const XML_Char **attr)
 {
-    if (strcmp(el, "D:response") == 0)
+    Debug_printf("WebDAV::Start(%s, )\n", el);
+    size_t el_len = strlen(el);
+    if (IS_ANYNS_ELEMENT("response", el, el_len))
         insideResponse = true;
-    else if (strcmp(el, "D:displayname") == 0)
+    else if (IS_ANYNS_ELEMENT("displayname", el, el_len))
         insideDisplayName = true;
-    else if (strcmp(el, "D:getcontentlength") == 0)
+    else if (IS_ANYNS_ELEMENT("getcontentlength", el, el_len))
         insideGetContentLength = true;
 }
 
 void WebDAV::End(const XML_Char *el)
 {
-    if (strcmp(el, "D:response") == 0)
+    size_t el_len = strlen(el);
+    if (IS_ANYNS_ELEMENT("response", el, el_len))
     {
         insideResponse = false;
         entries.push_back(currentEntry);
     }
-    else if (strcmp(el, "D:displayname") == 0)
+    else if (IS_ANYNS_ELEMENT("displayname", el, el_len))
         insideDisplayName = false;
-    else if (strcmp(el, "D:getcontentlength") == 0)
+    else if (IS_ANYNS_ELEMENT("getcontentlength", el, el_len))
         insideGetContentLength = false;
 }
 
