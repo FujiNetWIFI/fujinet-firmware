eb7202c262a85229a802fe6f102eb714336943dc
Gennaro Tortone <gennaro.tortone@na.infn.it>
2024-07-29 23:18:25 +0200

    migrate fnTimer to gptimer

--- lib/hardware/fnHardwareTimer.h
+++ lib/hardware/fnHardwareTimer.h
@@ -2,12 +2,23 @@
 #define FNHWTIMER_H
 
 #include "esp_idf_version.h"
-#include "driver/timer.h" // contains the hardware timer register data structure
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 0, 0)
+#include "driver/timer.h"
+#else
+#include "driver/gptimer.h"
+#endif
 #include "soc/soc.h"
 #include "soc/timer_group_struct.h"
+#include "soc/clk_tree_defs.h"
 
 // hardware timer parameters for bit-banging I/O
+
+#if defined(CONFIG_IDF_TARGET_ESP32)
 #define TIMER_DIVIDER         (2)  //  Hardware timer clock divider
+#elif defined(CONFIG_IDF_TARGET_ESP32S3)
+#define TIMER_DIVIDER         (8)  //  Hardware timer clock divider
+#endif
+
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
 #define TIMER_SCALE           (APB_CLK_FREQ / TIMER_DIVIDER)  // convert counter value to seconds
 #else
@@ -25,18 +36,24 @@ private:
     uint32_t t0;
   } fn_timer;
 
-timer_config_t fn_config;
+gptimer_handle_t gptimer;
+gptimer_config_t fn_config;
+//gptimer_alarm_config_t alarm_config;
 
 public:
   void config();
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
-  void reset() { TIMERG1.hw_timer[TIMER_1].loadlo.val = 0; TIMERG1.hw_timer[TIMER_1].load.val = 0; };
-  void latch() { TIMERG1.hw_timer[TIMER_1].update.val = 0; };
-  void read() { fn_timer.t0 = TIMERG1.hw_timer[TIMER_1].lo.val; };
-#else
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 0, 0)
   void reset() { TIMERG1.hw_timer[TIMER_1].load_low = 0; TIMERG1.hw_timer[TIMER_1].reload = 0; };
   void latch() { TIMERG1.hw_timer[TIMER_1].update = 0; };
   void read() { fn_timer.t0 = TIMERG1.hw_timer[TIMER_1].cnt_low; };
+#else
+  void reset() { gptimer_set_raw_count(gptimer, 0); };
+  void latch() {};
+  void read() { 
+    uint64_t count; 
+    gptimer_get_raw_count(gptimer, &count);
+    fn_timer.t0 = count & 0xFFFFFFFF;
+  };
 #endif
   bool timeout() { return (fn_timer.t0 > fn_timer.tn); };
   void wait() { do{latch(); read();} while (!timeout()); };
