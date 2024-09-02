3bb7d189cc07004f805541090594b4d491f274d6
Jaime Idolpx <jaime@idolpx.com>
2024-09-02 00:51:15 -0400

    [iec] properly release lines after TALK

--- lib/bus/iec/protocol/_protocol.cpp
+++ lib/bus/iec/protocol/_protocol.cpp
@@ -51,113 +51,113 @@ void IECProtocol::timer_stop()
 }
 
 
-int16_t IRAM_ATTR IECProtocol::timeoutWait(uint8_t pin, bool target_status, size_t wait_us, bool watch_atn)
-{
-    IEC.pull ( PIN_IEC_SRQ );
-    uint64_t start = esp_timer_get_time();
-    uint64_t current = 0;
-    timer_start( wait_us );
-
-#ifndef IEC_SPLIT_LINES
-    IEC.release ( pin );
-#endif
-
-    while ( !timer_timedout )
-    {
-        IEC.pull ( PIN_IEC_SRQ );
-        if ( IEC.status ( pin ) == target_status )
-        {
-            timer_stop();
-            current = esp_timer_get_time();
-            IEC.release ( PIN_IEC_SRQ );
-            return ( current - start );
-        }
-        usleep( 2 );
-        if ( watch_atn )
-        {
-            if ( IEC.status ( PIN_IEC_ATN ) )
-            {
-                IEC.release ( PIN_IEC_SRQ );
-                return -1;
-            }
-        }
-        IEC.release ( PIN_IEC_SRQ );
-        usleep( 2 );
-    }
-    IEC.release ( PIN_IEC_SRQ );
-    return wait_us;
-}
-
 // int16_t IRAM_ATTR IECProtocol::timeoutWait(uint8_t pin, bool target_status, size_t wait_us, bool watch_atn)
 // {
-//     uint64_t start = 0;
+//     IEC.pull ( PIN_IEC_SRQ );
+//     uint64_t start = esp_timer_get_time();
 //     uint64_t current = 0;
-//     uint64_t elapsed = 0;
-//     bool atn_status = false;
+//     timer_start( wait_us );
 
 // #ifndef IEC_SPLIT_LINES
 //     IEC.release ( pin );
 // #endif
 
-//     // Quick check to see if the target status is already set
-//     if ( IEC.status ( pin ) == target_status )
-//         return elapsed;
-
-//     if ( pin == PIN_IEC_ATN )
+//     while ( !timer_timedout )
 //     {
-//         watch_atn = false;
-//     }
-//     else
-//     {
-// #ifndef IEC_SPLIT_LINES
-//         IEC.release ( PIN_IEC_ATN );
-// #endif
-
-//         // Sample ATN and set flag to indicate COMMAND or DATA mode
-//         atn_status = IEC.status ( PIN_IEC_ATN );
-//         //if ( atn_status ) IEC.flags |= ATN_PULLED;
-//     }
-
-//     IEC.pull ( PIN_IEC_SRQ );
-//     start = esp_timer_get_time();
-//     while ( IEC.status ( pin ) != target_status )
-//     {
-//         current = esp_timer_get_time();
-//         elapsed = ( current - start );
-
-//         if ( elapsed >= wait_us && wait_us != FOREVER )
+//         IEC.pull ( PIN_IEC_SRQ );
+//         if ( IEC.status ( pin ) == target_status )
 //         {
+//             timer_stop();
+//             current = esp_timer_get_time();
 //             IEC.release ( PIN_IEC_SRQ );
-//             if ( wait_us == TIMEOUT_DEFAULT )
-//                 return -1;
-            
-//             return wait_us;
+//             return ( current - start );
 //         }
-
+//         usleep( 2 );
 //         if ( watch_atn )
 //         {
-//             bool atn_check = IEC.status ( PIN_IEC_ATN );
-//             if ( atn_check != atn_status )
+//             if ( IEC.status ( PIN_IEC_ATN ) )
 //             {
 //                 IEC.release ( PIN_IEC_SRQ );
-//                 //Debug_printv("pin[%d] state[%d] wait[%d] elapsed[%d]", pin, target_status, wait, elapsed);
 //                 return -1;
 //             }
 //         }
-
-//         if ( IEC.state < BUS_ACTIVE || elapsed > FOREVER )
-//         {
-//             // Something is messed up.  Get outta here.
-//             Debug_printv("wth? bus_state[%d]", IEC.state);
-//             Debug_printv("pin[%d] target_status[%d] wait[%d] elapsed[%d]", pin, target_status, wait_us, elapsed);
-//             return -1;
-//         }
+//         IEC.release ( PIN_IEC_SRQ );
+//         usleep( 2 );
 //     }
 //     IEC.release ( PIN_IEC_SRQ );
-
-//     // Debug_printv("pin[%d] state[%d] wait[%d] step[%d] t[%d]", pin, target_status, wait, elapsed);
-//     return elapsed;
+//     return wait_us;
 // }
+
+int16_t IRAM_ATTR IECProtocol::timeoutWait(uint8_t pin, bool target_status, size_t wait_us, bool watch_atn)
+{
+    uint64_t start = 0;
+    uint64_t current = 0;
+    uint64_t elapsed = 0;
+    bool atn_status = false;
+
+#ifndef IEC_SPLIT_LINES
+    IEC.release ( pin );
+#endif
+
+    // Quick check to see if the target status is already set
+    if ( IEC.status ( pin ) == target_status )
+        return elapsed;
+
+    if ( pin == PIN_IEC_ATN )
+    {
+        watch_atn = false;
+    }
+    else
+    {
+#ifndef IEC_SPLIT_LINES
+        IEC.release ( PIN_IEC_ATN );
+#endif
+
+        // Sample ATN and set flag to indicate COMMAND or DATA mode
+        atn_status = IEC.status ( PIN_IEC_ATN );
+        //if ( atn_status ) IEC.flags |= ATN_PULLED;
+    }
+
+    IEC.pull ( PIN_IEC_SRQ );
+    start = esp_timer_get_time();
+    while ( IEC.status ( pin ) != target_status )
+    {
+        current = esp_timer_get_time();
+        elapsed = ( current - start );
+
+        if ( elapsed >= wait_us && wait_us != FOREVER )
+        {
+            IEC.release ( PIN_IEC_SRQ );
+            if ( wait_us == TIMEOUT_DEFAULT )
+                return -1;
+            
+            return wait_us;
+        }
+
+        if ( watch_atn )
+        {
+            bool atn_check = IEC.status ( PIN_IEC_ATN );
+            if ( atn_check != atn_status )
+            {
+                IEC.release ( PIN_IEC_SRQ );
+                //Debug_printv("pin[%d] state[%d] wait[%d] elapsed[%d]", pin, target_status, wait, elapsed);
+                return -1;
+            }
+        }
+
+        if ( IEC.state < BUS_ACTIVE || elapsed > FOREVER )
+        {
+            // Something is messed up.  Get outta here.
+            Debug_printv("wth? bus_state[%d]", IEC.state);
+            Debug_printv("pin[%d] target_status[%d] wait[%d] elapsed[%d]", pin, target_status, wait_us, elapsed);
+            return -1;
+        }
+    }
+    IEC.release ( PIN_IEC_SRQ );
+
+    // Debug_printv("pin[%d] state[%d] wait[%d] step[%d] t[%d]", pin, target_status, wait, elapsed);
+    return elapsed;
+}
 
 bool IRAM_ATTR IECProtocol::wait(size_t wait_us, bool watch_atn)
 {
