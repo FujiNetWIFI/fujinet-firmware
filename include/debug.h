531d754c48b567802284592593e3d167a9f9f996
Jaime Idolpx <jaime@idolpx.com>
2025-02-07 20:10:39 -0500

    missed one edit

--- include/debug.h
+++ include/debug.h
@@ -39,7 +39,7 @@
 
     #define Debug_print(...) util_debug_printf(nullptr, __VA_ARGS__)
     #define Debug_printf(...) util_debug_printf(__VA_ARGS__)
-    #define Debug_println(...) util_debug_printf("%s\n", __VA_ARGS__)
+    #define Debug_println(...) util_debug_printf("%s\r\n", __VA_ARGS__)
     #define Debug_printv(format, ...) {util_debug_printf( ANSI_YELLOW "[%s:%u] %s(): " ANSI_GREEN_BOLD format ANSI_RESET "\r\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);}
 
     #define HEAP_CHECK(x) Debug_printf("HEAP CHECK %s " x "\r\n", heap_caps_check_integrity_all(true) ? "PASSED":"FAILED")
