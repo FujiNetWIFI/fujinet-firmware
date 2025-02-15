8651d1cdcc0ebd0a4577daf4f83441705e70912e
Noah Burney <noah.burney@gmail.com>
2025-02-14 19:08:08 -0600

    [webui] add edit button to host slots

--- lib/device/sio/fuji.cpp
+++ lib/device/sio/fuji.cpp
@@ -2781,6 +2781,7 @@ std::string sioFuji::get_host_prefix(int host_slot)
 fujiHost *sioFuji::set_slot_hostname(int host_slot, char *hostname)
 {
     _fnHosts[host_slot].set_hostname(hostname);
+    _populate_config_from_slots();
     return &_fnHosts[host_slot];
 }
 
