d5a9575a7672de366d99c59a3a98e77b1047682b
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-03-23 20:57:53 -0500

    Revert "Revert "fnDirCache - use pointers for sorted entries""
    This reverts commit 0e5fe26c0a99f940d17ab8613f14ede232646b56.

--- lib/FileSystem/fnDirCache.h
+++ lib/FileSystem/fnDirCache.h
@@ -14,10 +14,10 @@ class DirCache
 private:
 #ifdef ESP_PLATFORM
     std::vector<fsdir_entry,PSRAMAllocator<fsdir_entry>> _entries;
-    std::vector<fsdir_entry,PSRAMAllocator<fsdir_entry>> _entries_filtered;
+    std::vector<fsdir_entry *,PSRAMAllocator<fsdir_entry *>> _entries_filtered;
 #else
     std::vector<fsdir_entry> _entries;
-    std::vector<fsdir_entry> _entries_filtered;
+    std::vector<fsdir_entry *> _entries_filtered;
 #endif
     uint16_t _current = 0;
 
