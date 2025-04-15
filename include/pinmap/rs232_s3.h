0a64f00df3d52746c7aa9c6bef3eacf5637c6806
mozzwald <mozzwald@gmail.com>
2025-04-15 16:24:49 -0500

    rs232-s3: fix sdcard detect, send debug output to usb, enable button A, gpio pinmap fixes

--- include/pinmap/rs232_s3.h
+++ include/pinmap/rs232_s3.h
@@ -2,13 +2,15 @@
 #ifdef PINMAP_RS232_S3
 
 /* SD Card - fnFsSD.cpp */
+/* Card detect pin on RS232-REV1 goes high when card inserted */
+#define CARD_DETECT_HIGH        1
 #define PIN_CARD_DETECT         GPIO_NUM_9 // fnSystem.h
 #define PIN_CARD_DETECT_FIX     GPIO_NUM_9 // fnSystem.h
 
 #define PIN_SD_HOST_CS          GPIO_NUM_10  // fnFsSD.cpp
 #define PIN_SD_HOST_MISO        GPIO_NUM_13
 #define PIN_SD_HOST_MOSI        GPIO_NUM_11
-#define PIN_SD_HOST_SCK         GPIO_NUM_12
+#define PIN_SD_HOST_SCK         GPIO_NUM_21
 
 /* UART */
 #define PIN_UART0_RX            GPIO_NUM_44  // USB to Serial secondary
@@ -28,14 +30,14 @@
 #define PIN_DAC1                GPIO_NUM_21
 
 /* RS232 Pins */
-#define PIN_UART1_RX            GPIO_NUM_41 // RS232 35
-#define PIN_UART1_TX            GPIO_NUM_42 // RS232 36
+#define PIN_UART1_RX            GPIO_NUM_41 // (IN) ESP32S3 RX
+#define PIN_UART1_TX            GPIO_NUM_42 // (OUT) ESP32S3 TX
 #define PIN_RS232_RI            GPIO_NUM_16 // (OUT) Ring Indicator
-#define PIN_RS232_DCD           GPIO_NUM_NC // (OUT) Data Carrier Detect
-#define PIN_RS232_RTS           GPIO_NUM_NC // (IN) Request to Send
-#define PIN_RS232_CTS           GPIO_NUM_NC // (OUT) Clear to Send
-#define PIN_RS232_DTR           GPIO_NUM_17 // (IN) Data Terminal Ready
-#define PIN_RS232_DSR           GPIO_NUM_NC  // (OUT) Data Set Ready
-#define PIN_RS232_INVALID       GPIO_NUM_NC // (IN) RS232 Invalid Data (from TRS3238E)
+#define PIN_RS232_DCD           GPIO_NUM_4  // (OUT) Data Carrier Detect
+#define PIN_RS232_RTS           GPIO_NUM_15 // (IN) Request to Send
+#define PIN_RS232_CTS           GPIO_NUM_7  // (OUT) Clear to Send
+#define PIN_RS232_DTR           GPIO_NUM_6  // (IN) Data Terminal Ready
+#define PIN_RS232_DSR           GPIO_NUM_5  // (OUT) Data Set Ready
+#define PIN_RS232_INVALID       GPIO_NUM_18 // (IN) RS232 Invalid Data (from TRS3238E)
 
 #endif /* PINMAP_RS232_REV0 */
