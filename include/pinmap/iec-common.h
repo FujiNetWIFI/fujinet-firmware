da3f062dcaf9b5cac74e53f5444f01599376991b
FozzTexx <fozztexx@fozztexx.com>
2025-05-14 14:37:01 -0700

    Put shared GPIO pin definitions in include/pinmap/common.h (#928)

---
+++ include/pinmap/iec-common.h
@@ -0,0 +1,25 @@
+#ifndef PIN_IEC_CLK_IN
+#define PIN_IEC_CLK_IN          GPIO_NUM_33                    // use count: 5
+#endif /* PIN_IEC_CLK_IN */
+#ifndef IEC_HAS_RESET
+#define IEC_HAS_RESET                                          // use count: 5
+#endif /* IEC_HAS_RESET */
+#ifndef PIN_IEC_ATN
+#define PIN_IEC_ATN             GPIO_NUM_32                    // use count: 4
+#endif /* PIN_IEC_ATN */
+#ifndef PIN_IEC_CLK_OUT
+#define PIN_IEC_CLK_OUT         GPIO_NUM_33                    // use count: 4
+#endif /* PIN_IEC_CLK_OUT */
+#ifndef PIN_IEC_SRQ
+#define PIN_IEC_SRQ             GPIO_NUM_27                    // use count: 3
+#endif /* PIN_IEC_SRQ */
+
+/* Modem/Parallel Switch */
+/* Unused with Nugget    */
+#ifndef PIN_MODEM_ENABLE
+#define PIN_MODEM_ENABLE        GPIO_NUM_2  // High = Modem enabled
+#endif /* PIN_MODEM_ENABLE */
+#ifndef PIN_MODEM_UP9600
+#define PIN_MODEM_UP9600        GPIO_NUM_15 // High = UP9600 enabled
+#endif /* PIN_MODEM_UP9600 */
+
