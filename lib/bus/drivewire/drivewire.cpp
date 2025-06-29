13a91dd278829dba6e20c217b6f2c3a6a5ecc4b3
Chris Osborn <fozztexx@fozztexx.com>
2025-06-29 14:33:37 -0700

    Eliminate warnings when compiling CoCo

--- lib/bus/drivewire/drivewire.cpp
+++ lib/bus/drivewire/drivewire.cpp
@@ -62,11 +62,11 @@ inline uint16_t drivewire_checksum(uint8_t *buf, unsigned short len)
     return chk;
 }
 
+#ifdef ENABLE_DRIVEWIRE_INTR_TASK
 #ifdef ESP_PLATFORM
 static void drivewire_intr_task(void *arg)
 {
     uint32_t gpio_num;
-    int64_t d;
 
     systemBus *bus = (systemBus *)arg;
 
@@ -89,7 +89,8 @@ static void drivewire_intr_task(void *arg)
         vTaskDelay(10 / portTICK_PERIOD_MS); // avoid spinning too fast...
     }
 }
-#endif
+#endif /* ESP_PLATFORM */
+#endif /* ENABLE_DRIVEWIRE_INTR_TASK */
 
 // Helper functions outside the class defintions
 
@@ -207,7 +208,7 @@ void systemBus::op_readex()
 void systemBus::op_write()
 {
     drivewireDisk *d = nullptr;
-    uint16_t c1 = 0, c2 = 0;
+    uint16_t c1 = 0;
 
     drive_num = fnDwCom.read();
 
@@ -228,7 +229,7 @@ void systemBus::op_write()
     c1 = fnDwCom.read();
     c1 |= fnDwCom.read() << 8;
 
-    c2 = drivewire_checksum(sector_data, MEDIA_BLOCK_SIZE);
+    drivewire_checksum(sector_data, MEDIA_BLOCK_SIZE);
 
     // if (c1 != c2)
     // {
@@ -429,9 +430,11 @@ void systemBus::op_serwrite()
 
 void systemBus::op_serwritem()
 {
-    unsigned char vchan = fnDwCom.read();
-    unsigned char byte = fnDwCom.read();
-    unsigned char count = fnDwCom.read();
+    unsigned char vchan, count;
+
+    vchan = fnDwCom.read();
+    fnDwCom.read(); // discard
+    count = fnDwCom.read();
     
     for (int i = 0; i < count; i++) {
         int byte = fnDwCom.read();
