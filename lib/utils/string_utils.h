2f5c8f934a79f281966fe1ce9f18f8c0c4c884c4
Jaime Idolpx <jaime@idolpx.com>
2025-07-04 15:10:33 -0500

    update string_utils to fix mbedtls issue

--- lib/utils/string_utils.h
+++ lib/utils/string_utils.h
@@ -1,3 +1,20 @@
+// Meatloaf - A Commodore 64/128 multi-device emulator
+// https://github.com/idolpx/meatloaf
+// Copyright(C) 2020 James Johnston
+//
+// Meatloaf is free software : you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Meatloaf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Meatloaf. If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef STRING_UTILS_H
 #define STRING_UTILS_H
 
@@ -32,6 +49,7 @@ namespace mstr {
     bool equals(std::string &s1, const char *s2, bool case_sensitive = true);
     bool equals(const char* s1, const char *s2, bool case_sensitive);
     bool contains(std::string &s1, const char *s2, bool case_sensitive = true);
+    bool contains(const char *s1, const char *s2, bool case_sensitive = true);
     bool compare(std::string &s1, std::string &s2, bool case_sensitive = true); // s1 is Wildcard string, s2 is potential match
 
     std::vector<std::string> split(std::string toSplit, char ch, int limit = 9999);
