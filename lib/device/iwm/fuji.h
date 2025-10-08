01b88af506ffa4082ad40c08ae5c94d64a2afff5
Jaime Idolpx <jaime@idolpx.com>
2025-10-08 16:35:39 -0500

    Reworked the QRCode library (#1013)
    * Reworked the QRCode library
    
    * add full path to "qrmanager.h"
    
    * qrcode add nullptr checks

--- lib/device/iwm/fuji.h
+++ lib/device/iwm/fuji.h
@@ -147,6 +147,8 @@ private:
     Hash::Algorithm algorithm = Hash::Algorithm::UNKNOWN;
     bool hash_is_hex_output = false;
 
+    QRManager _qrManager = QRManager();
+
 protected:
     void iwm_dummy_command();                     // control 0xAA
     void iwm_hello_world();                       // status 0xAA
