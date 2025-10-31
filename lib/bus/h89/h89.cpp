bfc85956607b79966164201bb5c76282e29f7557
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-10-31 18:07:32 -0500

    [h89][bus] parking this.

--- lib/bus/h89/h89.cpp
+++ lib/bus/h89/h89.cpp
@@ -32,6 +32,8 @@ unsigned char xbyte=0xAA;
 #define D6 GPIO_NUM_12
 #define D7 GPIO_NUM_13
 
+#define OE GPIO_NUM_0 
+#define DIR GPIO_NUM_2
 #define STB GPIO_NUM_4
 #define ACK GPIO_NUM_15
 #define IBF GPIO_NUM_34
@@ -93,63 +95,96 @@ void systemBus::service()
     // xbyte++; 
 
 
-    ////////////////////////////////////////////////////////////////////////////
-    // THIS IS THE RECEIVE ROUTINE. IT WORKS!
-    // /STB is set high in ::setup()
+    // ////////////////////////////////////////////////////////////////////////////
+    // // THIS IS THE RECEIVE ROUTINE. IT WORKS!
+    // // /STB is set high in ::setup()
 
-    // Wait for /OBF to go high
-    Debug_printf("Wait for OBF to go high.\r\n");
-    while (gpio_get_level(GPIO_NUM_35) == DIGI_HIGH);
+    // // Wait for /OBF to go high
+    // Debug_printf("Wait for OBF to go high.\r\n");
+    // while (gpio_get_level(GPIO_NUM_35) == DIGI_HIGH);
 
-    Debug_printf("OBF Low\n Setting ACK LOW\r\n");
+    // Debug_printf("OBF Low\n Setting ACK LOW\r\n");
 
-    // Set /ACK low.
-    gpio_set_level(ACK,DIGI_LOW);
-    quick_delay();
+    // // Set /ACK low.
+    // gpio_set_level(ACK,DIGI_LOW);
+    // quick_delay();
 
-    Debug_printf("Waited 1us, sampling D0-D7\r\n");
+    // Debug_printf("Waited 1us, sampling D0-D7\r\n");
 
-    // sample and output bus state on console.
-    printf("LOW: ");
-    printf("%u",gpio_get_level(GPIO_NUM_13));
-    printf("%u",gpio_get_level(GPIO_NUM_12));
-    printf("%u",gpio_get_level(GPIO_NUM_14));
-    printf("%u",gpio_get_level(GPIO_NUM_27));
-    printf("%u",gpio_get_level(GPIO_NUM_26));
-    printf("%u",gpio_get_level(GPIO_NUM_25));
-    printf("%u",gpio_get_level(GPIO_NUM_33));
-    printf("%u\n",gpio_get_level(GPIO_NUM_32));
+    // // sample and output bus state on console.
+    // printf("LOW: ");
+    // printf("%u",gpio_get_level(GPIO_NUM_13));
+    // printf("%u",gpio_get_level(GPIO_NUM_12));
+    // printf("%u",gpio_get_level(GPIO_NUM_14));
+    // printf("%u",gpio_get_level(GPIO_NUM_27));
+    // printf("%u",gpio_get_level(GPIO_NUM_26));
+    // printf("%u",gpio_get_level(GPIO_NUM_25));
+    // printf("%u",gpio_get_level(GPIO_NUM_33));
+    // printf("%u\n",gpio_get_level(GPIO_NUM_32));
 
-    Debug_printf("Setting /ACK high again.\r\n");
+    // Debug_printf("Setting /ACK high again.\r\n");
 
-    // Set /ACK high again.
-    gpio_set_level(ACK,DIGI_HIGH);
-    quick_delay();
+    // // Set /ACK high again.
+    // gpio_set_level(ACK,DIGI_HIGH);
+    // quick_delay();
 
-    Debug_printf("Waiting for OBF to go high\r\n");
+    // Debug_printf("Waiting for OBF to go high\r\n");
 
-    // Wait for /OBF to go low
-    while (gpio_get_level(GPIO_NUM_35) == DIGI_LOW);
+    // // Wait for /OBF to go low
+    // while (gpio_get_level(GPIO_NUM_35) == DIGI_LOW);
     
-    Debug_printf("OBF now high. Sampling bus.\r\n");
+    // Debug_printf("OBF now high. Sampling bus.\r\n");
 
-    // sample and output bus state on console.
-    printf(" HI: ");
-    printf("%u",gpio_get_level(GPIO_NUM_13));
-    printf("%u",gpio_get_level(GPIO_NUM_12));
-    printf("%u",gpio_get_level(GPIO_NUM_14));
-    printf("%u",gpio_get_level(GPIO_NUM_27));
-    printf("%u",gpio_get_level(GPIO_NUM_26));
-    printf("%u",gpio_get_level(GPIO_NUM_25));
-    printf("%u",gpio_get_level(GPIO_NUM_33));
-    printf("%u\n",gpio_get_level(GPIO_NUM_32));
+    // // sample and output bus state on console.
+    // printf(" HI: ");
+    // printf("%u",gpio_get_level(GPIO_NUM_13));
+    // printf("%u",gpio_get_level(GPIO_NUM_12));
+    // printf("%u",gpio_get_level(GPIO_NUM_14));
+    // printf("%u",gpio_get_level(GPIO_NUM_27));
+    // printf("%u",gpio_get_level(GPIO_NUM_26));
+    // printf("%u",gpio_get_level(GPIO_NUM_25));
+    // printf("%u",gpio_get_level(GPIO_NUM_33));
+    // printf("%u\n",gpio_get_level(GPIO_NUM_32));
 
-    Debug_printf("Looping back around...\r\n");
+    // TX Firmware Pass 2 ///////////////////////////////////////////////////
+    
+    xbyte = 0xAA;
+
+    Debug_printf("Waiting for OE low\n");
+    // Wait for OE to go low, then wait 1us
+    while (gpio_get_level(OE) == DIGI_HIGH);
+    esp_rom_delay_us(1);
+    Debug_printf("OE low\n");
+
+    while(1) // Loop forever.
+    {
+        Debug_printf("%02X\n",xbyte);
+
+        // Write byte
+        gpio_set_level(D7,xbyte & 0x80 ? DIGI_HIGH : DIGI_LOW);
+        gpio_set_level(D6,xbyte & 0x40 ? DIGI_HIGH : DIGI_LOW);
+        gpio_set_level(D5,xbyte & 0x20 ? DIGI_HIGH : DIGI_LOW);
+        gpio_set_level(D4,xbyte & 0x10 ? DIGI_HIGH : DIGI_LOW);
+        gpio_set_level(D3,xbyte & 0x08 ? DIGI_HIGH : DIGI_LOW);
+        gpio_set_level(D2,xbyte & 0x04 ? DIGI_HIGH : DIGI_LOW);
+        gpio_set_level(D1,xbyte & 0x02 ? DIGI_HIGH : DIGI_LOW);
+        gpio_set_level(D0,xbyte & 0x01 ? DIGI_HIGH : DIGI_LOW);
+
+        // Set STB low
+        gpio_set_level(STB,DIGI_LOW);
+
+        // Wait IBF to go high
+        while(gpio_get_level(IBF) == DIGI_LOW);
+
+        // Increment and go back to next byte
+        xbyte++;
+    }
 }
     
 void systemBus::setup()
 {
-    Debug_println("H89 SETUP");
+    Debug_println("H89 SETUP\n");
+    Debug_println("TX Pass 2 (lots of bytes)\n");
 
     // // Reset pins
     gpio_reset_pin(GPIO_NUM_0);
