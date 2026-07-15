981090bcee48efcb12ccf5d7d6fde1a0b8d13b44
Jaime Idolpx <jaime@idolpx.com>
2026-07-15 07:11:45 -0400

    Enhance WebDAV (#1463)
    restructure webdav code
    enhance protocol to work better with Windows Explorer and OSX Finder
    optimize things to enhance speed

---
+++ lib/http/webdav/handler.h
@@ -0,0 +1,26 @@
+// Meatloaf - A Commodore 64/128 multi-device emulator
+// https://github.com/idolpx/meatloaf
+// Copyright(C) 2020 James Johnston
+//
+// Meatloaf is free software : you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Meatloaf is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Meatloaf. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+#ifndef MIN_CONFIG
+
+#include <esp_http_server.h>
+
+esp_err_t webdav_handler(httpd_req_t *req);
+void webdav_register(httpd_handle_t server, const char *root_uri = "/", const char *root_path = "/");
+
+#endif // MIN_CONFIG
