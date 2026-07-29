500d808f728fb9a2faa8f3f2837f8f64bc4aa3a2
FozzTexx <fozztexx@fozztexx.com>
2026-07-29 11:06:53 -0700

    Changing fujiDevice mixins to return a list commands they accept. (#1498)
    Changing fujiDevice mixins to return a list of all the commands they
    accept. This removes the giant switch statement and makes it possible to
    check to see if a command is supported before trying to call it.

--- lib/device/HashMixin.h
+++ lib/device/HashMixin.h
@@ -9,17 +9,32 @@
 
 class HashMixin : public FujiDeviceMixin
 {
+private:
+    FujiMixinCommandHandlers handlers = {
+    };
+
 protected:
     Hash::Algorithm _algorithm = Hash::Algorithm::UNKNOWN;
 
-    void hash_input(uint16_t len);
-    void hash_compute(bool clear_data, Hash::Algorithm algo);
-    void hash_length(bool as_hex);
-    void hash_output(bool as_hex);
-    void hash_clear();
+    FujiMixinCommandHandlers commandHandlers() override { return handlers; }
+
+    void hash_input(const FUJI_COMMAND_PACKET &packet);
+    void hash_compute(const FUJI_COMMAND_PACKET &packet);
+    void hash_length(const FUJI_COMMAND_PACKET &packet);
+    void hash_output(const FUJI_COMMAND_PACKET &packet);
+    void hash_clear(const FUJI_COMMAND_PACKET &packet);
 
 public:
-    bool processCommand(const FUJI_COMMAND_PACKET &packet) override;
+    HashMixin() {
+        handlers = {
+            { FUJICMD_HASH_INPUT,            FM_CMD_HANDLER(hash_input)   },
+            { FUJICMD_HASH_COMPUTE,          FM_CMD_HANDLER(hash_compute) },
+            { FUJICMD_HASH_COMPUTE_NO_CLEAR, FM_CMD_HANDLER(hash_compute) },
+            { FUJICMD_HASH_LENGTH,           FM_CMD_HANDLER(hash_length)  },
+            { FUJICMD_HASH_OUTPUT,           FM_CMD_HANDLER(hash_output)  },
+            { FUJICMD_HASH_CLEAR,            FM_CMD_HANDLER(hash_clear)   },
+        };
+    }
 };
 
 #endif // FUJI_MIXINS_ENABLED
