db9c529a4b286a018414b77cc199f2b342bd22ed
Bill Athing <bill.athing@gmail.com>
2026-09-12 16:07:45 -0500

    Add COCO_HS_UART support to ESP32UARTChannel; fix DevKitC UART1 RX pin

--- lib/hardware/ESP32UARTChannel.cpp
+++ lib/hardware/ESP32UARTChannel.cpp
@@ -73,12 +73,20 @@ void ESP32UARTChannel::begin(const ChannelConfig& conf)
 
     _halfDuplex = conf.isHalfDuplex;
 
+    // set RTS pin mode
     if (controlPins.rts >= 0)
+    {
         fnSystem.set_pin_mode(controlPins.rts, gpio_mode_t::GPIO_MODE_INPUT);
+    }
     if (controlPins.cts >= 0)
     {
         fnSystem.set_pin_mode(controlPins.cts, gpio_mode_t::GPIO_MODE_OUTPUT);
         fnSystem.digital_write(controlPins.cts, DIGI_LOW);
+    }
+    if (controlPins.rts >= 0 && controlPins.cts >= 0)
+    {
+        uart_set_hw_flow_ctrl(_uart_num, UART_HW_FLOWCTRL_CTS_RTS, 0);
+        Debug_printv("RTS/CTS flow control enabled");
     }
 
     if (controlPins.dtr >= 0)
@@ -241,7 +249,8 @@ void ESP32UARTChannel::setDSR(bool state)
 
 bool ESP32UARTChannel::getRTS()
 {
-    return getPin(controlPins.rts);
+    bool state = getPin(controlPins.rts);
+    return state;
 }
 
 void ESP32UARTChannel::setCTS(bool state)
