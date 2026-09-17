b3dcff67da66889c749b43c0e50801f70e10671d
wdathing <70298471+wdathing@users.noreply.github.com>
2026-09-17 18:25:05 -0500

    Add COCO_HS_UART support to ESP32UARTChannel; fix DevKitC UART1 RX pin (#1628)

--- include/pinmap/coco_devkitc.h
+++ include/pinmap/coco_devkitc.h
@@ -3,7 +3,7 @@
 
 
 /* UART - fnuart.cpp */
-#define PIN_UART1_RX            GPIO_NUM_13 // RS232
+#define PIN_UART1_RX            GPIO_NUM_33 // RS232
 #define PIN_UART1_TX            GPIO_NUM_21 // RS232
 
 /* LEDs - leds.cpp */
